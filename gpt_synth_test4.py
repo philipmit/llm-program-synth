7b55e6989de408ca1022531de922d99a08da5c08
philipmit <pschro@mit.edu>
2023-11-10 17:49:59 -0500

    Output 2 from GPT: Typically, the error "name 'pd' is not defined" means pandas library is not imported properly or not available. However, in your code, it is imported at the top. There might be a contextual or dependency issue on your system where the pandas library could not load. Please ensure that pandas is installed properly. It will be helpful to try importing pandas in a separate, simple script to make sure that works.
    Meanwhile, please find your code with little modifications.

--- gpt_synth_test4.py
+++ gpt_synth_test4.py
@@ -7,8 +7,8 @@ from torch import nn
 from torch.nn.utils.rnn import pad_sequence
 import torch.optim as optim
 # File paths
-TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
-LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
+TRAIN_DATA_PATH = r"/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
+LABEL_FILE = r"/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Define the Dataset
@@ -23,9 +23,10 @@ class ICUData(Dataset):
     def __getitem__(self, idx):
         file_path = os.path.join(self.data_path, self.file_names[idx])
         data = pd.read_csv(file_path).fillna(0)
-        features = data.select_dtypes(include=[np.number])
+        features = data.drop(['Hours'], axis=1)
         label = self.labels[idx]
         return torch.tensor(features.values, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+        
 # Define the LSTM model
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
@@ -35,14 +36,16 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
         self.sigmoid = nn.Sigmoid()
+        
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         return self.sigmoid(out)
+        
 # Model parameters
-input_size = 15  # number of features in the data
+input_size = 14  # number of features in the data
 hidden_size = 256
 num_layers = 2
 output_size = 1
@@ -75,7 +78,7 @@ def predict_icu_mortality(patient_data):
     model.eval()
     
     patient_data = pd.read_csv(patient_data).fillna(0)
-    features = patient_data.select_dtypes(include=[np.number])
+    features = patient_data.drop(['Hours'], axis=1)
     features = torch.tensor(features.values, dtype=torch.float32).to(device)
     
     with torch.no_grad():
