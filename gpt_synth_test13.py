3f779e6b4f86c4691e4473a7e3143fa892cba9ae
philipmit <pschro@mit.edu>
2023-11-10 18:59:01 -0500

    Output 3:
    Error from previous code:
      File "<stdin>", line 22, in get_gpt_resp_code
      File "<string>", line 69, in <module>
      File "<string>", line 40, in __init__
    <class 'NameError'>
    name 'LSTMModel' is not defined
    
    Response from GPT:
    It appears there is still an ordering issue when defining the `LSTMModel`. The `device` variable must also be defined before we instantiate the `LSTMModel`. Here is the corrected code:

--- gpt_synth_test13.py
+++ gpt_synth_test13.py
@@ -9,7 +9,8 @@ import torch.optim as optim
 # File paths
 TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
 LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
-
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Define the Dataset
 class ICUData(Dataset):
     def __init__(self, data_path, label_file):
@@ -17,23 +18,15 @@ class ICUData(Dataset):
         label_data = pd.read_csv(label_file)
         self.file_names = label_data['stay']
         self.labels = label_data['y_true']
-    
+   
     def __len__(self):
         return len(self.file_names)
-    
     def __getitem__(self, idx):
         file_path = os.path.join(self.data_path, self.file_names[idx])
         data = pd.read_csv(file_path).fillna(0)
         features = data.select_dtypes(include=[np.number])
         label = self.labels[idx]
         return torch.tensor(features.values, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
-
-# Define Batch Generator
-def collate_fn(batch):
-    inputs = [item[0] for item in batch]
-    targets = [item[1] for item in batch]
-    return pad_sequence(inputs, batch_first=True), torch.stack(targets)
-
 # Define LSTM Model
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
@@ -42,29 +35,26 @@ class LSTMModel(nn.Module):
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
-    
+   
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
         
-        out, _ = self.lstm(x, (h0, c0))  
+        out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         return out
-
-# Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyperparameters
 num_classes = 1
 num_epochs = 100
 batch_size = 32
 learning_rate = 0.001
-# Create Dataset and DataLoader
-icu_data = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
-data_loader = DataLoader(icu_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
-# Define dimensions
-input_size = icu_data[0][0].shape[1]
 hidden_size = 50
 num_layers = 2
+# Create Dataset and DataLoader
+icu_data = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
+data_loader = DataLoader(icu_data, batch_size=batch_size, shuffle=True)
+# Define dimensions
+input_size = icu_data[0][0].shape[1]
 # Create LSTM Model
 model = LSTMModel(input_size, hidden_size, num_layers, num_classes).to(device)
 # Loss and Optimizer
@@ -72,23 +62,26 @@ criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 # Training loop
 for epoch in range(num_epochs):
-    for i, (data, labels) in enumerate(data_loader):
-        data = data.to(device)
+    for i, (features, labels) in enumerate(data_loader):
+        features = features.to(device)
         labels = labels.to(device)
         
-        outputs = model(data)
-        loss = criterion(outputs, labels.unsqueeze(1))
+        # Forward pass
+        outputs = model(features)
+        loss = criterion(outputs, labels)
         
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    if (epoch+1) % 10 == 0:
-        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+        
+        if (i+1) % 10 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                   .format(epoch+1, num_epochs, i+1, len(data_loader), loss.item()))
 
 def predict_icu_mortality(raw_data):
     raw_data = raw_data.fillna(0)
     features = raw_data.select_dtypes(include=[np.number])
     tensor_data = torch.tensor(features.values, dtype=torch.float32).unsqueeze(0).to(device)
-    output = torch.sigmoid(model(tensor_data))
-    return output.item()
+    output = model(tensor_data)
+    return torch.sigmoid(output).item()
