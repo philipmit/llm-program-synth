e857136f8e1a54dc404701b0a69528f8b9d2de6c
philipmit <pschro@mit.edu>
2023-11-11 13:36:51 -0500

    Initial output

---
+++ gpt_synth_test20.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+# File paths
+TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
+TEST_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/test/"
+LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
+# Define the Dataset
+class ICUData(Dataset):
+    def __init__(self, data_path, label_file):
+        self.data_path = data_path
+        label_data = pd.read_csv(label_file)
+        self.file_names = label_data['stay']
+        self.labels = label_data['y_true']
+    def __len__(self):
+        return len(self.file_names)
+    def __getitem__(self, idx):
+        file_path = os.path.join(self.data_path, self.file_names[idx])
+        data = pd.read_csv(file_path).fillna(0)
+        features = data.select_dtypes(include=[np.number])
+        label = self.labels[idx]
+        return torch.tensor(features.values, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+# Define an LSTM model
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.lstm(x, (h0, c0))  
+        out = self.fc(out[:, -1, :])  
+        return out
+# Train the LSTM model
+dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
+dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=True)
+input_size = next(iter(dataloader))[0].size(-1)
+hidden_size = 50
+num_layers = 2
+output_size = 1
+model = LSTMModel(input_size, hidden_size, num_layers, output_size)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+num_epochs = 50
+for epoch in range(num_epochs):
+    for i, (features, labels) in enumerate(dataloader):
+        output = model(features)
+        labels = labels.view(-1, 1)
+        loss = criterion(output, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+def predict_icu_mortality(patient_data):
+    model.eval()
+    with torch.no_grad():
+        data = pd.read_csv(patient_data).fillna(0)
+        features = data.select_dtypes(include=[np.number])
+        output = model(torch.tensor(features.values, dtype=torch.float32).view(1,-1,input_size))
+        prediction = torch.sigmoid(output)
+    return prediction.item()
