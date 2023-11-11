c16a3a17d02aa07e926c2a5bfd1ea94bb02f31ef
philipmit <pschro@mit.edu>
2023-11-11 13:57:03 -0500

    Output 2: Response to RuntimeError
    Error from previous code:
      File "<stdin>", line 2, in <module>
      File "<string>", line 87, in <module>
      File "<string>", line 50, in train
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/dataloader.py", line 628, in __next__
        data = self._next_data()
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/dataloader.py", line 671, in _next_data
        data = self._dataset_fetcher.fetch(index)  # may raise StopIteration
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/fetch.py", line 61, in fetch
        return self.collate_fn(data)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 265, in default_collate
        return collate(batch, collate_fn_map=default_collate_fn_map)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 143, in collate
        return [collate(samples, collate_fn_map=collate_fn_map) for samples in transposed]  # Backwards compatibility.
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 143, in <listcomp>
        return [collate(samples, collate_fn_map=collate_fn_map) for samples in transposed]  # Backwards compatibility.
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 120, in collate
        return collate_fn_map[elem_type](batch, collate_fn_map=collate_fn_map)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 163, in collate_tensor_fn
        return torch.stack(batch, 0, out=out)
    <class 'RuntimeError'>
    stack expects each tensor to be equal size, but got [93, 15] at entry 0 and [82, 15] at entry 2
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    
    The error message indicates that there is an issue with the collate function when using the `DataLoader`. More specifically, `torch.stack()` is failing because it requires each tensor to have the same size, but different patients have different numbers of records, resulting in tensors of different sizes.
    
    To resolve this issue, we need to apply padding to the sequences with fewer records in order to ensure that all tensors have the same size. PyTorch provides functionality for this purpose in the form of the `pad_sequence` function. Therefore, we can modify the `__getitem__` method in the dataset to return the sequence length as well, and override the `collate_fn` method in the `DataLoader` to handle the padding and keep the original lengths for later use in the model.
    
    Also, to ensure the appropriate use of LSTM, it's necessary to normalize the sequence data. We add a data-normalization step in the `__getitem__` function in the `ICUData` class.
    
    Here's the fixed code:

--- gpt_synth_test22.py
+++ gpt_synth_test22.py
@@ -3,8 +3,11 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from sklearn.preprocessing import StandardScaler
 from torch import nn
 from torch.nn import functional as F
+
 # File paths
 TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
 LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
@@ -15,28 +18,31 @@ class ICUData(Dataset):
         label_data = pd.read_csv(label_file)
         self.file_names = label_data['stay']
         self.labels = label_data['y_true']
+        self.scaler = StandardScaler()
     def __len__(self):
         return len(self.file_names)
     def __getitem__(self, idx):
         file_path = os.path.join(self.data_path, self.file_names[idx])
         data = pd.read_csv(file_path).fillna(0)
-        features = data.select_dtypes(include=[np.number])
+        features = self.scaler.fit_transform(data.select_dtypes(include=[np.number]))
         label = self.labels[idx]
-        return torch.tensor(features.values, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
-
+        return torch.tensor(features, dtype=torch.float32), torch.tensor(label, dtype=torch.float32), features.shape[0]
+# Custom collate_fn to handle padding of tensors
+def collate_fn(batch):
+    sequences, labels, lengths = zip(*batch)
+    sequences_padded = pad_sequence(sequences, batch_first=True, padding_value=0)
+    return sequences_padded, torch.stack(labels), torch.stack(lengths)
 # LSTM model
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(LSTMModel, self).__init__()
-        self.num_layers = num_layers
         self.hidden_size = hidden_size
-       
+        self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=0.1)
         self.linear = nn.Linear(hidden_size, num_classes)
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        
         out, _ = self.lstm(x, (h0, c0))
         out = out[:, -1, :]
         out = self.linear(out)
@@ -45,10 +51,8 @@ class LSTMModel(nn.Module):
 def train(model, dataloader, criterion, optimizer, n_epochs):
     model.train()
     for epoch in range(n_epochs):
-        
         epoch_loss = 0.0
-        for i, (inputs, labels) in enumerate(dataloader):
-            
+        for i, (inputs, labels, lengths) in enumerate(dataloader):
             inputs = inputs.to(device)
             labels = labels.to(device)
             optimizer.zero_grad()
@@ -58,29 +62,28 @@ def train(model, dataloader, criterion, optimizer, n_epochs):
             optimizer.step()
             epoch_loss += loss.item()
         print(f'Epoch {epoch+1} / {n_epochs}: Avg. Loss: {epoch_loss / len(dataloader)}')
-
 def predict_icu_mortality(raw_data): 
     processed_data = raw_data.fillna(0)
-    features = processed_data.select_dtypes(include=[np.number])
-    tensor_data = torch.tensor(features.values, dtype=torch.float32).unsqueeze(0)
+    features = dataset.scaler.transform(processed_data.select_dtypes(include=[np.number]))
+    tensor_data = torch.tensor(features, dtype=torch.float32).unsqueeze(0)
     with torch.no_grad():
         model.eval()
         tensor_data = tensor_data.to(device)
         output = model(tensor_data)
-    probas = torch.sigmoid(output).cpu()
+        probas = torch.sigmoid(output).cpu()
     return probas[0][0].item()
   
 # Hyperparameters
-input_size = 14  # number of features
-hidden_size = 128  # LSTM hidden size
-num_layers = 2  # number of LSTM layers
-num_classes = 1  # output size 
-batch_size = 64  # batch size
-lr = 0.001  # learning rate
-n_epochs = 10  # number of epochs
+input_size = 14  
+hidden_size = 128  
+num_layers = 2  
+num_classes = 1 
+batch_size = 64  
+lr = 0.001
+n_epochs = 10 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 model = LSTMModel(input_size, hidden_size, num_layers, num_classes).to(device)
 criterion = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
