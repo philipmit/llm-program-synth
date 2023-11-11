a503ea269594be4950cb36c1d2f40412a4615df6
philipmit <pschro@mit.edu>
2023-11-11 13:38:43 -0500

    Output 3: Response to NameError
    Error from previous code:
      File "<stdin>", line 24, in get_gpt_resp_code
      File "<string>", line 50, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/dataloader.py", line 628, in __next__
        data = self._next_data()
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/dataloader.py", line 671, in _next_data
        data = self._dataset_fetcher.fetch(index)  # may raise StopIteration
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/fetch.py", line 61, in fetch
        return self.collate_fn(data)
      File "<string>", line 30, in collate_fn
    <class 'NameError'>
    name 'pad_sequence' is not defined
    
    Response from GPT:
    The error message suggests that the function `pad_sequence` is not defined. This is because for using `pad_sequence` you need to specify it as `torch.nn.utils.rnn.pad_sequence`.
    
    Here is the updated code:

--- gpt_synth_test20.py
+++ gpt_synth_test20.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import torch
 from torch import nn
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 # File paths
 TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
 TEST_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/test/"
