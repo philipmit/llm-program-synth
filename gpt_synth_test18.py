e38800d88d89496ff311b5c06411e33327f14982
philipmit <pschro@mit.edu>
2023-11-11 13:30:59 -0500

    Output 3: Response to RuntimeError
    Error from previous code:
      File "<stdin>", line 23, in get_gpt_resp_code
      File "<string>", line 50, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/dataloader.py", line 628, in __next__
        data = self._next_data()
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/dataloader.py", line 671, in _next_data
        data = self._dataset_fetcher.fetch(index)  # may raise StopIteration
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/fetch.py", line 61, in fetch
        return self.collate_fn(data)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 265, in default_collate
        return collate(batch, collate_fn_map=default_collate_fn_map)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 143, in collate
        return [collate(samples, collate_fn_map=collate_fn_map) for samples in transposed]  # Backwards compatibility.
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 143, in <listcomp>
        return [collate(samples, collate_fn_map=collate_fn_map) for samples in transposed]  # Backwards compatibility.
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 120, in collate
        return collate_fn_map[elem_type](batch, collate_fn_map=collate_fn_map)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/utils/data/_utils/collate.py", line 163, in collate_tensor_fn
        return torch.stack(batch, 0, out=out)
    <class 'RuntimeError'>
    stack expects each tensor to be equal size, but got [87, 14] at entry 0 and [86, 14] at entry 1
    
    Response from GPT:
    It seems the issue here is that the LSTM model expects inputs of the same length in the sequences. However, some of the ICU time-series data may have different lengths because the number of hours from admission to gathering various health metrics can vary across patients. As a common practice for dealing with variable length time-series data in deep learning models, we can apply padding to make the sequences have the same length.
    
    Here is the modified code:

--- gpt_synth_test18.py
+++ gpt_synth_test18.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from torch.utils.data import Dataset, DataLoader
 # File paths
 TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
@@ -21,10 +22,12 @@ class ICUData(Dataset):
         data = pd.read_csv(file_path).fillna(0)
         features = data.drop(columns='Hours').select_dtypes(include=[np.number])
         label = self.labels[idx]
-        return torch.tensor(features.values, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
-# Load the data
+        length = features.shape[0]
+        return torch.tensor(features.values, dtype=torch.float32), torch.tensor(label, dtype=torch.float32), length
+# load data and sort by lengths for pack_padded_sequence
 dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+data_loader = sorted(data_loader, key=lambda x: x[2], reverse=True)
 # Define the LSTM model
 class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
@@ -33,10 +36,10 @@ class LSTM(nn.Module):
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_dim)
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_().to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_().to(x.device)
-        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+    def forward(self, x, lengths):
+        pack = pack_padded_sequence(x, lengths, batch_first=True)
+        out, (hn, cn) = self.lstm(pack)
+        out, _ = pad_packed_sequence(out, batch_first=True)
         out = self.fc(out[:, -1, :]) 
         return torch.sigmoid(out)
 # Initialize the LSTM
@@ -47,9 +50,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 epochs = 10
 # Training
 for epoch in range(epochs):
-    for batch, (features, labels) in enumerate(dataloader):
+    for batch, (features, labels, lengths) in enumerate(data_loader):
         model.zero_grad()
-        output = model(features)
+        output = model(features, lengths)
         loss = criterion(output, labels.unsqueeze(1))
         loss.backward()
         optimizer.step()
@@ -59,5 +62,5 @@ for epoch in range(epochs):
 def predict_icu_mortality(patient_data):
     data = pd.read_csv(patient_data).fillna(0)
     features = torch.tensor(data.drop(columns='Hours').select_dtypes(include=[np.number]).values, dtype=torch.float32)
-    model_output = model(features.unsqueeze(0))
+    model_output = model(features.unsqueeze(0), [features.shape[0]])
     return model_output.item()
