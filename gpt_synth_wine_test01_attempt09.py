dfb075cd780beefca0532cb2a6914135c5308e0a
philipmit <pschro@mit.edu>
2023-11-13 16:44:01 -0500

    Initial output: Sure, here's the final program that includes logistic regression training and the predict_label function.
    ```python
    from sklearn.datasets import load_wine
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    import numpy as np
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Define and train the logistic regression model
    lr = LogisticRegression(max_iter=1000)
    lr.fit(X_train, y_train)
    
    # Define the function predict_label
    def predict_label(raw_data):
        return lr.predict_proba(np.array(raw_data).reshape(1,-1))
    
    ```
    This program will train a logistic regression model using the training data (X_train, y_train). The predict_label function takes the raw data for a single sample and returns the predicted probabilities of the labels for that sample. It uses the trained logistic regression model to predict the probabilities. This function can be used for any new, unseen data to predict the probabilities of the wine labels. The reshape function is used to make sure that the raw data is in the correct format for the predict_proba function.

---
+++ gpt_synth_wine_test01_attempt09.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Define and train the logistic regression model
+lr = LogisticRegression(max_iter=1000)
+lr.fit(X_train, y_train)
+# Define the function predict_label
+def predict_label(raw_data):
+    return lr.predict_proba(np.array(raw_data).reshape(1,-1))
