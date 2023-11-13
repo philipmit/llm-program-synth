917f2f70593f9b3b29a2c8f3f5455ae26dd7a0cd
philipmit <pschro@mit.edu>
2023-11-13 15:04:27 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 21, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 693, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
    
    Response from GPT:
    There's an error in your validation code because the function `predict_label()` does not return 'probabilities of prediction for each class' instead returns 'predicted class labels'. The `roc_auc_score()` in the validation code expects 'true multilabel indicator labels' and their corresponding predicted probabilities, but not the predicted class labels.
    
    Below is the correct implementation of predict_label() function that returns the probabilities for each class:
    
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Apply standard scaling to get optimized result
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    # Train the multi-layer perceptron
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    mlp.fit(X_train, y_train)
    
    def predict_label(single_sample_data):
        # The sample needs to be scaled using the scaler fitted on the training set
        single_sample_data_scaled = sc.transform([single_sample_data])
        # Return array of predicted probabilities
        return mlp.predict_proba(single_sample_data_scaled)[0]
    ```
    
    The function predict_label() now aligns with the requirements of the roc_auc_score() function in the validation code, so you should no longer be getting the ValueError.

--- gpt_synth_iris_test14_attempt04.py
+++ gpt_synth_iris_test14_attempt04.py
@@ -2,7 +2,6 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
-import numpy as np
 # Load the Iris dataset
 iris = load_iris()
 X = iris.data
@@ -12,14 +11,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Apply standard scaling to get optimized result
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-# train the multi-layer perceptron
+# Train the multi-layer perceptron
 mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
 mlp.fit(X_train, y_train)
 def predict_label(single_sample_data):
     # The sample needs to be scaled using the scaler fitted on the training set
     single_sample_data_scaled = sc.transform([single_sample_data])
-    # returns array of predicted probabilities
-    proba = mlp.predict_proba(single_sample_data_scaled)
-    # using argmax to get the class with the highest probability
-    predicted_class = np.argmax(proba, axis=1)
-    return predicted_class
+    # Return array of predicted probabilities
+    return mlp.predict_proba(single_sample_data_scaled)[0]
