fe72f183b751f32b80c9af70d07a75f702e4dc73
philipmit <pschro@mit.edu>
2023-11-13 18:44:14 -0500

    Output 4 (phase 2): Response to IndentationError
    Error from previous code:
      File "<stdin>", line 147, in <module>
    <class 'IndentationError'>
    unexpected indent (<string>, line 1)
    
    Response from GPT:
    ```
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    import numpy as np
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize StandardScaler
    sc = StandardScaler().fit(X_train)
    
    # Standardize the training set and test set
    X_train_std = sc.transform(X_train)
    X_test_std = sc.transform(X_test)
    
    # Initialize Multilayer Perceptron classifier
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    
    # Fit the data to the MLP classifier
    mlp.fit(X_train_std, y_train)
    
    def predict_label(raw_data_sample):
        # Reshape raw_data_sample to 2D array
        raw_data_sample = np.reshape(raw_data_sample, (1, -1))
    
        # Standardize the raw data sample using the same mean and standard deviation as before
        raw_data_sample_std = sc.transform(raw_data_sample)
    
        # Make the prediction
        return mlp.predict_proba(raw_data_sample_std)[0]
    ```

--- UCI_benchmarks/gpt_synth_iris_test_set01/test02.py
+++ UCI_benchmarks/gpt_synth_iris_test_set01/test02.py
@@ -1,4 +1,3 @@
- python
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -10,19 +9,19 @@ X = iris.data
 y = iris.target
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# standardizing the dataset and storing the mean and standard deviation
+# Initialize StandardScaler
 sc = StandardScaler().fit(X_train)
-# standardize the training set and test set
+# Standardize the training set and test set
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
-# initialize Multilayer Perceptron classifier
+# Initialize Multilayer Perceptron classifier
 mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
-# fitting the data to the MLP classifier
+# Fit the data to the MLP classifier
 mlp.fit(X_train_std, y_train)
 def predict_label(raw_data_sample):
     # Reshape raw_data_sample to 2D array
     raw_data_sample = np.reshape(raw_data_sample, (1, -1))
-    # standardize the raw data sample using the same mean and standard deviation as before
+    # Standardize the raw data sample using the same mean and standard deviation as before
     raw_data_sample_std = sc.transform(raw_data_sample)
-    # Making the prediction
+    # Make the prediction
     return mlp.predict_proba(raw_data_sample_std)[0]
