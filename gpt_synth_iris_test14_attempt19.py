314ae3756935431bfa3c5732237c4decdd4582a5
philipmit <pschro@mit.edu>
2023-11-13 15:18:49 -0500

    Initial output: Here's the complete code that trains a Multi-Layer Perceptron model on the Iris dataset and includes a 'predict_label' function that takes a raw unprocessed single sample as input and returns the predicted probabilities:
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.neural_network import MLPClassifier
    from sklearn.preprocessing import StandardScaler
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Data normalization
    scaler = StandardScaler()
    X_train_scaled = scaler.fit_transform(X_train)
    
    # Create a Multi-Layer Perceptron classifier and train it
    mlp = MLPClassifier(hidden_layer_sizes=(10,), activation='relu', solver='adam', max_iter=1000)
    mlp.fit(X_train_scaled, y_train)
    
    # Define the predict_label function
    def predict_label(raw_sample):
        # Reshape raw_sample and normalize
        sample = raw_sample.reshape(1, -1)
        sample = scaler.transform(sample)
    
        # Use the trained MLP model to predict the probabilities
        predicted_probabilities = mlp.predict_proba(sample)
        return predicted_probabilities
    ```
    This code divides the Iris dataset into training and test sets, normalizes the training set data, creates a Multi-Layer Perceptron classifier, trains the classifier using the normalized training set, and finally defines the 'predict_label' function. The function takes in unprocessed raw data for a single sample, scales it using the trained scaler, and then uses the MPL model to predict the sample probabilities.

---
+++ gpt_synth_iris_test14_attempt19.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Data normalization
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+# Create a Multi-Layer Perceptron classifier and train it
+mlp = MLPClassifier(hidden_layer_sizes=(10,), activation='relu', solver='adam', max_iter=1000)
+mlp.fit(X_train_scaled, y_train)
+# Define the predict_label function
+def predict_label(raw_sample):
+    # Reshape raw_sample and normalize
+    sample = raw_sample.reshape(1, -1)
+    sample = scaler.transform(sample)
+    # Use the trained MLP model to predict the probabilities
+    predicted_probabilities = mlp.predict_proba(sample)
+    return predicted_probabilities
