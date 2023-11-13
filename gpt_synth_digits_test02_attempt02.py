3d326ba61a152e4a60143266de63cf9dbf3c247c
philipmit <pschro@mit.edu>
2023-11-13 17:19:04 -0500

    Initial output: ```python from sklearn.datasets import load_digits from sklearn.model_selection import train_test_split from sklearn.linear_model import LogisticRegression import numpy as np
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    log_reg = LogisticRegression(max_iter=5000)
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_data):
        raw_data = np.array(raw_data).reshape(1, -1)
        prediction = log_reg.predict_proba(raw_data)
        return prediction
    ```

---
+++ gpt_synth_digits_test02_attempt02.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train the logistic regression model
+log_reg = LogisticRegression(max_iter=5000)
+log_reg.fit(X_train, y_train)
+def predict_label(raw_data):
+    raw_data = np.array(raw_data).reshape(1, -1)
+    prediction = log_reg.predict_proba(raw_data)
+    return prediction
