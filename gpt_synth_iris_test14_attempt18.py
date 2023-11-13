0503affea73428d123f03616e990ee90e132689e
philipmit <pschro@mit.edu>
2023-11-13 15:17:45 -0500

    Initial output: ``` from sklearn.datasets import load_iris from sklearn.model_selection import train_test_split from sklearn.preprocessing import StandardScaler from sklearn.neural_network import MLPClassifier import numpy as np
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Create a Multi-layer Perceptron model and train it
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    mlp.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(raw_data):
        processed_data = scaler.transform(np.array(raw_data).reshape(1, -1))
        return mlp.predict_proba(processed_data)
    ```
    This code loads the Iris dataset and splits it into training and testing sets. It then standardizes the features, creates a multi-layer perceptron (MLP) classifier, and trains the classifier on the training set. After that, a function called `predict_label` is defined, which uses the trained MLP model to predict the labels for a given data sample. The function takes a raw data sample as input, standardizes it using the same scaler fit on the training set, and then feeds it into the MLP model to find the predicted probabilities for the different labels. This function can be used to make predictions for new, unseen data samples.

---
+++ gpt_synth_iris_test14_attempt18.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Create a Multi-layer Perceptron model and train it
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+mlp.fit(X_train, y_train)
+# Define the predict_label function
+def predict_label(raw_data):
+    processed_data = scaler.transform(np.array(raw_data).reshape(1, -1))
+    return mlp.predict_proba(processed_data)
