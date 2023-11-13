7c68ddfaf85cc9c88de196d9d2fc57bde78eb761
philipmit <pschro@mit.edu>
2023-11-13 18:53:38 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error message you're receiving is due to a mismatch in the dimensions of "true_label_list" and "prediction_label_list". It looks like "predict_label" function is returning an array with fewer classes than there are in the actual test dataset ("y_test").
    
    The validation script is trying to calculate the AUC score for each class individually, using multi_class='ovr' this means we need as many predicted probabilities as there are classes in the test set.
    
    Let's revise the training function ensuring we have predictions for each of the 8 classes:
    
    ```
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    import pandas as pd
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Define predictor and target variables
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # replace class labels with numbers
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    # Convert to numpy arrays
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Scale the dataset
    sc = StandardScaler()
    X = sc.fit_transform(X)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create and train the logistic regression model with multi_class='multinomial'
    # Using the 'newton-cg' solver which can handle both l2 penalty and multinomial loss.
    model = LogisticRegression(multi_class='multinomial', solver='newton-cg')
    model.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Preprocess the raw data
        raw_data = np.array(raw_data).reshape(1, -1)
    
        # Since the data was trained on scaled data, we need to scale the new data
        raw_data = sc.transform(raw_data)
    
        # Get the predictions
        predicted_probabilities = model.predict_proba(raw_data)
    
        return predicted_probabilities[0]
    ```
    Now "predict_label" should return an array with 8 probabilities (one for each class), and the validation script should run without errors.

--- UCI_benchmarks/gpt_synth_ecoli_test_set02/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set02/test01.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -10,24 +11,23 @@ X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
 # replace class labels with numbers
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
-# convert to numpy arrays
+# Convert to numpy arrays
 X=X.to_numpy()
 y=y.to_numpy()
-# scale the dataset 
+# Scale the dataset 
 sc = StandardScaler()
 X = sc.fit_transform(X)
-# split the dataset into training and testing sets
-from sklearn.model_selection import train_test_split
+# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# create and train the logistic regression model
-model = LogisticRegression()
+# Create and train the logistic regression model with multi_class='multinomial' 
+# Using the 'newton-cg' solver which can handle both l2 penalty and multinomial loss.
+model = LogisticRegression(multi_class='multinomial', solver='newton-cg')
 model.fit(X_train, y_train)
 def predict_label(raw_data):
-    # preprocess the raw data
+    # Preprocess the raw data
     raw_data = np.array(raw_data).reshape(1, -1)
-    # since the data was trained on scaled data, we need to scale the new data
+    # Since the data was trained on scaled data, we need to scale the new data
     raw_data = sc.transform(raw_data)
-    # get the predictions
+    # Get the predictions
     predicted_probabilities = model.predict_proba(raw_data)
-    # return only the first row from the predicted probabilities, making it a 1D array
     return predicted_probabilities[0]
