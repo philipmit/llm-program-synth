916df5a567f53edff7ec1fd12234ed833d5e6917
philipmit <pschro@mit.edu>
2023-11-13 18:11:44 -0500

    Output 3 (phase 3): Response to improving AUC of 0.8891686674155554
    AUC from previous code:
    0.8891686674155554
    
    Response from GPT:
    ```
    from sklearn.datasets import load_digits
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    from sklearn.decomposition import PCA
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.pipeline import make_pipeline
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
    
    # Making a pipeline for preprocessing and model
    # Changing the classifier to RandomForest with more number of estimators
    # and more depth to capture complex features.
    pipeline = make_pipeline(
        StandardScaler(),
        PCA(n_components=40),
        RandomForestClassifier(n_estimators=500, max_depth=12, random_state=42))
    
    # Train the model
    pipeline.fit(X_train, y_train)
    
    def predict_icu_mortality(raw_data):
        # Reshape the raw_data
        reshaped_data = np.array(raw_data).reshape(1, -1)
    
        # Make a prediction using the trained model
        pred_probs = pipeline.predict_proba(reshaped_data)
    
        return pred_probs[0]
    ```
    The RandomForestClassifier model typically performs better than logistic regression on complex, non-linear data. Adjusting the parameters such as the number of estimators and depth may help improve accuracy by allowing the model to learn more complex data relationships. Additionally, reducing the number of components used in PCA might help the model focus on the most significant sources of variance, potentially improving performance.

--- gpt_synth_digits_test02_attempt08.py
+++ gpt_synth_digits_test02_attempt08.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 import numpy as np
 # Load the digits dataset
@@ -12,9 +12,13 @@ X = digits.data
 y = digits.target
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# Increasing the PCA components might help in improving the model performance
-# I have also switched the model to GradientBoostingClassifier which generally give more performance than RandomForest
-pipeline = make_pipeline(StandardScaler(), PCA(n_components=45), GradientBoostingClassifier(n_estimators=250, random_state = 42))
+# Making a pipeline for preprocessing and model
+# Changing the classifier to RandomForest with more number of estimators
+# and more depth to capture complex features.
+pipeline = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=40),
+    RandomForestClassifier(n_estimators=500, max_depth=12, random_state=42))
 # Train the model
 pipeline.fit(X_train, y_train)
 def predict_icu_mortality(raw_data):
