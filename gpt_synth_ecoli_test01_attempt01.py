f7494d6ca784db436a29cf9e009eccf22418e2a4
philipmit <pschro@mit.edu>
2023-11-13 17:56:14 -0500

    Output 12 (phase 3): Response to improving AUC of 0.8556738323410946
    AUC from previous code:
    0.8556738323410946
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.pipeline import make_pipeline
    
    # Load data
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                     delim_whitespace=True,
                     header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Separate features and target
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Convert string to num for labels
    unique_classes = pd.unique(y)
    class_dict = {value: index for index, value in enumerate(unique_classes)}
    y = y.replace(class_dict)
    
    # Split data into training and testing
    X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                        test_size=0.5,
                                                        random_state=42)
    
    # Change the logistic regression to random forest for better predictive power
    model = make_pipeline(StandardScaler(),
                          RandomForestClassifier(n_estimators=500,
                                                 random_state=42))
    
    model.fit(X_train, y_train)
    
    n_classes = len(unique_classes)
    
    def predict_label(data):
        data = np.reshape(data, (1, -1))
        # Create a template for all probabilities being zero
        proba_template = np.zeros(n_classes)
        # Get the model's predicted probabilities
        model_proba = model.predict_proba(data)[0]
        # Fill in the probabilities for classes that the model was trained on
        proba_template[model.classes_] = model_proba
        return proba_template
    ```
    The modification incorporated in this code is moving the model from a logistic regression to a RandomForest classifier with 500 trees, to increase the model's predictive power. Moreover, the data pipeline is also adjusted to include feature scaling, which is often beneficial for machine learning algorithms. Note that these are just some of the possible tweaks, other considerations could include feature selection/engineering, or employing more advanced models.

--- gpt_synth_ecoli_test01_attempt01.py
+++ gpt_synth_ecoli_test01_attempt01.py
@@ -3,30 +3,36 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+# Load data
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                  delim_whitespace=True, 
                  header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+# Separate features and target
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
+# Convert string to num for labels
 unique_classes = pd.unique(y)  
 class_dict = {value: index for index, value in enumerate(unique_classes)}
 y = y.replace(class_dict)
+# Split data into training and testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, 
                                                     test_size=0.5, 
                                                     random_state=42)
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-model = LogisticRegression(multi_class='ovr', solver='liblinear')
-model.fit(X_train_scaled, y_train)
+# Change the logistic regression to random forest for better predictive power
+model = make_pipeline(StandardScaler(),
+                      RandomForestClassifier(n_estimators=500, 
+                                             random_state=42))
+model.fit(X_train, y_train)
 n_classes = len(unique_classes)
 def predict_label(data):
     data = np.reshape(data, (1, -1))  
-    data_scaled = scaler.transform(data)  
     # Create a template for all probabilities being zero
     proba_template = np.zeros(n_classes)
     # Get the model's predicted probabilities
-    model_proba = model.predict_proba(data_scaled)[0]
+    model_proba = model.predict_proba(data)[0]
     # Fill in the probabilities for classes that the model was trained on
     proba_template[model.classes_] = model_proba
     return proba_template
