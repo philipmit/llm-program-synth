961b16efc8001c8ce0046f1a428d81fe779276e1
philipmit <pschro@mit.edu>
2023-11-13 17:44:02 -0500

    Output 3 (phase 3): Response to improving AUC of 0.8891686674155554
    AUC from previous code:
    0.8891686674155554
    
    Response from GPT:
    ```
    from sklearn.datasets import load_digits
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    from sklearn.decomposition import PCA
    from sklearn.pipeline import make_pipeline
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Normalize the data and apply Principal Component Analysis (PCA) for feature reduction before applying Logistic Regression
    pipeline = make_pipeline(StandardScaler(), PCA(n_components=30), LogisticRegression(C=0.1, max_iter=5000))
    
    # Train the model
    pipeline.fit(X_train, y_train)
    
    def predict_icu_mortality(raw_data):
        # Reshape the raw_data
        reshaped_data = np.array(raw_data).reshape(1, -1)
        # Make a prediction using the trained model
        pred_probs = pipeline.predict_proba(reshaped_data)
        return pred_probs[0]
    ```
    In this revised model, we apply normalization and Principal Component Analysis (PCA) to reduce dimensionality and remove noise from the dataset. We also lower the inverse regularization strength (C=0.1) to avoid overfitting. The max iteration number is also increased to ensure convergence when implementing Logistic Regression.

--- gpt_synth_digits_test02_attempt08.py
+++ gpt_synth_digits_test02_attempt08.py
@@ -1,6 +1,9 @@
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
 import numpy as np
 # Load the digits dataset
 digits = load_digits()
@@ -8,15 +11,13 @@ X = digits.data
 y = digits.target
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Train the model using a RandomForestClassifier
-# RandomForest model is chosen as it can handle a larger number of features and can prevent overfitting
-rf = RandomForestClassifier(n_estimators=2000, max_depth=15, random_state=42)
-rf.fit(X_train, y_train)
+# Normalize the data and apply Principal Component Analysis (PCA) for feature reduction before applying Logistic Regression
+pipeline = make_pipeline(StandardScaler(), PCA(n_components=30), LogisticRegression(C=0.1, max_iter=5000))
+# Train the model
+pipeline.fit(X_train, y_train)
 def predict_icu_mortality(raw_data):
     # Reshape the raw_data
     reshaped_data = np.array(raw_data).reshape(1, -1)
-    # Normalize the reshaped_data
-    normalized_data = reshaped_data / 16.0
     # Make a prediction using the trained model
-    pred_probs = rf.predict_proba(normalized_data)
+    pred_probs = pipeline.predict_proba(reshaped_data)
     return pred_probs[0]
