78b2714c26a15d5bd1932a4e6fe19f3b4b119720
philipmit <pschro@mit.edu>
2023-11-13 18:51:52 -0500

    Initial output: Here is the fully completed code:
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.neural_network import MLPClassifier
    from sklearn.preprocessing import StandardScaler
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize the standard scaler and fit on the training data
    scaler = StandardScaler()
    X_train_scaled = scaler.fit_transform(X_train)
    
    # Initialize Multi-Layer Perceptron Classifier.
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10),
                        max_iter=1000,
                        alpha=0.0001,
                        solver='sgd',
                        verbose=10,
                        random_state=21,
                        tol=0.000000001)
    
    # Train the Model
    mlp.fit(X_train_scaled, y_train)
    
    def predict_label(raw_sample):
        # Preprocess the raw unprocessed input data
        raw_sample_scaled = scaler.transform(raw_sample)
    
        # Predict and return the probabilities for the input data
        return mlp.predict_proba(raw_sample_scaled)
    ```
    
    This code sets up a Multi-Layer Perceptron network with the scikit-learn implementation of this architecture. The neural network contains 3 hidden layers, each with 10 neurons. We use Stochastic Gradient Descent ('sgd') as the solver for weight optimization.
    
    The fit method is then used to train our model with the scaled iris dataset.
    
    The `predict_label` function is defined to receive the raw unprocessed data, scales (preprocesses) it using the previously trained standard scaler and then feeds it into the model to get the predicted probabilities.
    
    Please note, you should make sure that `raw_sample` should have same features as training data and it should be in 2D array format as `StandardScaler().transform()` and `mlp.predict_proba()` functions need a 2D array.
    
    For instance, to predict probabilities for a single sample with 4 features, the input should look like this: `[[5.1, 3.5, 1.4, 0.2]]`.

---
+++ UCI_benchmarks/gpt_synth_iris_test_set01/test09.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Initialize the standard scaler and fit on the training data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+# Initialize Multi-Layer Perceptron Classifier.
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10),
+                    max_iter=1000,
+                    alpha=0.0001,
+                    solver='sgd',
+                    verbose=10,
+                    random_state=21,
+                    tol=0.000000001)
+# Train the Model
+mlp.fit(X_train_scaled, y_train)
+def predict_label(raw_sample):
+    # Preprocess the raw unprocessed input data
+    raw_sample_scaled = scaler.transform(raw_sample)
+    # Predict and return the probabilities for the input data
+    return mlp.predict_proba(raw_sample_scaled)
