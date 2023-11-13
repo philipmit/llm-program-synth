3e411153faa09aad67053ed50452c70dfce37d5b
philipmit <pschro@mit.edu>
2023-11-13 17:35:35 -0500

    Output 3 (phase 3): Response to improving AUC of 0.8904125036675945
    AUC from previous code:
    0.8904125036675945
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    
    ```
    from sklearn.datasets import load_digits
    from sklearn.model_selection import train_test_split
    from sklearn.ensemble import RandomForestClassifier
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the model using a RandomForestClassifier
    # RandomForest model is chosen as it can handle a larger number of features and can prevent overfitting
    rf = RandomForestClassifier(n_estimators=2000, max_depth=15, random_state=42)
    rf.fit(X_train, y_train)
    
    def predict_icu_mortality(raw_data):
        # Reshape the raw_data
        reshaped_data = np.array(raw_data).reshape(1, -1)
        # Normalize the reshaped_data
        normalized_data = reshaped_data / 16.0
    
        # Make a prediction using the trained model
        pred_probs = rf.predict_proba(normalized_data)
        return pred_probs[0]
    ```
    #####################################

--- gpt_synth_digits_test02_attempt08.py
+++ gpt_synth_digits_test02_attempt08.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 # Load the digits dataset
 digits = load_digits()
@@ -8,12 +8,15 @@ X = digits.data
 y = digits.target
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Train a Gradient Boosting Classifier with altered hyperparameters intending to improve the AUC score
-gb = GradientBoostingClassifier(n_estimators=1200, learning_rate=0.01, max_depth=5, random_state=42)
-gb.fit(X_train, y_train)
+# Train the model using a RandomForestClassifier
+# RandomForest model is chosen as it can handle a larger number of features and can prevent overfitting
+rf = RandomForestClassifier(n_estimators=2000, max_depth=15, random_state=42)
+rf.fit(X_train, y_train)
 def predict_icu_mortality(raw_data):
-    # Reshape and normalize raw_data
-    normalized_data = np.array(raw_data).reshape(1, -1) / 16.0
+    # Reshape the raw_data
+    reshaped_data = np.array(raw_data).reshape(1, -1)
+    # Normalize the reshaped_data
+    normalized_data = reshaped_data / 16.0
     # Make a prediction using the trained model
-    pred_probs = gb.predict_proba(normalized_data)
+    pred_probs = rf.predict_proba(normalized_data)
     return pred_probs[0]
