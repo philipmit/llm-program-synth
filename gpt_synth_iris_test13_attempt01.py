cc3f7453443f2b1eca098c8c0798016f4416c00b
philipmit <pschro@mit.edu>
2023-11-13 14:44:26 -0500

    Initial output: Here is the complete Python code that uses Multi-layer Perceptron classifier from the scikit-learn library to classify types of iris plants. The training is done on X_train and y_train as specified. Additionally it includes a function `predict_label` that takes raw input and returns the prediction from the trained model.
    ```
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Feature scaling
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    X_test = scaler.transform(X_test)
    
    # Create Multi-layer Perceptron classifier
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    
    # Train the classifier
    mlp.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Transform raw_data with the same scaler used in training
        scaled_data = scaler.transform([raw_data])
        # Predict label
        proba = mlp.predict_proba(scaled_data)
        return proba
    ```
    Please note that raw_data in the predict_label function is expected to be a list of features with length equal to the number of features in the original dataset (4 in case of the iris dataset). The output of predict_label gives you an array where each entry is the probability of the corresponding class given the input.

---
+++ gpt_synth_iris_test13_attempt01.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Feature scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+# Create Multi-layer Perceptron classifier
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+# Train the classifier
+mlp.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Transform raw_data with the same scaler used in training
+    scaled_data = scaler.transform([raw_data])
+    # Predict label
+    proba = mlp.predict_proba(scaled_data)
+    return proba
