1dbaa5551b7a5d20a63e58a59e2503bd6662c6bd
philipmit <pschro@mit.edu>
2023-11-13 15:07:56 -0500

    Initial output: Here is a complete code constructing an MLP model, training it using the 'Iris' dataset, and defining a 'predict_label' function.
    ```python
    # Required Libraries
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    import numpy as np
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Preprocess the data
    scaler = StandardScaler()
    X_train_scaled = scaler.fit_transform(X_train)
    
    # Initialize the MLP Classifier
    mlp = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
    
    # Train the model
    mlp.fit(X_train_scaled, y_train)
    
    def predict_label(data):
        # Preprocessing the single sample
        data_scaled = scaler.transform(data.reshape(1, -1))
    
        # Predict and return the probabilities
        return mlp.predict_proba(data_scaled)
    ```
    In the code above, we use the MLP (Multi Layer Perceptron) classifier from the Sklearn library. Before feeding the data into the MLP, it is common to scale the input data to have zero mean and unit variance for better performance.
    
    The "predict_label" function takes the raw data as input, preprocesses it using the scaler (which is fitted with the training data), and then predicts the label using the trained model. The function would return the predicted probabilities for that sample.

---
+++ gpt_synth_iris_test14_attempt08.py
@@ -0,0 +1,24 @@
+# Required Libraries
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Preprocess the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+# Initialize the MLP Classifier
+mlp = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
+# Train the model
+mlp.fit(X_train_scaled, y_train)
+def predict_label(data):
+    # Preprocessing the single sample
+    data_scaled = scaler.transform(data.reshape(1, -1))
+    # Predict and return the probabilities
+    return mlp.predict_proba(data_scaled)
