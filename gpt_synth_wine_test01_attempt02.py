c8d6228e236c2dba829f40fc4ba64df62dd6f765
philipmit <pschro@mit.edu>
2023-11-13 16:36:54 -0500

    Initial output: Here is the solution:
    ```python
    from sklearn.linear_model import LogisticRegression
    from sklearn.datasets import load_wine
    from sklearn.model_selection import train_test_split
    import numpy as np
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train a Logistic Regression model
    lr = LogisticRegression(max_iter=1000)
    lr.fit(X_train, y_train)
    
    def predict_label(data):
        # As logistic regression predicts for multiple samples simultaneously,
        # We put our single instance into an array in order to feed it to predict_proba
        data_reshaped = np.array(data).reshape(1, -1)
        prob_predictions = lr.predict_proba(data_reshaped)
        return prob_predictions
    ```
    This program first loads the Wine dataset and splits it into training and testing sets. It then trains a logistic regression model on the training data. The function `predict_label` is then defined to predict the probabilities of the output labels for a single given input sample.

---
+++ gpt_synth_wine_test01_attempt02.py
@@ -0,0 +1,19 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+import numpy as np
+# Load the wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train a Logistic Regression model
+lr = LogisticRegression(max_iter=1000)
+lr.fit(X_train, y_train)
+def predict_label(data):
+    # As logistic regression predicts for multiple samples simultaneously, 
+    # We put our single instance into an array in order to feed it to predict_proba
+    data_reshaped = np.array(data).reshape(1, -1)
+    prob_predictions = lr.predict_proba(data_reshaped)
+    return prob_predictions
