a4d712802e27d879548cab422553f856b8f037e6
philipmit <pschro@mit.edu>
2023-11-13 16:11:50 -0500

    Initial output:     ```     from sklearn.datasets import load_iris     from sklearn.model_selection import train_test_split     from sklearn.linear_model import LogisticRegression     from sklearn.preprocessing import StandardScaler
        # Load the Iris dataset
        iris = load_iris()
        X = iris.data
        y = iris.target
    
        # Split the dataset into training and testing sets
        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
        # Scale the data
        sc = StandardScaler()
        X_train_scaled = sc.fit_transform(X_train)
    
        # Train a logistic regression model on the training data
        lr = LogisticRegression(random_state=42)
        lr.fit(X_train_scaled, y_train)
    
        # Function to predict label for a single sample
        def predict_label(input_data):
            # Scale the input data using the same scaler as before
            input_data_scaled = sc.transform([input_data])
            # Use the logistic regression model to predict probabilities
            probabilities = lr.predict_proba(input_data_scaled)
            return probabilities
        ```

---
+++ gpt_synth_iris_test17_attempt20.py
@@ -0,0 +1,23 @@
+rom sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Scale the data
+sc = StandardScaler()
+X_train_scaled = sc.fit_transform(X_train)
+# Train a logistic regression model on the training data
+lr = LogisticRegression(random_state=42)
+lr.fit(X_train_scaled, y_train)
+# Function to predict label for a single sample
+def predict_label(input_data):
+    # Scale the input data using the same scaler as before
+    input_data_scaled = sc.transform([input_data])
+    # Use the logistic regression model to predict probabilities
+    probabilities = lr.predict_proba(input_data_scaled)
+    return probabilities
