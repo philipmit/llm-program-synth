6e8dd355b4c7c3fd642ec56cd7ba2d9b0b45f987
philipmit <pschro@mit.edu>
2023-11-13 18:46:21 -0500

    Initial output: ```python from sklearn.datasets import load_digits from sklearn.linear_model import LogisticRegression from sklearn.model_selection import train_test_split import numpy as np
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train a logistic regression model
    model = LogisticRegression(max_iter=10000)
    model.fit(X_train, y_train)
    
    def predict_label(sample):
      """
      This function takes raw unprocessed data for a single sample
      and returns the predicted probabilities for the 10 possible classes for that sample.
      """
      sample = np.array(sample).reshape(1, -1)
      return model.predict_proba(sample)
    ```

---
+++ UCI_benchmarks/gpt_synth_digits_test_set02/test12.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_digits
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train a logistic regression model
+model = LogisticRegression(max_iter=10000)
+model.fit(X_train, y_train)
+def predict_label(sample):
+  """
+  This function takes raw unprocessed data for a single sample 
+  and returns the predicted probabilities for the 10 possible classes for that sample.
+  """
+  sample = np.array(sample).reshape(1, -1)
+  return model.predict_proba(sample)
