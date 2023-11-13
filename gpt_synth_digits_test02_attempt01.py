ca370b4959f2be129dfb27cef43d1ae16af03e4e
philipmit <pschro@mit.edu>
2023-11-13 17:18:35 -0500

    Initial output: Here is the code that you need. It trains a logistic regression model and creates a `predict_label` function to make predictions.
    ```python
    from sklearn.datasets import load_digits
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create a logistic regression model
    model = LogisticRegression(multi_class='ovr', solver='liblinear')
    
    # Train the logistic regression model
    model.fit(X_train, y_train)
    
    def predict_label(x):
        # Reshape the input to be 2D as the model expects
        x = np.array(x).reshape(1, -1)
    
        # Return predicted probabilities
        return model.predict_proba(x)
    
    # Example usage:
    # prob = predict_label(X_train[0])
    # print(prob)
    ```
    
    Please note that the logistic regression model is expecting a two-dimensional array for its input. So, it's necessary to reshape the input sample if it's a one-dimensional array. This is why the `predict_label` function is reshaping the input to have 1 row and as many columns as necessary (-1).

---
+++ gpt_synth_digits_test02_attempt01.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Create a logistic regression model
+model = LogisticRegression(multi_class='ovr', solver='liblinear')
+# Train the logistic regression model
+model.fit(X_train, y_train)
+def predict_label(x):
+    # Reshape the input to be 2D as the model expects
+    x = np.array(x).reshape(1, -1)
+    # Return predicted probabilities
+    return model.predict_proba(x)
+# Example usage:
+# prob = predict_label(X_train[0])
+# print(prob)
