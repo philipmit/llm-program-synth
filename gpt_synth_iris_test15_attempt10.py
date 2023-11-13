e71fcdf71c2a5e9c7111dc18241291195a043af2
philipmit <pschro@mit.edu>
2023-11-13 15:45:04 -0500

    Initial output: Here is the complete program, with a logistic regression model being trained on the given Iris dataset and a predict function defined as per your requirements.
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
    
    # Standardize the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the model
    model = LogisticRegression(multi_class='ovr', solver='liblinear')
    model.fit(X_train, y_train)
    
    # Define prediction function
    def predict_label(raw_data):
        processed_data = scaler.transform(raw_data.reshape(1, -1))
        probabilities = model.predict_proba(processed_data)
        return probabilities
    ```
    
    In this code, Logistic Regression model has been trained on the iris dataset. The function `predict_label(raw_data: array)` takes raw data for an iris sample and return the predicted probabilities for the three different classes of iris flowers. The raw_data should be an array-like structure of size (4,), as there are four features in the iris dataset.

---
+++ gpt_synth_iris_test15_attempt10.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Train the model
+model = LogisticRegression(multi_class='ovr', solver='liblinear')
+model.fit(X_train, y_train)
+# Define prediction function
+def predict_label(raw_data):
+    processed_data = scaler.transform(raw_data.reshape(1, -1))
+    probabilities = model.predict_proba(processed_data)
+    return probabilities
