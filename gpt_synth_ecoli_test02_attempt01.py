bf65e3d368deb771ef7012c9bcc1a3e0bd7eec33
philipmit <pschro@mit.edu>
2023-11-13 18:09:54 -0500

    Output 10 (phase 3): Response to improving AUC of 0.9548933154170555
    AUC from previous code:
    0.9548933154170555
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.ensemble import GradientBoostingClassifier
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Get features X and label y
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # Replace strings with integer labels in y
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10,
                                                        random_state=42, stratify=y)
    # Standardizing the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    # Train the GradientBoostingClassifier model with increased number of estimators and learning rate
    gbModel = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.1,
                                         random_state=0, max_depth=3)
    gbModel.fit(X_train, y_train)
    def predict_icu_mortality(raw_data):
        raw_data = np.asarray(raw_data)
        # Standardize the features of the raw input data
        raw_data_scaled = scaler.transform(raw_data.reshape(1, -1))
        # Predict the probabilities of mortality
        prediction_proba = gbModel.predict_proba(raw_data_scaled)[0]
        return prediction_proba
    ```
    Modifications:
    * The RandomForestClassifier model has been replaced by boosting model GradientBoostingClassifier, which generally fares better in terms of prediction performance.
    * The number of estimators (n_estimators) in the GradientBoostingClassifier model has also been increased to 1000 for better learning and generalization.
    * The learning rate of the GradientBoostingClassifier model has been raised to 0.1. This might cause the model to learn the training data faster and improve the details of the dataset captured by the model.
    * More data is allocated to training (90%) rather than testing (10%), hoping to provide the model with richer information to learn from for improved predictions.

--- gpt_synth_ecoli_test02_attempt01.py
+++ gpt_synth_ecoli_test02_attempt01.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import GradientBoostingClassifier
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -14,19 +13,19 @@ y = ecoli.iloc[:, -1]   # All rows, only the last column
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, 
                                                     random_state=42, stratify=y)
 # Standardizing the features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Train the RandomForestClassifier model over Gradient Boosting for better performance
-rfModel = RandomForestClassifier(n_estimators=500, max_depth=None, 
-                                 min_samples_split=2, random_state=0)
-rfModel.fit(X_train, y_train)
+# Train the GradientBoostingClassifier model with increased number of estimators and learning rate
+gbModel = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.1, 
+                                     random_state=0, max_depth=3)
+gbModel.fit(X_train, y_train)
 def predict_icu_mortality(raw_data):
     raw_data = np.asarray(raw_data)
     # Standardize the features of the raw input data
     raw_data_scaled = scaler.transform(raw_data.reshape(1, -1))
     # Predict the probabilities of mortality
-    prediction_proba = rfModel.predict_proba(raw_data_scaled)[0]
+    prediction_proba = gbModel.predict_proba(raw_data_scaled)[0]
     return prediction_proba
