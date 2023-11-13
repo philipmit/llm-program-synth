108af25dd1ce10fd2e433e7e1a03ac2b5df11597
philipmit <pschro@mit.edu>
2023-11-13 16:49:34 -0500

    Initial output: Here is the complete code:
    ```python
    from sklearn.datasets import load_wine
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    import numpy as np
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Intialize and fit the logistic regression model
    lr = LogisticRegression(max_iter=10000)
    lr.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Reshape the data to have shape (1, -1)
        reshaped_data = np.array(raw_data).reshape(1, -1)
        # Return the probability of each class
        return lr.predict_proba(reshaped_data)
    ```
    
    This code defines a logistic regression model and fits it using the wine dataset. The `predict_label` function takes the raw data of a sample, reshapes it to match the expected input shape for the logistic regression model, and returns the predicted probabilities for the classes of the given sample.

---
+++ gpt_synth_wine_test01_attempt16.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Intialize and fit the logistic regression model
+lr = LogisticRegression(max_iter=10000)
+lr.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Reshape the data to have shape (1, -1)
+    reshaped_data = np.array(raw_data).reshape(1, -1)
+    # Return the probability of each class
+    return lr.predict_proba(reshaped_data)
