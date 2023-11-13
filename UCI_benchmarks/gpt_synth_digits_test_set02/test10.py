b6d1c5b8b20b37e493011e2682bfaa9450a06deb
philipmit <pschro@mit.edu>
2023-11-13 18:44:53 -0500

    Initial output: ```python from sklearn.datasets import load_digits from sklearn.model_selection import train_test_split from sklearn.linear_model import LogisticRegression import numpy as np
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    log_reg = LogisticRegression(max_iter=10000)
    log_reg.fit(X_train, y_train)
    
    # Function to predict probabilities for a single sample
    def predict_label(sample):
        if len(sample) != X_train.shape[1]:
            raise ValueError("Input data does not have the right shape. It should be ({},), but is {}.".format(X_train.shape[1], sample.shape))
    
        sample = np.reshape(sample, (1, -1))
        return log_reg.predict_proba(sample)
    ```

---
+++ UCI_benchmarks/gpt_synth_digits_test_set02/test10.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train the logistic regression model
+log_reg = LogisticRegression(max_iter=10000)
+log_reg.fit(X_train, y_train)
+# Function to predict probabilities for a single sample
+def predict_label(sample):
+    if len(sample) != X_train.shape[1]:
+        raise ValueError("Input data does not have the right shape. It should be ({},), but is {}.".format(X_train.shape[1], sample.shape))
+    sample = np.reshape(sample, (1, -1))
+    return log_reg.predict_proba(sample)
