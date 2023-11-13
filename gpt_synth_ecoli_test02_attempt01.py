ad390dedb5c454a8804a2fc1ba97ab49729bf91e
philipmit <pschro@mit.edu>
2023-11-13 17:57:41 -0500

    Initial output: ```python import pandas as pd import numpy as np from sklearn.model_selection import train_test_split from sklearn.linear_model import LogisticRegression from sklearn.preprocessing import StandardScaler
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Get features X and label y
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the Logistic Regression model
    lrModel = LogisticRegression(random_state=42, max_iter=5000)
    lrModel.fit(X_train, y_train)
    
    # Define the function predict_label
    def predict_label(raw_data):
        raw_data = np.array(raw_data)
        raw_data_scaled = scaler.transform(raw_data.reshape(1,-1))
        prediction = lrModel.predict_proba(raw_data_scaled)
        return prediction
    ```
    This script trains a logistic regression model using half of the Ecoli dataset. The trained model is then used by `predict_label` function that predicts the label given raw data. All preprocessing steps are also handled by the `predict_label` function. The output of function `predict_label` are probabilities for each label.

---
+++ gpt_synth_ecoli_test02_attempt01.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the Ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+# Get features X and label y
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# replace strings with numbers in y
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X=X.to_numpy()
+y=y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Train the Logistic Regression model
+lrModel = LogisticRegression(random_state=42, max_iter=5000)
+lrModel.fit(X_train, y_train)
+# Define the function predict_label
+def predict_label(raw_data):
+    raw_data = np.array(raw_data)
+    raw_data_scaled = scaler.transform(raw_data.reshape(1,-1))
+    prediction = lrModel.predict_proba(raw_data_scaled)
+    return prediction
