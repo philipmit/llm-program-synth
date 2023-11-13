979fc46134ff9a22c554cbea63b643b7279b7c59
philipmit <pschro@mit.edu>
2023-11-13 18:56:17 -0500

    Output 2 (phase 3): Response to improving AUC of 0.5
    AUC from previous code:
    0.5
    
    Response from GPT:
    ```
    from sklearn.datasets import load_breast_cancer
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Breast Cancer dataset
    breast_cancer = load_breast_cancer()
    X = breast_cancer.data
    y = breast_cancer.target
    
    # Feature Scaling
    sc = StandardScaler()
    X_sc = sc.fit_transform(X)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X_sc, y, test_size=0.5, random_state=42)
    
    # Utilize Logistic Regression which is the appropriate model for this problem
    # Use 'liblinear' solver for smaller datasets and L1 penalty for feature selection
    # Set C to a smaller value for stronger regularization to prevent overfitting
    model = LogisticRegression(penalty='l1', C=0.01, solver='liblinear')
    
    model.fit(X_train, y_train)
    
    # define the function
    def predict_icu_mortality(raw_data):
        raw_data = sc.transform([raw_data]) # Expecting 2D array-like input
        return model.predict_proba(raw_data)[0][1]
    ```

--- UCI_benchmarks/gpt_synth_breast_cancer_test_set01/test11.py
+++ UCI_benchmarks/gpt_synth_breast_cancer_test_set01/test11.py
@@ -1,33 +1,22 @@
 from sklearn.datasets import load_breast_cancer 
 from sklearn.model_selection import train_test_split 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler 
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 # Load the Breast Cancer dataset 
 breast_cancer = load_breast_cancer() 
 X = breast_cancer.data 
 y = breast_cancer.target 
 # Feature Scaling 
 sc = StandardScaler() 
-X = sc.fit_transform(X) 
+X_sc = sc.fit_transform(X)
 # Split the dataset into training and testing sets 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Define a Random Forest model, an ensemble method which uses multiple decision trees to improve the performance
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-# Define the grid of hyperparameters to search
-param_grid = { 
-    'n_estimators': [200, 500],
-    'max_features': ['auto', 'sqrt', 'log2'],
-    'max_depth' : [4,5,6,7,8],
-    'criterion' :['gini', 'entropy']
-}
-# Set up the grid search with 3-fold cross validation
-grid_cv = GridSearchCV(estimator=model, param_grid=param_grid, cv= 3)
-grid_cv.fit(X_train, y_train)
-# Train the optimized model
-model = grid_cv.best_estimator_
+X_train, X_test, y_train, y_test = train_test_split(X_sc, y, test_size=0.5, random_state=42)
+# Utilize Logistic Regression which is the appropriate model for this problem
+# Use 'liblinear' solver for smaller datasets and L1 penalty for feature selection
+# Set C to a smaller value for stronger regularization to prevent overfitting
+model = LogisticRegression(penalty='l1', C=0.01, solver='liblinear')
 model.fit(X_train, y_train)
-# Define the function
+# define the function
 def predict_icu_mortality(raw_data): 
-    raw_data = sc.transform([raw_data]) 
+    raw_data = sc.transform([raw_data]) # Expecting 2D array-like input
     return model.predict_proba(raw_data)[0][1]
