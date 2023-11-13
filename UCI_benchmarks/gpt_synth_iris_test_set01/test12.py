194fca4584d1c7e0f1a03678808318359dd1d648
philipmit <pschro@mit.edu>
2023-11-13 18:55:03 -0500

    Initial output: Here is your requested code:
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Normalize the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Build and train the multi-layer perceptron model
    model = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000, random_state=42)
    model.fit(X_train, y_train)
    
    def predict_label(X):
        # The input should be preprocessed in the same way as the training data
        X = scaler.transform([X])
    
        # Predict the probabilities for each class
        probabilities = model.predict_proba(X)
    
        return probabilities
    ```
    
    This version of `predict_label` expects the input to be a list of four values which correspond to the four features in the Iris dataset. The function first preprocesses the input in the same way as the training dataset, then it generates the predicted probabilities for each class using the model. It returns a list of probabilities. The position of each probability in the list corresponds to the class with the same index in the `iris.target_names` array.

---
+++ UCI_benchmarks/gpt_synth_iris_test_set01/test12.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Normalize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Build and train the multi-layer perceptron model
+model = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000, random_state=42)
+model.fit(X_train, y_train)
+def predict_label(X):
+    # The input should be preprocessed in the same way as the training data
+    X = scaler.transform([X])
+    # Predict the probabilities for each class
+    probabilities = model.predict_proba(X)
+    return probabilities
