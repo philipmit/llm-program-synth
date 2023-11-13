06218ae55c503ecdb65d92f8d710fe537e04c9ae
philipmit <pschro@mit.edu>
2023-11-13 15:08:59 -0500

    Initial output: Here's the complete code:
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Normalization of data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the Multi-layer Perceptron
    clf = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000, random_state=42)
    clf.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Transform raw_data using the previously declared scaler
        raw_data = scaler.transform(raw_data.reshape(1, -1))
    
        # return probabilities using the trained MLP classifier
        return clf.predict_proba(raw_data)
    ```
    
    This code will train an MLP classifier with the iris dataset, and the `predict_label` function will return the predicted probabilities for a given input. Note that using this function expects a 1-D numpy array with the features of the sample. If the input isn't normalized or if the shape isn't correct, it might cause issues.

---
+++ gpt_synth_iris_test14_attempt09.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Normalization of data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Train the Multi-layer Perceptron
+clf = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000, random_state=42)
+clf.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Transform raw_data using the previously declared scaler
+    raw_data = scaler.transform(raw_data.reshape(1, -1))
+    # return probabilities using the trained MLP classifier
+    return clf.predict_proba(raw_data)
