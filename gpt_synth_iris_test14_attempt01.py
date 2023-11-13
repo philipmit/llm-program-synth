75ca60c0a443a8376282dab5b1dcabdbb8d16283
philipmit <pschro@mit.edu>
2023-11-13 15:00:33 -0500

    Initial output: Here's how you might solve this problem:
    ```python
    import numpy as np
    from sklearn.datasets import load_iris
    from sklearn.neural_network import MLPClassifier
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features to help with MLP training
    scaler = StandardScaler()
    
    # Fit only on training data
    scaler.fit(X_train)
    
    # Apply transform to both the training and the testing set
    X_train = scaler.transform(X_train)
    
    # Initialize the multi-layer perceptron classifier
    mlp = MLPClassifier(hidden_layer_sizes=(10,10),
                        max_iter=1000,
                        alpha=1e-4,
                        solver='sgd',
                        verbose=10,
                        tol=1e-4,
                        random_state=1,
                        learning_rate_init=.1)
    
    # Train the model
    mlp.fit(X_train, y_train)
    
    # Define the prediction function
    def predict_label(x):
        x = np.array(x).reshape(1, -1)
        x = scaler.transform(x)
        return mlp.predict_proba(x)
    ```
    
    This code:
    
    1. Loads the iris dataset.
    2. Splits the dataset into training and testing sets.
    3. Standardizes the features to help with MLP training.
    4. Trains an MLP classifier using the iris training data.
    5. Defines a function called `predict_label` that accepts raw, unprocessed Iris data for a single sample and outputs MLP's predicted probabilities for that sample.
    
    Note that the `predict_label` function uses the same transformation that was fit on the training data prior to making predictions.

---
+++ gpt_synth_iris_test14_attempt01.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the features to help with MLP training
+scaler = StandardScaler()
+# Fit only on training data
+scaler.fit(X_train)
+# Apply transform to both the training and the testing set
+X_train = scaler.transform(X_train)
+# Initialize the multi-layer perceptron classifier
+mlp = MLPClassifier(hidden_layer_sizes=(10,10), 
+                    max_iter=1000, 
+                    alpha=1e-4, 
+                    solver='sgd', 
+                    verbose=10, 
+                    tol=1e-4, 
+                    random_state=1, 
+                    learning_rate_init=.1)
+# Train the model
+mlp.fit(X_train, y_train)
+# Define the prediction function
+def predict_label(x):
+    x = np.array(x).reshape(1, -1)
+    x = scaler.transform(x)
+    return mlp.predict_proba(x)
