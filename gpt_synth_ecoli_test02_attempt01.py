40250605b8db95b29bca5c553d9e2784ca32fe6e
philipmit <pschro@mit.edu>
2023-11-13 18:04:33 -0500

    Output 10 (phase 3): Response to improving AUC of 0.9591326210654094
    AUC from previous code:
    0.9591326210654094
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.model_selection import train_test_split
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Get features X and label y
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # Replace strings with integer labels in y
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    # Split the dataset into training and testing sets using stratify to balance the classes
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
    # Standardizing the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    # Train the RandomForestClassifier model
    rfModel = RandomForestClassifier(random_state=42, n_estimators=1000)
    rfModel.fit(X_train, y_train)
    # Define the function predict_label to return probabilities for all classes
    def predict_label(raw_data):
        raw_data = np.array(raw_data)
        raw_data_scaled = scaler.transform(raw_data.reshape(1, -1))
        prediction_proba = rfModel.predict_proba(raw_data_scaled)[0]
        return prediction_proba.tolist()
    ```
    
    In the code provided above, we made use of `RandomForestClassifier` instead of `LogisticRegression`. Random Forests generally have a higher performance and accuracy over logistic regression as it avoids overfitting most of the time. Increasing the number of trees in the forest (n_estimators) can also lead to model improvement up to a certain point. Furthermore, Random Forests handle well with a high number of input features without variable deletion, providing two levels of randomness (bootstrap of data + feature randomness) and supplying an in-built method for balancing errors in case of unbalanced data-sets.

--- gpt_synth_ecoli_test02_attempt01.py
+++ gpt_synth_ecoli_test02_attempt01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 # Load the Ecoli dataset
@@ -17,12 +17,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Standardizing the features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Train the Logistic Regression model with multinomial setting for multi-class
-lrModel = LogisticRegression(random_state=42, max_iter=5000, multi_class='multinomial')
-lrModel.fit(X_train, y_train)
+# Train the RandomForestClassifier model
+rfModel = RandomForestClassifier(random_state=42, n_estimators=1000)
+rfModel.fit(X_train, y_train)
 # Define the function predict_label to return probabilities for all classes
 def predict_label(raw_data):
     raw_data = np.array(raw_data)
     raw_data_scaled = scaler.transform(raw_data.reshape(1, -1))
-    prediction_proba = lrModel.predict_proba(raw_data_scaled)[0]
+    prediction_proba = rfModel.predict_proba(raw_data_scaled)[0]
     return prediction_proba.tolist()
