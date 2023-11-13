7d5acdbd1a87994de54441e3a5906bda1c08ebea
philipmit <pschro@mit.edu>
2023-11-13 14:46:25 -0500

    Initial output: ```python from sklearn.datasets import load_iris from sklearn.model_selection import train_test_split from sklearn.preprocessing import StandardScaler from sklearn.neural_network import MLPClassifier import numpy as np
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize features by removing the mean and scaling to unit variance
    scaler = StandardScaler()
    scaler.fit(X_train)
    
    X_train = scaler.transform(X_train)
    
    # Training the MLPClassifier
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    mlp.fit(X_train, y_train)
    
    # Define function to predict label for a single sample
    def predict_label(raw_data):
        # standardize the raw_data
        raw_data = scaler.transform(np.array(raw_data).reshape(1,-1))
    
        # return predicted probabilities
        return mlp.predict_proba(raw_data)
    ```

---
+++ gpt_synth_iris_test13_attempt03.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize features by removing the mean and scaling to unit variance
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+# Training the MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+mlp.fit(X_train, y_train)
+# Define function to predict label for a single sample
+def predict_label(raw_data):
+    # standardize the raw_data
+    raw_data = scaler.transform(np.array(raw_data).reshape(1,-1))
+    # return predicted probabilities
+    return mlp.predict_proba(raw_data)
