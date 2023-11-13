5d532e4db33bb80f320f7ee593ee3d674d166ad8
philipmit <pschro@mit.edu>
2023-11-13 16:37:17 -0500

    Output 2 (phase 3): Response to improving AUC of 0.9633035032194696
    AUC from previous code:
    0.9633035032194696
    
    Response from GPT:
    Response 2 from GPT:
    ```python
    from sklearn.datasets import load_breast_cancer
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.model_selection import GridSearchCV
    from sklearn.preprocessing import StandardScaler
    import numpy as np
    
    # Load the Breast Cancer dataset
    breast_cancer = load_breast_cancer()
    X = breast_cancer.data
    y = breast_cancer.target
    
    # Feature scaling for improved performance
    scaler = StandardScaler()
    X = scaler.fit_transform(X)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create a Random Forest Classifier and parameters for Grid Search
    rf = RandomForestClassifier(random_state=42)
    param_grid = [
        {'n_estimators': [50, 100, 200], 'max_depth' : [5, 10, 15], 'min_samples_split' : [2, 5, 10]}
    ]
    
    # Grid Search to find the best hyperparameters
    clf = GridSearchCV(rf, param_grid, cv=5, scoring='roc_auc')
    clf.fit(X_train, y_train)
    
    def predict_label(single_sample):
        sample = scaler.transform(np.reshape(single_sample, (1, -1)))  # Rescale and reshape data
        prob = clf.predict_proba(sample)[0][1]  # Get the probability of label 1
        return prob
    ```
    This code swaps out the Logistic Regression model for a more complex Random Forest model. The hyperparameters of the model are optimized using GridSearchCV to maximize the Area Under the ROC Curve (AUC) on the training data. The function predict_label has been modified to apply the same scaling to the single_sample input that was applied to the training data.

--- gpt_synth_breast_cancer_test02_attempt20.py
+++ gpt_synth_breast_cancer_test02_attempt20.py
@@ -1,17 +1,28 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 # Load the Breast Cancer dataset
 breast_cancer = load_breast_cancer()
 X = breast_cancer.data
 y = breast_cancer.target
+# Feature scaling for improved performance
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Train a logistic regression model 
-log_reg = LogisticRegression(max_iter=10000)
-log_reg.fit(X_train, y_train)
+# Create a Random Forest Classifier and parameters for Grid Search
+rf = RandomForestClassifier(random_state=42)
+param_grid = [
+    {'n_estimators': [50, 100, 200], 'max_depth' : [5, 10, 15], 'min_samples_split' : [2, 5, 10]}
+]
+# Grid Search to find the best hyperparameters
+clf = GridSearchCV(rf, param_grid, cv=5, scoring='roc_auc')
+clf.fit(X_train, y_train)
 def predict_label(single_sample):
-    sample = np.reshape(single_sample, (1, -1))  # Reshape data
-    prob = log_reg.predict_proba(sample)[0][1]  # Get the probability of label 1
+    sample = scaler.transform(np.reshape(single_sample, (1, -1)))  # Rescale and reshape data
+    prob = clf.predict_proba(sample)[0][1]  # Get the probability of label 1
     return prob
