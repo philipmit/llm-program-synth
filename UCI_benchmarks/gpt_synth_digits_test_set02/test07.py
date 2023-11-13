afa3af35abac2e989ed1203fb339a876631e4694
philipmit <pschro@mit.edu>
2023-11-13 18:42:23 -0500

    Initial output: Here's the final program:
    ```python
    from sklearn.datasets import load_digits
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    model = LogisticRegression(max_iter=10000)
    model.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(raw_data):
        processed_data = np.array(raw_data).reshape(1, -1)
        probabilities = model.predict_proba(processed_data)
        return probabilities
    ```
    This program is using the Logistic Regression model from the scikit-learn package to predict the labels of the Digits dataset.
    After training the model using the training set, it defines a function called 'predict_label' that takes raw, unprocessed data for one sample as an input and returns the predicted probabilities for that sample produced by the trained model. The input to the function is reshaped into a 2D array since the trained model's prediction functions always expect input in that form, even when it is just one sample.

---
+++ UCI_benchmarks/gpt_synth_digits_test_set02/test07.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train the logistic regression model
+model = LogisticRegression(max_iter=10000)
+model.fit(X_train, y_train)
+# Define the predict_label function
+def predict_label(raw_data):
+    processed_data = np.array(raw_data).reshape(1, -1)
+    probabilities = model.predict_proba(processed_data)
+    return probabilities
