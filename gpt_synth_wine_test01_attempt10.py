3ae90a074fb4cd13fd7b8e791b9d80ca39aa342c
philipmit <pschro@mit.edu>
2023-11-13 16:44:38 -0500

    Initial output: Here's the complete Python code for training a logistic regression model and making predictions on the Wine dataset.
    ```python
    from sklearn.datasets import load_wine
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Perform feature scaling on the training set
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the logistic regression model
    model = LogisticRegression()
    model.fit(X_train, y_train)
    
    # Define a function that makes predictions on unprocessed data
    def predict_label(raw_data):
        raw_data = raw_data.reshape(1, -1) # reshape to 1 sample
        processed_data = scaler.transform(raw_data) # apply the same transformations used on the training data
        probabilities = model.predict_proba(processed_data) # get probabilities
        return probabilities
    ```
    
    The function `predict_label` expects a raw unprocessed array of features for a single sample and returns the predicted probabilities for that sample.

---
+++ gpt_synth_wine_test01_attempt10.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Perform feature scaling on the training set
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Train the logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+# Define a function that makes predictions on unprocessed data
+def predict_label(raw_data):
+    raw_data = raw_data.reshape(1, -1) # reshape to 1 sample 
+    processed_data = scaler.transform(raw_data) # apply the same transformations used on the training data
+    probabilities = model.predict_proba(processed_data) # get probabilities
+    return probabilities
