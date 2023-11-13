7e9d50d0c5c49d82bdee817106fb2386cbac3f1c
philipmit <pschro@mit.edu>
2023-11-13 18:58:34 -0500

    Initial output: Sure, here's the Python code that includes model training and the function to predict probabilities on a new instance. You start by splitting your data, then you're training your logistic regression model and finally defining your prediction function:
    ```python
    from sklearn.datasets import load_wine
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    import numpy as np
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    model = LogisticRegression(max_iter=1000)
    model.fit(X_train, y_train)
    
    def predict_label(X_raw):
        X_raw = np.array(X_raw).reshape(1, -1)
        probabilities = model.predict_proba(X_raw)
        return probabilities
    ```
    
    The `predict_label` function expects the raw, unprocessed data for a single feature vector as input, which it first reshapes to be compatible with the `predict_proba` method, then returns the predicted probabilities.

---
+++ UCI_benchmarks/gpt_synth_wine_test_set01/test14.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_wine
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
+# Load the wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train the logistic regression model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+def predict_label(X_raw):
+    X_raw = np.array(X_raw).reshape(1, -1)
+    probabilities = model.predict_proba(X_raw)
+    return probabilities
