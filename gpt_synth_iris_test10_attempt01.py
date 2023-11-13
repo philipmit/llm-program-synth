170387eb27d420b6cfdc5c5cc1a2bc6e6e9642ef
philipmit <pschro@mit.edu>
2023-11-13 13:39:32 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 21, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 693, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    ```python
    from sklearn.neural_network import MLPClassifier
    from sklearn.preprocessing import StandardScaler
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    import numpy as np
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
    
    # Standardize the features
    scaler = StandardScaler()
    X_train_s = scaler.fit_transform(X_train)
    
    # Define and train the multi-layer perceptron
    mlp = MLPClassifier(hidden_layer_sizes=(10,10), max_iter=1000, probability=True)
    mlp.fit(X_train_s, y_train)
    
    def predict_label(raw_data):
        scaled_data = scaler.transform(np.array(raw_data).reshape(1, -1))
        probability = mlp.predict_proba(scaled_data)[0]
        return probability
    ```

--- gpt_synth_iris_test10_attempt01.py
+++ gpt_synth_iris_test10_attempt01.py
@@ -12,11 +12,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Standardize the features
 scaler = StandardScaler()
 X_train_s = scaler.fit_transform(X_train)
-X_test_s = scaler.transform(X_test)
 # Define and train the multi-layer perceptron
-mlp = MLPClassifier(hidden_layer_sizes=(10,10), max_iter=1000)
+mlp = MLPClassifier(hidden_layer_sizes=(10,10), max_iter=1000, probability=True)
 mlp.fit(X_train_s, y_train)
 def predict_label(raw_data):
     scaled_data = scaler.transform(np.array(raw_data).reshape(1, -1))
-    prediction = mlp.predict(scaled_data)
-    return prediction
+    probability = mlp.predict_proba(scaled_data)[0]
+    return probability
