3ac1ca7a29040b30323397444657a1f5177e935a
philipmit <pschro@mit.edu>
2023-11-13 18:49:08 -0500

    Initial output: Here is the full Python code that uses the multi-layer perceptron model from Sklearn.
    ```python
    import numpy as np
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.neural_network import MLPClassifier
    from sklearn.preprocessing import StandardScaler
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize StandardScaler
    scaler = StandardScaler()
    
    # Fit on the training set
    scaler.fit(X_train)
    
    # Apply transform to both the training set and the test set
    X_train = scaler.transform(X_train)
    
    # Create an instance of MLPClassifier
    mlp = MLPClassifier(hidden_layer_sizes=(10,10,10),max_iter=1000)
    
    # Train the model
    mlp.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(raw_data):
        # Apply the same scaler to the raw data
        scaled_data = scaler.transform(raw_data.reshape(1, -1))
    
        # Predict probabilities
        probs = mlp.predict_proba(scaled_data)
    
        return probs
    ```
    The 'predict_label' function takes a 1D numpy array as input and returns the predicted probabilities for each class for the given input. It's important to note that 'raw_data' should have the same number of features as the data used to train the model. Also note that 'StandardScaler' has been used to scale the input data because MLPs often benefit from standardizing the input data. It helps in faster convergence.

---
+++ UCI_benchmarks/gpt_synth_iris_test_set01/test08.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Initialize StandardScaler
+scaler = StandardScaler()
+# Fit on the training set
+scaler.fit(X_train)
+# Apply transform to both the training set and the test set
+X_train = scaler.transform(X_train)
+# Create an instance of MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(10,10,10),max_iter=1000)
+# Train the model
+mlp.fit(X_train, y_train)
+# Define the predict_label function
+def predict_label(raw_data):
+    # Apply the same scaler to the raw data
+    scaled_data = scaler.transform(raw_data.reshape(1, -1))
+    # Predict probabilities
+    probs = mlp.predict_proba(scaled_data)
+    return probs
