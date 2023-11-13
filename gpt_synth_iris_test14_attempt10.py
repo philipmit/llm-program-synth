322633f3521232436263d00394a22ae6e4c4cb95
philipmit <pschro@mit.edu>
2023-11-13 15:09:54 -0500

    Initial output: ```python from sklearn.datasets import load_iris from sklearn.model_selection import train_test_split from sklearn.neural_network import MLPClassifier from sklearn.preprocessing import StandardScaler
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    X_test = sc.transform(X_test)
    
    # Create an instance of the MLPClassifier
    mlp = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000)
    
    # Train the model
    mlp.fit(X_train, y_train)
    
    # Define the prediction function
    def predict_label(raw_data):
        raw_data = sc.transform([raw_data])
        pred_proba = mlp.predict_proba(raw_data)
        return pred_proba
    ```

---
+++ gpt_synth_iris_test14_attempt10.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the features
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# Create an instance of the MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(10,), max_iter=1000)
+# Train the model
+mlp.fit(X_train, y_train)
+# Define the prediction function
+def predict_label(raw_data):
+    raw_data = sc.transform([raw_data])
+    pred_proba = mlp.predict_proba(raw_data)
+    return pred_proba
