a402d11c9065d696289ace627f05a3426cb7637d
philipmit <pschro@mit.edu>
2023-11-13 17:23:26 -0500

    Initial output: Here's the full code to load the dataset, train a logistic regression model on `X_train` and `y_train`, and finally a function `predict_label` that takes in a 1D array representing a single digit image and predicts the probabilities of each digit (0, 1, 2, ..., 9):
    ```python
    from sklearn.datasets import load_digits
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train a logistic regression model
    lr = LogisticRegression(max_iter=5000)
    lr.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Reshape and normalize raw_data to fit the trained model
        normalized_data = np.array(raw_data).reshape(1, -1) / 16.0
    
        # Make prediction using trained model and return predicted probabilities
        pred_probs = lr.predict_proba(normalized_data)
        return pred_probs
    ```
    
    The `predict_label` function takes in a 1D array (raw_data) representing the raw pixel intensities of 64 pixels of a digit image. It first normalizes the raw_data by dividing it by 16 (since the raw pixel intensities are in the range 0-16) before making a prediction. The function returns a 1D array of 10 probabilities, corresponding to each digit from 0 to 9.

---
+++ gpt_synth_digits_test02_attempt08.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_digits
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train a logistic regression model
+lr = LogisticRegression(max_iter=5000)
+lr.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Reshape and normalize raw_data to fit the trained model
+    normalized_data = np.array(raw_data).reshape(1, -1) / 16.0
+    # Make prediction using trained model and return predicted probabilities
+    pred_probs = lr.predict_proba(normalized_data)
+    return pred_probs
