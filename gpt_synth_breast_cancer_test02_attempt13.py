d36eca3b9426d513f572ce093e5fcca237d1cce6
philipmit <pschro@mit.edu>
2023-11-13 16:33:15 -0500

    Initial output: ```python from sklearn.datasets import load_breast_cancer from sklearn.model_selection import train_test_split from sklearn.linear_model import LogisticRegression from sklearn.preprocessing import StandardScaler
    # Load the Breast Cancer dataset
    breast_cancer = load_breast_cancer()
    X = breast_cancer.data
    y = breast_cancer.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Fit the Logistic Regression model
    model = LogisticRegression()
    model.fit(X_train, y_train)
    
    def predict_label(raw_data):
        processed_data = scaler.transform(raw_data.reshape(1, -1))         # reshape and standardize the data
        return model.predict_proba(processed_data)[0][1]
    ```

---
+++ gpt_synth_breast_cancer_test02_attempt13.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the Breast Cancer dataset
+breast_cancer = load_breast_cancer()
+X = breast_cancer.data
+y = breast_cancer.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Fit the Logistic Regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+def predict_label(raw_data):
+    processed_data = scaler.transform(raw_data.reshape(1, -1))         # reshape and standardize the data
+    return model.predict_proba(processed_data)[0][1]
