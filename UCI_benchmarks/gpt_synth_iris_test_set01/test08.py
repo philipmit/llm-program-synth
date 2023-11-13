ae71102edb805428fcb408a16f1e9389bbd97bcc
philipmit <pschro@mit.edu>
2023-11-13 18:50:11 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 21, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 605, in roc_auc_score
        y_score = check_array(y_score, ensure_2d=False)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 953, in check_array
        raise ValueError(
    <class 'ValueError'>
    Found array with dim 3. None expected <= 2.
    
    Response from GPT:
    The error is happening because the `predict_label` function is returning a 2D array, while the roc_auc_score function expects a 1D array. We can fix this by selecting the index corresponding to the highest probability in the `predict_label` function.
    
    Here's the corrected GPT code:
    
    ```
    import numpy as np
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.neural_network import MLPClassifier
    from sklearn.preprocessing import StandardScaler
    
    iris = load_iris()
    X = iris.data
    y = iris.target
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize StandardScaler
    scaler = StandardScaler()
    scaler.fit(X_train)
    
    # Apply transform to both the training set and the test set
    X_train_std = scaler.transform(X_train)
    
    # Create an instance of MLPClassifier
    mlp = MLPClassifier(hidden_layer_sizes=(10,10,10),max_iter=1000)
    mlp.fit(X_train_std, y_train)
    
    def predict_label(raw_data):
        scaled_data = scaler.transform(raw_data.reshape(1, -1))
        # Retrive the label with highest probability
        return np.argmax(mlp.predict_proba(scaled_data), axis=1)
    ```

--- UCI_benchmarks/gpt_synth_iris_test_set01/test08.py
+++ UCI_benchmarks/gpt_synth_iris_test_set01/test08.py
@@ -3,25 +3,19 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
-# Load the Iris dataset
 iris = load_iris()
 X = iris.data
 y = iris.target
-# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Initialize StandardScaler
 scaler = StandardScaler()
-# Fit on the training set
 scaler.fit(X_train)
 # Apply transform to both the training set and the test set
-X_train = scaler.transform(X_train)
+X_train_std = scaler.transform(X_train)
 # Create an instance of MLPClassifier
 mlp = MLPClassifier(hidden_layer_sizes=(10,10,10),max_iter=1000)
-# Train the model
-mlp.fit(X_train, y_train)
-# Define the predict_label function
+mlp.fit(X_train_std, y_train)
 def predict_label(raw_data):
-    # Apply the same scaler to the raw data
     scaled_data = scaler.transform(raw_data.reshape(1, -1))
-    # Predict and return probabilities
-    return mlp.predict_proba(scaled_data)
+    # Retrive the label with highest probability
+    return np.argmax(mlp.predict_proba(scaled_data), axis=1)
