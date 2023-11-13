4f167fdb74a74bccf159982f13cec7c468178f65
philipmit <pschro@mit.edu>
2023-11-13 16:52:41 -0500

    Output 2 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 21, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 605, in roc_auc_score
        y_score = check_array(y_score, ensure_2d=False)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 953, in check_array
        raise ValueError(
    <class 'ValueError'>
    Found array with dim 3. None expected <= 2.
    
    Response from GPT:
    ```
    from sklearn.datasets import load_wine
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Normalize the features for better prediction
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    # Apply Logistic Regression
    lr = LogisticRegression(multi_class='ovr', solver='liblinear')
    lr.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Normalize the raw_data before prediction
        normalized_data = sc.transform([raw_data])
    
        # Predict the class probabilities
        # adjust the shape of predicted probabilities to 1D array for roc_auc_score function
        return lr.predict_proba(normalized_data)[0]
    ```

--- gpt_synth_wine_test01_attempt19.py
+++ gpt_synth_wine_test01_attempt19.py
@@ -8,12 +8,15 @@ X = wine.data
 y = wine.target
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# normalize the features for better prediction
+# Normalize the features for better prediction
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 # Apply Logistic Regression
 lr = LogisticRegression(multi_class='ovr', solver='liblinear')
 lr.fit(X_train, y_train)
 def predict_label(raw_data):
+    # Normalize the raw_data before prediction
     normalized_data = sc.transform([raw_data])
-    return lr.predict_proba(normalized_data)
+    # Predict the class probabilities
+    # adjust the shape of predicted probabilities to 1D array for roc_auc_score function
+    return lr.predict_proba(normalized_data)[0]
