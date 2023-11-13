77c052b9910aef1972027067ea9215cc1ca3e4b0
philipmit <pschro@mit.edu>
2023-11-13 15:12:39 -0500

    Initial output: ```python from sklearn.datasets import load_iris from sklearn.model_selection import train_test_split from sklearn.preprocessing import StandardScaler from sklearn.neural_network import MLPClassifier
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize features by removing the mean and scaling to unit variance
    scaler = StandardScaler()
    scaler.fit(X_train)
    
    X_train = scaler.transform(X_train)
    
    # Defining the Multi-Layer Perceptron Classifier
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    
    # Training the model
    mlp.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Preprocessing the raw_data using the same scaler used on training data
        raw_scaled = scaler.transform([raw_data])
        predicted_probs = mlp.predict_proba(raw_scaled)
        return predicted_probs
    
    ```

---
+++ gpt_synth_iris_test14_attempt13.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize features by removing the mean and scaling to unit variance
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+# Defining the Multi-Layer Perceptron Classifier
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+# Training the model
+mlp.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Preprocessing the raw_data using the same scaler used on training data
+    raw_scaled = scaler.transform([raw_data])
+    predicted_probs = mlp.predict_proba(raw_scaled)
+    return predicted_probs
