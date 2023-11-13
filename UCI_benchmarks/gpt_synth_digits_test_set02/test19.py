d65b4ab793e67f4ffbb04637374e2e352eeb11d1
philipmit <pschro@mit.edu>
2023-11-13 18:53:08 -0500

    Initial output: Here's your logistic regression classifier written using sklearn. The logistic regression model is trained using the training set, and then a function 'predict_label' is defined to return the predicted probabilities for a provided input sample.
    ```python
    from sklearn.datasets import load_digits
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    import numpy as np
    
    # Load the digits dataset
    digits = load_digits()
    X = digits.data
    y = digits.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train a Logistic Regression model
    model = LogisticRegression(max_iter=5000)
    model.fit(X_train, y_train)
    
    # Function to predict the probabilities for a single sample
    def predict_label(single_sample):
        # Using model.predict_proba to get the predicted probabilities
        predicted_probabilities = model.predict_proba(single_sample.reshape(1, -1))
        return predicted_probabilities
    ```
    Please note that since sklearn needs 2D array for prediction, we are reshaping the input in predict_label function to make it a 2D array and Logistic Regression can be slow to converge, hence the max_iter is set to 5000.

---
+++ UCI_benchmarks/gpt_synth_digits_test_set02/test19.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+# Load the digits dataset
+digits = load_digits()
+X = digits.data
+y = digits.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train a Logistic Regression model
+model = LogisticRegression(max_iter=5000)
+model.fit(X_train, y_train)
+# Function to predict the probabilities for a single sample
+def predict_label(single_sample):
+    # Using model.predict_proba to get the predicted probabilities
+    predicted_probabilities = model.predict_proba(single_sample.reshape(1, -1))
+    return predicted_probabilities
