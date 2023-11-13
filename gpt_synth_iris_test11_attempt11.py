a1e2d4a43638e447a1cc1b6b4668264558cddc26
philipmit <pschro@mit.edu>
2023-11-13 14:17:52 -0500

    Initial output: Here's the final code solution with the specified function:
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.neural_network import MLPClassifier
    import numpy as np
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
    
    # Define the multi-layer perceptron model
    mlp = MLPClassifier(hidden_layer_sizes=(10, ), max_iter=1000, random_state=42)
    
    # Train the model using training data
    mlp.fit(X_train, y_train)
    
    def predict_label(input_data):
        # Ensure the input data is a 2D array
        if input_data.ndim == 1:
            input_data = np.array([input_data])
        # Return predicted probabilities for a single sample
        return mlp.predict_proba(input_data)
    
    ```
    This model uses the irises' features to learn the distinctions among the iris classes (setosa, versicolor, and virginica). After fitting the model, you can then use the function `predict_label` to obtain the probability distribution across the classes for any new sample data. Remember that the `input_data` should be an array matching the feature structure of the training data (that is, it should consist of four feature values representing the sepal length, sepal width, petal length, and petal width of an iris).

---
+++ gpt_synth_iris_test11_attempt11.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Define the multi-layer perceptron model
+mlp = MLPClassifier(hidden_layer_sizes=(10, ), max_iter=1000, random_state=42)
+# Train the model using training data
+mlp.fit(X_train, y_train)
+def predict_label(input_data):
+    # Ensure the input data is a 2D array
+    if input_data.ndim == 1:
+        input_data = np.array([input_data])
+    # Return predicted probabilities for a single sample
+    return mlp.predict_proba(input_data)
