a51638b5a0f397b76b6da306e39e75c3dc6e9a84
philipmit <pschro@mit.edu>
2023-11-13 15:03:18 -0500

    Initial output: Here is the complete Python code with a multi-layer perceptron to predict the labels in the Iris dataset and a function called `predict_label`:
    ```python
    from sklearn.datasets import load_iris
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.neural_network import MLPClassifier
    
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Apply standard scaling to get optimized result
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    # train the multi-layer perceptron
    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
    mlp.fit(X_train, y_train)
    
    def predict_label(single_sample_data):
        # The sample needs to be scaled using the scaler fitted on the training set
        single_sample_data_scaled = sc.transform([single_sample_data])
        # returns array of predicted probabilities
        return mlp.predict_proba(single_sample_data_scaled)
    ```

---
+++ gpt_synth_iris_test14_attempt04.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Apply standard scaling to get optimized result
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+# train the multi-layer perceptron
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+mlp.fit(X_train, y_train)
+def predict_label(single_sample_data):
+    # The sample needs to be scaled using the scaler fitted on the training set
+    single_sample_data_scaled = sc.transform([single_sample_data])
+    # returns array of predicted probabilities
+    return mlp.predict_proba(single_sample_data_scaled)
