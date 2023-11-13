5cdaf7cf892ed4f01ae5e6096f6736755132496e
philipmit <pschro@mit.edu>
2023-11-13 16:37:47 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 21, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 693, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
    
    Response from GPT:
    The error appears to occur because the `predict_label` function is returning class labels and not probabilities. The function `predict_proba` should be used to get probabilities associated with each class, which would sum up to 1. Therefore, `predict_proba` function must be used in the `predict_label` function instead of `lr.predict`. Here is the updated code.
    
    ```
    from sklearn.linear_model import LogisticRegression
    from sklearn.datasets import load_wine
    from sklearn.model_selection import train_test_split
    import numpy as np
    
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train a Logistic Regression model
    lr = LogisticRegression(max_iter=1000)
    lr.fit(X_train, y_train)
    
    def predict_label(data):
        # As logistic regression predicts for multiple samples simultaneously,
        # We put our single instance into an array in order to feed it to predict_proba
        data_reshaped = np.array(data).reshape(1, -1)
        class_probability = lr.predict_proba(data_reshaped)
        return class_probability
    ```

--- gpt_synth_wine_test01_attempt02.py
+++ gpt_synth_wine_test01_attempt02.py
@@ -12,8 +12,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 lr = LogisticRegression(max_iter=1000)
 lr.fit(X_train, y_train)
 def predict_label(data):
-    # As logistic regression predicts for multiple samples simultaneously, 
+    # As logistic regression predicts for multiple samples simultaneously,
     # We put our single instance into an array in order to feed it to predict_proba
     data_reshaped = np.array(data).reshape(1, -1)
-    class_prediction = lr.predict(data_reshaped)
-    return class_prediction
+    class_probability = lr.predict_proba(data_reshaped)
+    return class_probability
