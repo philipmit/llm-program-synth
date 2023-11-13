0573f96fc749b8c2a287bde4b55cfedb2cd06098
philipmit <pschro@mit.edu>
2023-11-13 17:59:44 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Get features X and label y
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Replace strings with numbers in y
    unique_labels = np.unique(y)
    if len(unique_labels) > 2:
        y = pd.get_dummies(y)
    else:
        y = y.replace(unique_labels, [0, 1])
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    if len(unique_labels) > 2:
        # Train the Logistic Regression model with multinomial setting for multi-class
        lrModel = LogisticRegression(random_state=42, max_iter=5000, multi_class='multinomial')
        lrModel.fit(X_train, y_train)
    else:
        # If it's a binary classification, 'auto' will choose 'ovr'
        lrModel = LogisticRegression(random_state=42, max_iter=5000, multi_class='auto')
        lrModel.fit(X_train, y_train)
    
    # Define the function predict_label
    def predict_label(raw_data):
        raw_data = np.array(raw_data)
        raw_data_scaled = scaler.transform(raw_data.reshape(1, -1))
        prediction = lrModel.predict_proba(raw_data_scaled)
        return prediction[0]
    ```

--- gpt_synth_ecoli_test02_attempt01.py
+++ gpt_synth_ecoli_test02_attempt01.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -10,7 +10,11 @@ ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'al
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
 # Replace strings with numbers in y
-y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+unique_labels = np.unique(y)
+if len(unique_labels) > 2:
+    y = pd.get_dummies(y)
+else:
+    y = y.replace(unique_labels, [0, 1])
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the dataset into training and testing sets
@@ -18,9 +22,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Standardize the features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Train the Logistic Regression model
-lrModel = LogisticRegression(random_state=42, max_iter=5000, multi_class='multinomial')
-lrModel.fit(X_train, y_train)
+if len(unique_labels) > 2:
+    # Train the Logistic Regression model with multinomial setting for multi-class
+    lrModel = LogisticRegression(random_state=42, max_iter=5000, multi_class='multinomial')
+    lrModel.fit(X_train, y_train)
+else:
+    # If it's a binary classification, 'auto' will choose 'ovr'
+    lrModel = LogisticRegression(random_state=42, max_iter=5000, multi_class='auto')
+    lrModel.fit(X_train, y_train)
 # Define the function predict_label
 def predict_label(raw_data):
     raw_data = np.array(raw_data)
