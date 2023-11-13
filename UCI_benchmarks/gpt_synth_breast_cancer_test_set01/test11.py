59eba534edba646ec98afbdef7cd66aacd3fe620
philipmit <pschro@mit.edu>
2023-11-13 18:54:13 -0500

    Output 2 (phase 3): Response to improving AUC of 0.5
    AUC from previous code:
    0.5
    
    Response from GPT:
    ```
    from sklearn.datasets import load_breast_cancer
    from sklearn.model_selection import train_test_split
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.model_selection import GridSearchCV
    from sklearn.preprocessing import StandardScaler
    
    # Load the Breast Cancer dataset
    breast_cancer = load_breast_cancer()
    X = breast_cancer.data
    y = breast_cancer.target
    
    # Feature Scaling
    sc = StandardScaler()
    X = sc.fit_transform(X)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Define a Random Forest model, an ensemble method which uses multiple decision trees to improve the performance
    model = RandomForestClassifier(n_estimators=100, random_state=42)
    
    # Define the grid of hyperparameters to search
    param_grid = {
        'n_estimators': [200, 500],
        'max_features': ['auto', 'sqrt', 'log2'],
        'max_depth' : [4,5,6,7,8],
        'criterion' :['gini', 'entropy']
    }
    
    # Set up the grid search with 3-fold cross validation
    grid_cv = GridSearchCV(estimator=model, param_grid=param_grid, cv= 3)
    grid_cv.fit(X_train, y_train)
    
    # Train the optimized model
    model = grid_cv.best_estimator_
    model.fit(X_train, y_train)
    
    # Define the function
    def predict_icu_mortality(raw_data):
        raw_data = sc.transform([raw_data])
        return model.predict_proba(raw_data)[0][1]
    ```
    This updated code replaces the logistic regression model with a Random Forest Classifier which is a more powerful model and has the potential to improve the AUC. Additionally, it uses GridSearch to tune hyperparameters including the number of decision trees (estimators), the maximum number of features considered for splitting a node, the maximum number of levels in each decision tree and the function to measure the quality of a split.

--- UCI_benchmarks/gpt_synth_breast_cancer_test_set01/test11.py
+++ UCI_benchmarks/gpt_synth_breast_cancer_test_set01/test11.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_breast_cancer 
 from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import LogisticRegression 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler 
 # Load the Breast Cancer dataset 
@@ -11,29 +11,23 @@ y = breast_cancer.target
 sc = StandardScaler() 
 X = sc.fit_transform(X) 
 # Split the dataset into training and testing sets 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42) 
-# Define a logistic regression model. 
-model = LogisticRegression(max_iter=1000, tol=0.1)
-# Create a list of options for the regularization penalty
-penalty = ['l1', 'l2', 'elasticnet', 'none']
-# Create a list of options for the solver
-solver = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Define a Random Forest model, an ensemble method which uses multiple decision trees to improve the performance
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 # Define the grid of hyperparameters to search
-hyperparameter_grid = {'penalty': penalty, 'solver': solver}
-# Set up the grid search 
-grid_cv = GridSearchCV(estimator=model, 
-                       param_grid=hyperparameter_grid,
-                       cv=5, 
-                       n_jobs =-1, 
-                       scoring = 'roc_auc')
-# Conduct the grid search 
+param_grid = { 
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'max_depth' : [4,5,6,7,8],
+    'criterion' :['gini', 'entropy']
+}
+# Set up the grid search with 3-fold cross validation
+grid_cv = GridSearchCV(estimator=model, param_grid=param_grid, cv= 3)
 grid_cv.fit(X_train, y_train)
-# Find out the best hyperparameters
-print('Best Penalty:', grid_cv.best_estimator_.get_params()['penalty'])
-print('Best solver:', grid_cv.best_estimator_.get_params()['solver'])
-# Train the optimized logistic regression model
+# Train the optimized model
 model = grid_cv.best_estimator_
 model.fit(X_train, y_train)
+# Define the function
 def predict_icu_mortality(raw_data): 
     raw_data = sc.transform([raw_data]) 
     return model.predict_proba(raw_data)[0][1]
