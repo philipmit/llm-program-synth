5ade34d35c4e39eecd2c5bffdbc7af5b0e9f7c27
philipmit <pschro@mit.edu>
2023-11-13 15:13:46 -0500

    Initial output: ```python from sklearn.datasets import load_iris from sklearn.model_selection import train_test_split from sklearn.neural_network import MLPClassifier from sklearn.preprocessing import StandardScaler
    # Load the Iris dataset
    iris = load_iris()
    X = iris.data
    y = iris.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Training the multilayer perceptron model
    model = MLPClassifier(random_state=42)
    model.fit(X_train, y_train)
    
    # Defining the function for predicting labels
    def predict_label(sample):
        # Reshaping sample to 2D array as it is required for the transform method
        sample = sample.reshape(1, -1)
        # Standardizing the sample
        sample = scaler.transform(sample)
        # Predicting probabilities
        predicted = model.predict_proba(sample)
        return predicted
    ```

---
+++ gpt_synth_iris_test14_attempt14.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Training the multilayer perceptron model
+model = MLPClassifier(random_state=42)
+model.fit(X_train, y_train)
+# Defining the function for predicting labels
+def predict_label(sample):
+    # Reshaping sample to 2D array as it is required for the transform method
+    sample = sample.reshape(1, -1)
+    # Standardizing the sample
+    sample = scaler.transform(sample)
+    # Predicting probabilities
+    predicted = model.predict_proba(sample)
+    return predicted
