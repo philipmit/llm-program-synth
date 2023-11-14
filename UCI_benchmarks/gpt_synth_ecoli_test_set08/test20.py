9da06f23f865e3f93b0f0edd580c23899b9ed667
philipmit <pschro@mit.edu>
2023-11-14 11:29:23 -0500

    Output 5 (phase 3): Response to improving AUC of 0.8483112580819951
    AUC from previous code:
    0.8483112580819951
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.ensemble import RandomForestClassifier
    
    # Load the digits dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1].values # All rows, all columns except the last one
    y = ecoli.iloc[:, -1].values # All rows, only the last column
    # Label Encoding for multi-class classification
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
    # Feature Scaling
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    X_test = sc.transform(X_test)
    
    # Implement RandomForestClassifier to potentially increase AUC
    rf_clf = RandomForestClassifier(n_estimators=500, max_depth=10, random_state=42)
    # Model Training
    rf_clf.fit(X_train, y_train)
    
    # Predict function
    def predict_label(sample):
        sample = np.array(sample).reshape(1, -1)
        sample = sc.transform(sample) # scaling
        pred_proba = rf_clf.predict_proba(sample) # Prediction
        probabilities = np.zeros(len(label_encoder.classes_))
        probabilities[rf_clf.classes_] = pred_proba[0]
        return probabilities
    ```
    In this revised code, we swapped GradientBoostingClassifier with RandomForestClassifier because it has a good track record in producing effective models and operates well with default parameters. We increased the number of trees in the forest (`n_estimators`) to 500 and set `max_depth` to 10 in order to add more signaling value to the model. By reducing the test data size to 0.3, we allow more data for model training, hopefully leading to an improved validation AUC. However, it is crucial to note that improving a model isn't guaranteed since results are largely dependent on the specific dataset and its characteristics.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 # Load the digits dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -13,20 +13,20 @@ y = ecoli.iloc[:, -1].values # All rows, only the last column
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 # Feature Scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-# Trying out GradientBoostingClassifier to improve accuracy
-gb_clf = GradientBoostingClassifier(n_estimators=300, learning_rate=0.05, random_state=42)
+# Implement RandomForestClassifier to potentially increase AUC
+rf_clf = RandomForestClassifier(n_estimators=500, max_depth=10, random_state=42)
 # Model Training
-gb_clf.fit(X_train, y_train)
+rf_clf.fit(X_train, y_train)
 # Predict function
 def predict_label(sample):
     sample = np.array(sample).reshape(1, -1)
     sample = sc.transform(sample) # scaling
-    pred_proba = gb_clf.predict_proba(sample) # Prediction
+    pred_proba = rf_clf.predict_proba(sample) # Prediction
     probabilities = np.zeros(len(label_encoder.classes_))
-    probabilities[gb_clf.classes_] = pred_proba[0]
+    probabilities[rf_clf.classes_] = pred_proba[0]
     return probabilities
