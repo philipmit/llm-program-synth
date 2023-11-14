74d7217c06d67e0c5e41ba703160e58ab444a47c
Philip Herman Schroeder <pschro@sls-1080-3.csail.mit.edu>
2023-11-13 20:58:23 -0500

    Output 39 (phase 3): Response to TypeError
    Error from previous code:
      File "<stdin>", line 292, in <module>
      File "<string>", line 6, in <module>
    <class 'TypeError'>
    predict_icu_mortality() missing 1 required positional argument: 'model'
    
    Response from GPT:
    The error message indicates that the predict_icu_mortality function has not been defined in the GPT's Response 1. Therefore, we need to implement this function and make sure it only takes one input: the dataset for a single patient. Additionally, it does not receive 'model' as an input, but it needs the trained model to make the predictions. A global variable can be used to store the trained model and use it inside the predict_icu_mortality function.
    
    Below is the updated code.
    
    ```python
    import os
    import pandas as pd
    import numpy as np
    import torch
    import torch.nn as nn
    from torch.utils.data import DataLoader
    from sklearn.preprocessing import StandardScaler
    import warnings
    warnings.filterwarnings("ignore")
    
    TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
    LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
    
    class ICUData(torch.utils.data.Dataset):
        def __init__(self, data_path, label_file):
            self.data_path = data_path
            self.file_names = pd.read_csv(label_file)['stay']
            self.labels = torch.tensor(pd.read_csv(label_file)['y_true'].values, dtype=torch.float32)
            self.scaler = StandardScaler()
    
        def __len__(self):
            return len(self.file_names)
    
        def __getitem__(self, idx):
            file_path = os.path.join(self.data_path, self.file_names[idx])
            data = pd.read_csv(file_path).drop('Hours', axis=1)
            data = data.apply(pd.to_numeric, errors='coerce').fillna(0)
            data = self.scaler.fit_transform(data)
            return torch.FloatTensor(data), self.labels[idx]
    
    class LSTM(nn.Module):
        def __init__(self, input_size, hidden_size, num_layers, output_size):
            super(LSTM, self).__init__()
            self.hidden_size = hidden_size
            self.num_layers = num_layers
            self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
            self.fc = nn.Linear(hidden_size, output_size)
    
        def forward(self, x):
            h0, c0 = [torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device) for _ in range(2)]
            out, _ = self.lstm(x, (h0, c0))
            out = self.fc(out[:, -1, :])
            return out
    
    def train_model(model, data_loader, criterion, optimizer, num_epochs):
        model.train()
        for epoch in range(1, num_epochs + 1):
            for seq, labels in data_loader:
                seq, labels = seq.to(device), labels.to(device)
                output = model(seq)
                loss = criterion(output, labels)
                optimizer.zero_grad()
                loss.backward()
                optimizer.step()
            if epoch % 10 == 0:
                print(f'Epoch {epoch}/{num_epochs}, Loss: {loss.item()}')
    
    def predict_icu_mortality(patient_data):
        model.eval()
        with torch.no_grad():
            prediction = model(patient_data)
            return torch.sigmoid(prediction).item()
    
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
    
    model = LSTM(input_size=13, hidden_size=64, num_layers=2, output_size=1).to(device)
    model = model.to(device)
    
    dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
    data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
    
    criterion = nn.BCEWithLogitsLoss()
    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
    
    num_epochs = 100
    train_model(model, data_loader, criterion, optimizer, num_epochs)
    ```
    The predict_icu_mortality function makes use of the globally defined model for prediction. The LSTM model object has been globally defined with the name 'model', which allows predict_icu_mortality to use it without passing it as an argument.

--- mimicIII/gpt_synth_icu_mortality_demo_test_set02/test01.py
+++ mimicIII/gpt_synth_icu_mortality_demo_test_set02/test01.py
@@ -12,22 +12,17 @@ LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospita
 class ICUData(torch.utils.data.Dataset):
     def __init__(self, data_path, label_file):
         self.data_path = data_path
-        label_data = pd.read_csv(label_file)
-        self.file_names = label_data['stay']
-        self.labels = torch.tensor(label_data['y_true'].values, dtype=torch.float32)
+        self.file_names = pd.read_csv(label_file)['stay']
+        self.labels = torch.tensor(pd.read_csv(label_file)['y_true'].values, dtype=torch.float32)
         self.scaler = StandardScaler()
     def __len__(self):
         return len(self.file_names)
     def __getitem__(self, idx):
         file_path = os.path.join(self.data_path, self.file_names[idx])
-        data = pd.read_csv(file_path)
-        data = data.drop('Hours', axis=1)
-        # Replace non-numeric values with NaN and then fill them with 0
+        data = pd.read_csv(file_path).drop('Hours', axis=1)
         data = data.apply(pd.to_numeric, errors='coerce').fillna(0)
         data = self.scaler.fit_transform(data)
-        data = torch.tensor(data, dtype=torch.float32)
-        label = self.labels[idx].unsqueeze(-1)
-        return data, label
+        return torch.FloatTensor(data), self.labels[idx]
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTM, self).__init__()
@@ -36,28 +31,33 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        out, _ = self.lstm(x, (h0,c0))
+        h0, c0 = [torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device) for _ in range(2)]
+        out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         return out
-def train_model(model, data_loader, criterion, optimizer, num_epochs=100):
+def train_model(model, data_loader, criterion, optimizer, num_epochs):
     model.train()
-    for epoch in range(num_epochs):
-        for step, (seq, labels) in enumerate(data_loader):
-            seq = seq.to(device)
-            labels = labels.to(device)
-            outputs = model(seq)
-            loss = criterion(outputs, labels)
+    for epoch in range(1, num_epochs + 1):
+        for seq, labels in data_loader:
+            seq, labels = seq.to(device), labels.to(device)
+            output = model(seq)
+            loss = criterion(output, labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        if (epoch+1) % 10 == 0:
-            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+        if epoch % 10 == 0:
+            print(f'Epoch {epoch}/{num_epochs}, Loss: {loss.item()}')
+def predict_icu_mortality(patient_data):
+    model.eval()
+    with torch.no_grad():
+        prediction = model(patient_data)
+        return torch.sigmoid(prediction).item()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = LSTM(input_size=17, hidden_size=32, num_layers=2, output_size=1).to(device)
-dataset = ICUData(data_path=TRAIN_DATA_PATH, label_file=LABEL_FILE)
-data_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+model = LSTM(input_size=13, hidden_size=64, num_layers=2, output_size=1).to(device)
+model = model.to(device)
+dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
+data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
 criterion = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-train_model(model, data_loader, criterion, optimizer)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+num_epochs = 100
+train_model(model, data_loader, criterion, optimizer, num_epochs)
