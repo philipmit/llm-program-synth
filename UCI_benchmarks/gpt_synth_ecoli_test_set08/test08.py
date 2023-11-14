a554a19f55b5500cc870f3dc9b8973bab726e2d5
philipmit <pschro@mit.edu>
2023-11-13 23:08:23 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    I understand the issue. It seems that the 'predict_label' function is not producing the appropriate number of class probabilities for each instance in X_test. This issue could happen if the 'predict_proba' function is not seeing all classes during training, leading to probabilities only for the observed classes. I'll modify the 'predict_label' function to always return probabilities for all classes, even if the logistic regression model has not been trained on all of them.
    
    My approach will involve encoding the 'y' labels in a manner that accounts for all possible classes and modifies the training process of the logistic regression model to expect all possible classes. The 'predict_label' function will then use this information to always output probabilities for all classes.
    
    Here is the corrected code:
    
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder
    
    # Load dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1].values  # All rows, all columns except last one
    y = ecoli.iloc[:, -1].values   # All rows, only the last column
    
    # Use LabelEncoder to encode class labels as numbers
    le = LabelEncoder()
    all_classes = np.unique(y)
    num_classes = len(all_classes)
    y_encoded = le.fit_transform(y)
    
    # Split the dataset
    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.5, random_state=42)
    
    # Train logistic regression
    logreg = LogisticRegression(random_state=42, multi_class='multinomial', solver='lbfgs', max_iter=10000)
    logreg.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(X):
        # Assume the input x is a list with 7 elements
        # Convert it to (1, 7) shape numpy.ndarray
        X = np.array(X).reshape(1, -1)
    
        # Get probabilities as output of the logistic regression model
        probas = logreg.predict_proba(X)[0]
    
        # Create a zero-filled array to hold probabilities for all classes
        all_probas = np.zeros(num_classes)
    
        # Fill in the probabilities for the classes that the model was trained on
        all_probas[logreg.classes_] = probas
    
        return all_probas
    ```
    The function predict_label will now always return a numpy array containing probabilities for each class, even if some classes were not present in the training data. This should resolve the error when computing roc_auc_score in the validation code.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test08.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test08.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 # Load dataset
@@ -10,24 +10,23 @@ X = ecoli.iloc[:, 1:-1].values  # All rows, all columns except last one
 y = ecoli.iloc[:, -1].values   # All rows, only the last column
 # Use LabelEncoder to encode class labels as numbers
 le = LabelEncoder()
-y = le.fit_transform(y)
-# Ensure stratified split
-stratified_split = StratifiedKFold(n_splits=2, random_state=42, shuffle=True)
-train_index, test_index = next(stratified_split.split(X, y))
+all_classes = np.unique(y)
+num_classes = len(all_classes)
+y_encoded = le.fit_transform(y)
 # Split the dataset
-X_train, X_test = X[train_index], X[test_index]
-y_train, y_test = y[train_index], y[test_index]
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.5, random_state=42)
 # Train logistic regression
-logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+logreg = LogisticRegression(random_state=42, multi_class='multinomial', solver='lbfgs', max_iter=10000)
 logreg.fit(X_train, y_train)
+# Define the predict_label function
 def predict_label(X):
     # Assume the input x is a list with 7 elements
     # Convert it to (1, 7) shape numpy.ndarray
     X = np.array(X).reshape(1, -1)
     # Get probabilities as output of the logistic regression model
-    prob = logreg.predict_proba(X)[0]
-    # Check if all classes have their probabilities
-    missing_classes = set(le.classes_) - set(logreg.classes_)
-    for c in missing_classes:
-        prob = np.insert(prob, le.transform([c])[0], 0)
-    return prob
+    probas = logreg.predict_proba(X)[0]
+    # Create a zero-filled array to hold probabilities for all classes
+    all_probas = np.zeros(num_classes)
+    # Fill in the probabilities for the classes that the model was trained on
+    all_probas[logreg.classes_] = probas
+    return all_probas
