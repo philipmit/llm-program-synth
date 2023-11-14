5dbdfcc0306639a480c0e7357ba768bd766ee7cd
philipmit <pschro@mit.edu>
2023-11-13 20:59:48 -0500

    Initial output: Here's the completed program:
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    
    def preprocess_data(raw_data):
        raw_data = raw_data.iloc[:, 1:-1]  # All rows, all columns except the last one
    
        # Convert DataFrame to np.array
        raw_data = raw_data.to_numpy()
    
        # Standardize
        raw_data = scaler.transform(raw_data)
    
        return raw_data
    
    def predict_label(raw_data):
        # Preprocess the raw data
        processed_data = preprocess_data(raw_data)
    
        # Predict the probabilities using trained model
        probabilities = model.predict_proba(processed_data)
    
        return probabilities
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    # Convert DataFrame to np.array
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Splitting the dataset
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the logistic regression model
    model = LogisticRegression(multi_class='multinomial')
    model.fit(X_train, y_train)
    ```
    Remember to replace the file path for the dataset in the `pd.read_csv()` function to your local file path.

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set06/test01.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+def preprocess_data(raw_data):
+    raw_data = raw_data.iloc[:, 1:-1]  # All rows, all columns except the last one
+    # Convert DataFrame to np.array
+    raw_data = raw_data.to_numpy()
+    # Standardize
+    raw_data = scaler.transform(raw_data)
+    return raw_data
+def predict_label(raw_data):
+    # Preprocess the raw data
+    processed_data = preprocess_data(raw_data)
+    # Predict the probabilities using trained model
+    probabilities = model.predict_proba(processed_data)
+    return probabilities
+# Load the dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1] 
+y = ecoli.iloc[:, -1]   
+# Replace strings with numbers in y
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+# Convert DataFrame to np.array
+X=X.to_numpy()
+y=y.to_numpy()
+# Splitting the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Train the logistic regression model
+model = LogisticRegression(multi_class='multinomial')
+model.fit(X_train, y_train)
