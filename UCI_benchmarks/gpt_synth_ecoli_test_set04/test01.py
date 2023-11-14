6bbf1dd4fa814c18617e45b292307a4b656bba4d
philipmit <pschro@mit.edu>
2023-11-13 19:48:47 -0500

    Output 6 (phase 3): Response to improving AUC of 0.8508468480915071
    AUC from previous code:
    0.8508468480915071
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import GridSearchCV
    from sklearn.pipeline import Pipeline
    # Load the Ecoli data
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Create feature matrix (X) and target array (Y)
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    # List to keep track of unique classes
    unique_classes = list(np.unique(y))
    # Convert string labels in y to numeric
    y = y.replace(unique_classes, list(range(len(unique_classes))))
    # Convert pandas objects to numpy arrays
    X, y = X.to_numpy(), y.to_numpy()
    # Split data into train and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Defining RandomForest model with a pipeline for scaling
    model = Pipeline([
        ('scaler', StandardScaler()),
        ('rfc', RandomForestClassifier())
    ])
    # Setting up a grid of parameters to optimize
    params = {
        "rfc__n_estimators": [10, 50, 100, 200],
        "rfc__max_depth": [None, 10, 20, 30],
        "rfc__min_samples_split": [2, 5, 10],
        "rfc__min_samples_leaf": [1, 2, 4]
    }
    # Performing Grid Search to find the best parameters
    grid_search = GridSearchCV(estimator=model, param_grid=params, cv=5)
    grid_search.fit(X_train, y_train)
    def predict_label(input_data):
        """
        Function to predict the label for given input data using the optimized RandomForest model.
        input_data should be a numpy array.
        """
        prediction_probs = [0 for _ in range(len(unique_classes))]
        input_data_features = input_data.reshape(1, -1)
        predicted_probabilities = grid_search.predict_proba(input_data_features)[0]
        trained_classes = grid_search.best_estimator_.named_steps['rfc'].classes_
        for i in range(len(trained_classes)):
            prediction_probs[trained_classes[i]] = predicted_probabilities[i]
        return prediction_probs
    ```
    In this revised model, a RandomForestClassifier model is used in place of Logistic Regression. RandomForestClassifier is an ensemble learning method that operates by constructing a multitude of decision trees at training time. It combines votes from different decision trees to decide the final class of the test object. This typically provides a better result in comparison to a single decision tree classifier and sometimes, even outperforms logistic regression especially when dealing with non-linear data. The parameters for the RandomForestClassifier are optimized using GridSearchCV that systematically works through multiple combinations of parameter tunes, cross-validating as it goes to determine which tune gives the best performance.

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test01.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
@@ -19,28 +19,30 @@ y = y.replace(unique_classes, list(range(len(unique_classes))))
 X, y = X.to_numpy(), y.to_numpy()
 # Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Defining logistic regression model with a pipeline for scaling
+# Defining RandomForest model with a pipeline for scaling
 model = Pipeline([
     ('scaler', StandardScaler()),
-    ('lr', LogisticRegression(solver='liblinear'))
+    ('rfc', RandomForestClassifier())
 ])
 # Setting up a grid of parameters to optimize
 params = {
-    "lr__C":np.logspace(-3,3,7),
-    "lr__penalty":["l1", "l2"]
+    "rfc__n_estimators": [10, 50, 100, 200],
+    "rfc__max_depth": [None, 10, 20, 30],
+    "rfc__min_samples_split": [2, 5, 10],
+    "rfc__min_samples_leaf": [1, 2, 4]
 }
 # Performing Grid Search to find the best parameters
 grid_search = GridSearchCV(estimator=model, param_grid=params, cv=5)
 grid_search.fit(X_train, y_train)
 def predict_label(input_data):
     """
-    Function to predict the label for given input data using the optimized logistic regression model.
+    Function to predict the label for given input data using the optimized RandomForest model.
     input_data should be a numpy array.
     """
     prediction_probs = [0 for _ in range(len(unique_classes))]
     input_data_features = input_data.reshape(1, -1)
     predicted_probabilities = grid_search.predict_proba(input_data_features)[0]
-    trained_classes = grid_search.best_estimator_.named_steps['lr'].classes_
+    trained_classes = grid_search.best_estimator_.named_steps['rfc'].classes_
     for i in range(len(trained_classes)):
         prediction_probs[trained_classes[i]] = predicted_probabilities[i]
     return prediction_probs
