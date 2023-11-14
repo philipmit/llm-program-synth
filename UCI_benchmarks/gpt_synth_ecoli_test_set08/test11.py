eb2d3a3840c6f5ccf87776e35543c9b8d1bc6ac1
philipmit <pschro@mit.edu>
2023-11-13 23:40:31 -0500

    Output 4 (phase 3): Response to sklearn.utils._param_validation.InvalidParameterError
    Error from previous code:
      File "<stdin>", line 239, in <module>
      File "<string>", line 36, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 1151, in wrapper
        return fit_method(estimator, *args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/model_selection/_search.py", line 933, in fit
        self.best_estimator_.fit(X, y, **fit_params)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 1151, in wrapper
        return fit_method(estimator, *args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/pipeline.py", line 420, in fit
        self._final_estimator.fit(Xt, y, **fit_params_last_step)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 1144, in wrapper
        estimator._validate_params()
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 637, in _validate_params
        validate_parameter_constraints(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 95, in validate_parameter_constraints
        raise InvalidParameterError(
    <class 'sklearn.utils._param_validation.InvalidParameterError'>
    The 'max_features' parameter of RandomForestClassifier must be an int in the range [1, inf), a float in the range (0.0, 1.0], a str among {'log2', 'sqrt'} or None. Got 'auto' instead.
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.pipeline import make_pipeline
    from sklearn.model_selection import GridSearchCV
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows and all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows and only the last column
    # Replace categorical classes with numerical labels in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    # Convert pandas DataFrame to numpy array
    X = X.values
    y = y.values
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features to have mean=0 and variance=1
    scaler = StandardScaler().fit(X_train)
    X_train = scaler.transform(X_train)
    
    # Define the logistic regression model
    model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
    # Fit the model using training data
    model.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Preprocess the raw data
        raw_data = np.array(raw_data)
        raw_data = scaler.transform(raw_data.reshape(1, -1))
        # Predict the probabilities
        predicted_probabilities = model.predict_proba(raw_data)
        return predicted_probabilities[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test11.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test11.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
@@ -22,27 +21,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Standardize the features to have mean=0 and variance=1
 scaler = StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
-# Define the number of classes
-num_classes = len(np.unique(y))
-# Instead of logistic regression let's try using a Random Forest Classifier which usually performs better
-pipeline = make_pipeline(RandomForestClassifier())
-param_grid = {
-    'randomforestclassifier__n_estimators': [100, 200],
-    'randomforestclassifier__max_depth': [5, 10, 15],
-    'randomforestclassifier__max_features': ['auto', 'sqrt', 'log2'],
-    'randomforestclassifier__criterion' :['gini', 'entropy']
-}
-grid = GridSearchCV(pipeline, param_grid, cv=5, scoring='roc_auc_ovr', n_jobs=-1)
-grid.fit(X_train, y_train)
-model = grid.best_estimator_
+# Define the logistic regression model
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+# Fit the model using training data
+model.fit(X_train, y_train)
 def predict_label(raw_data):
     # Preprocess the raw data
     raw_data = np.array(raw_data)
     raw_data = scaler.transform(raw_data.reshape(1, -1))
     # Predict the probabilities
     predicted_probabilities = model.predict_proba(raw_data)
-    # Initialize a probabilities vector filled with zeros for each class type
-    probabilities = np.zeros(num_classes)
-    # Fill the probabilities of the classes that the model can predict
-    probabilities[model.classes_] = predicted_probabilities
-    return probabilities
+    return predicted_probabilities[0]
