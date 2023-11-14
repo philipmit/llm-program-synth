ef9376a8e5e388e19856c8dd4cb23876acd27bd9
philipmit <pschro@mit.edu>
2023-11-13 22:56:53 -0500

    Output 10 (phase 3): Response to ValueError
    Error from previous code:
      File "<stdin>", line 292, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error is suggesting that the dimension of the predicted probability array from the function `predict_label` does not match the expected number of classes in the `y_true` array. This indicates that Logistic Regression algorithm might be predicting only probabilities for the classes it has seen in the training data. It may not output probabilities for all classes if some of them are not present in the training data.
    
    Hence, by guaranteeing that `LogisticRegression` model is trained with all classes from the dataset, we can avoid this error. We can use `np.unique(y)` to get all possible classes. Then, we will create a fake X dataset with the size equals to number of classes and y as classes. After training with real data, we retrain the model with fake data but with a very small `sample_weight` to minimally affect the model.
    
    Please see below for the updated coding solution:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.pipeline import make_pipeline
    
    # Load Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Pre-process the dataset
    X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
    y = ecoli.iloc[:, -1].values # All rows, only the last column
    
    # Replace string labels with numbers using LabelEncoding
    from sklearn.preprocessing import LabelEncoder
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into training and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Define a pipeline that includes Standard Scaler and logistic regression model
    # It standardizes features by removing the mean and scaling to unit variance and then applies logistic regression.
    # Also, increased the iterations or max_iter to 5000
    # Here, an l2 penalty is also used which is a very simple way to improve performance was added to avoid overfitting
    pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000, penalty='l2', solver='liblinear', multi_class='auto'))
    
    # Then train the model
    pipeline.fit(X_train, y_train)
    
    # Fake a X dataset and a y array that contains all classes
    classes= np.unique(y)
    fake_X= np.zeros((len(classes), X_train.shape[1]))
    fake_y= classes
    # Retrain with very small sample_weight
    pipeline.fit(fake_X, fake_y, logisticregression__sample_weight=np.full(len(classes), 1e-3))
    
    def predict_label(X_sample):
        # Reshaping the input to match the input data shape for Logistic Regression model
        X_sample = np.array(X_sample).reshape(1, -1)
        # Get the model's predicted probabilities with predict_proba
        pred_proba = pipeline.predict_proba(X_sample)
        return pred_proba[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
@@ -4,9 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-# Load the ecoli dataset
-ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
-                    delim_whitespace=True, header=None)
+# Load Ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Pre-process the dataset
 X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
@@ -15,16 +14,21 @@ y = ecoli.iloc[:, -1].values # All rows, only the last column
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-# Split the dataset into training and testing sets
+# Split the dataset into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Define a pipeline that includes Standard Scaler and logistic regression model
 # It standardizes features by removing the mean and scaling to unit variance and then applies logistic regression.
 # Also, increased the iterations or max_iter to 5000
 # Here, an l2 penalty is also used which is a very simple way to improve performance was added to avoid overfitting
-pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000, penalty='l2', solver='liblinear'))
+pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000, penalty='l2', solver='liblinear', multi_class='auto'))
 # Then train the model
 pipeline.fit(X_train, y_train)
-# Still using predict_proba method but modified function to work with the pipeline
+# Fake a X dataset and a y array that contains all classes
+classes= np.unique(y)
+fake_X= np.zeros((len(classes), X_train.shape[1]))
+fake_y= classes
+# Retrain with very small sample_weight 
+pipeline.fit(fake_X, fake_y, logisticregression__sample_weight=np.full(len(classes), 1e-3))
 def predict_label(X_sample):
     # Reshaping the input to match the input data shape for Logistic Regression model
     X_sample = np.array(X_sample).reshape(1, -1)
