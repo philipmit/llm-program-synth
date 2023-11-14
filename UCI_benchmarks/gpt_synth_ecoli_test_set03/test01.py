0e732e7f6d1aaf2a6d4e158462272da7a4177458
philipmit <pschro@mit.edu>
2023-11-13 19:05:29 -0500

    Output 6 (phase 3): Response to sklearn.utils._param_validation.InvalidParameterError
    Error from previous code:
      File "<stdin>", line 239, in <module>
      File "<string>", line 24, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 1144, in wrapper
        estimator._validate_params()
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 637, in _validate_params
        validate_parameter_constraints(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 95, in validate_parameter_constraints
        raise InvalidParameterError(
    <class 'sklearn.utils._param_validation.InvalidParameterError'>
    The 'max_features' parameter of RandomForestClassifier must be an int in the range [1, inf), a float in the range (0.0, 1.0], a str among {'sqrt', 'log2'} or None. Got 'auto' instead.
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Define features and target
    X = ecoli.iloc[:, 1:-1]    # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]     # All rows, only the last column
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    # Convert to numpy arrays
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Pre-processing: Standard Scaler
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    # Train Logistic Regression model
    lr = LogisticRegression(random_state=42, max_iter=10000)
    lr.fit(X_train, y_train)
    
    def predict_label(data):
        data = sc.transform(np.array(data).reshape(1, -1))    # Apply the standard scaler on new data
        probabilities = lr.predict_proba(data)[0]            # Predict probabilities
        return probabilities
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set03/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test01.py
@@ -1,28 +1,27 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Define features and target
-X = ecoli.drop(['Sequence Name', 'class'], axis=1)
-y = ecoli['class']
+X = ecoli.iloc[:, 1:-1]    # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]     # All rows, only the last column
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
-# Cast to numpy arrays
+# Convert to numpy arrays
 X = X.to_numpy()
 y = y.to_numpy()
-# Stratified Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y) # reduced test size to 0.3 to have more data for training.
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Pre-processing: Standard Scaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-# Train RandomForestClassifier with increased estimators for better accuracy
-rfc = RandomForestClassifier(n_estimators=2000, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features='auto', bootstrap=True, oob_score=False, n_jobs=-1, random_state=42, verbose=0, warm_start=False, class_weight=None)
-rfc.fit(X_train, y_train)
-def predict_icu_mortality(data):
-    data = sc.transform(np.array(data).reshape(1, -1)) # apply the standard scalar on new data
-    proba = rfc.predict_proba(data)[0]
-    return proba
+# Train Logistic Regression model
+lr = LogisticRegression(random_state=42, max_iter=10000)
+lr.fit(X_train, y_train)
+def predict_label(data):
+    data = sc.transform(np.array(data).reshape(1, -1))    # Apply the standard scaler on new data
+    probabilities = lr.predict_proba(data)[0]            # Predict probabilities
+    return probabilities
