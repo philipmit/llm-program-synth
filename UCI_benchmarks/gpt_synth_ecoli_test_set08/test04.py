5c3dd4fbb9071ee88b1980412450753be2a70054
philipmit <pschro@mit.edu>
2023-11-13 22:47:11 -0500

    Output 5 (phase 3): Response to improving AUC of 0.7127678948997982
    AUC from previous code:
    0.7127678948997982
    
    Response from GPT:
    ```
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    import numpy as np
    from sklearn.ensemble import GradientBoostingClassifier
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    # Convert dataframe to numpy array
    X= X.to_numpy()
    y= y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the datasets using StandardScaler
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Changed model to Gradient Boosting Classifier to possibly get a more complex decision boundary and improve accuracy.
    # Tuned parameters such as learning_rate, n_estimators and max_depth, which help in reducing overfitting of model and improving performance.
    model = GradientBoostingClassifier(learning_rate=0.01, n_estimators=500, max_depth=5, random_state=42)
    model.fit(X_train, y_train)
    
    # Function to predict label
    def predict_label(X_raw):
        num_class = len(np.unique(y))  # Compute the number of unique classes in the dataset
        X_raw_np_array = np.array(X_raw, dtype=float).reshape(1, -1)
        X_raw_scaled = scaler.transform(X_raw_np_array)
        # Compute prediction probabilities
        prediction = model.predict_proba(X_raw_scaled)[0]
        # Handle case where prediction probabilities have less classes than provided in dataset
        if len(prediction) != num_class:
            diff = num_class - len(prediction)  # Compute the difference between the number of classes in dataset and classes predicted
            return np.concatenate([prediction, np.zeros(diff)])  # Append zeros at the end of the prediction array to match class count
        else:
            return prediction
    ```
    Here, I have opted to use the Gradient Boosting Classifier. Gradient Boosting often gives better results than random forests. While all parameters could be tuned, a few significant changes have been made.
    
    The learning rate is lowered which will make the learning slower, but it will make the model less likely to overfit and overall more robust.
    
    The number of estimators (n_estimators) is increased to 500 which means that we will have 500 weak learners where each one will correct the mistakes of the previous one. A high number of estimators can lead to better performance but also longer training times.
    
    Finally, max_depth parameter which controls the depth of the individual tree estimators. The maximum depth limits the number of nodes in the tree, lower value discourages overfitting.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test04.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test04.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
@@ -15,24 +15,20 @@ y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
 # Convert dataframe to numpy array
 X= X.to_numpy()
 y= y.to_numpy()
-# Add Polynomial Features to increase complexity and possibly capture more patterns in the data
-poly_transform = PolynomialFeatures(degree=2)
-X = poly_transform.fit_transform(X)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Scale the datasets using StandardScaler
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Changed model to Random Forest Classifier to possibly get a more complex decision boundary and improve accuracy
-# Also added more estimators for a stronger model, at the cost of increased computation
-model = RandomForestClassifier(n_estimators=200, random_state=42)
+# Changed model to Gradient Boosting Classifier to possibly get a more complex decision boundary and improve accuracy.
+# Tuned parameters such as learning_rate, n_estimators and max_depth, which help in reducing overfitting of model and improving performance.
+model = GradientBoostingClassifier(learning_rate=0.01, n_estimators=500, max_depth=5, random_state=42)
 model.fit(X_train, y_train)
 # Function to predict label
 def predict_label(X_raw):
     num_class = len(np.unique(y))  # Compute the number of unique classes in the dataset
     X_raw_np_array = np.array(X_raw, dtype=float).reshape(1, -1)
-    X_raw_poly = poly_transform.transform(X_raw_np_array)     # Don't forget to transform raw input as well
-    X_raw_scaled = scaler.transform(X_raw_poly)
+    X_raw_scaled = scaler.transform(X_raw_np_array)  
     # Compute prediction probabilities
     prediction = model.predict_proba(X_raw_scaled)[0]
     # Handle case where prediction probabilities have less classes than provided in dataset
