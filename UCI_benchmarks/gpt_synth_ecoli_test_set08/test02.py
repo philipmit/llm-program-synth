593bafcada320deea12dc4c4ba116d30bc05fe55
philipmit <pschro@mit.edu>
2023-11-13 22:20:31 -0500

    Output 7 (phase 3): Response to improving AUC of 0.7082025169997862
    AUC from previous code:
    0.7082025169997862
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder, MinMaxScaler
    from sklearn.feature_selection import SelectKBest, f_classif
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Select the features and target
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Convert categorical labels to numeric
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Splitting the data into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Feature selection based on k-best ANOVA F-value
    selector = SelectKBest(f_classif, k=6)
    X_train = selector.fit_transform(X_train, y_train)
    
    # Scaling
    scaler = MinMaxScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Training the Logistic Regression model
    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=5000, C=0.7)
    
    model.fit(X_train, y_train)
    
    # Function to predict labels for new data
    def predict_label(sample):
      sample = selector.transform([sample])
      sample_scaled = scaler.transform(sample)
      probabilities = model.predict_proba(sample_scaled)
    
      # Extend the probabilities list with zeros if number of classes is less than 8
      if len(probabilities[0]) < 8:
        extended_probabs = np.append(probabilities[0], [0]*(8 - len(probabilities[0])))
        return extended_probabs
      else:
        return probabilities[0]
    ```
    While recreating the code from GPT, I have made a couple of changes such as I have implemented a feature selection method to keep the most relevant features. I also switched the scaling method to MinMaxScaler, which scales the features to a fixed range of 0-1 and can sometimes yield better results compared to Standard Scaler. In the Logistic Regression model, I have switched to a 'multinomial' setting which considers the multiclass nature of the problem more directly than the 'ovr' (One-vs-Rest) setting. I have also increased the max_iter parameter to 5000 and tuned the regularization parameter C by setting it to 0.7. These changes can potentially increase the validation AUC value.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test02.py
@@ -2,27 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.feature_selection import SelectKBest, f_classif
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Select the features and target
 X = ecoli.iloc[:, 1:-1] 
-y = ecoli.iloc[:, -1] 
+y = ecoli.iloc[:, -1]
 # Convert categorical labels to numeric
 le = LabelEncoder()
 y = le.fit_transform(y)
 # Splitting the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Standardization
-scaler = StandardScaler()
+# Feature selection based on k-best ANOVA F-value
+selector = SelectKBest(f_classif, k=6)
+X_train = selector.fit_transform(X_train, y_train)
+# Scaling
+scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 # Training the Logistic Regression model
-model = LogisticRegression(multi_class='ovr', max_iter=1000)
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=5000, C=0.7)
 model.fit(X_train, y_train)
 # Function to predict labels for new data
 def predict_label(sample):
-  sample_scaled = scaler.transform([sample])
+  sample = selector.transform([sample])
+  sample_scaled = scaler.transform(sample)
   probabilities = model.predict_proba(sample_scaled)
   # Extend the probabilities list with zeros if number of classes is less than 8
   if len(probabilities[0]) < 8:
