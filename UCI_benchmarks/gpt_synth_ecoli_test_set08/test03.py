fb1e06800015ae7b5ef5715212165c18bcac4b50
philipmit <pschro@mit.edu>
2023-11-13 22:22:19 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error message is due to the fact the number of unique labels in 'y_true' are not matching with the number of labels 'y_score' is predicting. This can be due to label mismatch caused while splitting the data or by missing labels in the test data. Let's modify the training code to avoid this issue.
    
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelBinarizer
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare the feature and target variables
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Transform target labels into binary array
    lb = LabelBinarizer()
    y = lb.fit_transform(y)
    
    # If y has only 1 column, i.e., only 2 unique labels, reshape it to (n_samples,)
    if y.shape[1] == 1:
        y = y.reshape(-1)
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    log_reg = LogisticRegression(max_iter=1000)
    if len(y_train.shape) > 1 and y_train.shape[1] > 1:
        # If y_train has more than 1 column, i.e., more than 2 unique labels
        from sklearn.multiclass import OneVsRestClassifier
        cls = OneVsRestClassifier(log_reg)
    else:
        cls = log_reg
    cls.fit(X_train, y_train)
    
    def predict_label(raw_data_sample):
        proba = cls.predict_proba(raw_data_sample.reshape(1, -1))
        return proba[0]
    ```
    As there is one-model-per-class in the OneVsRestClassifier setup, each class's result is independent of the other classes. Thus no matter how many unique classes exist in `y_test` or `y_true`, the prediction will always yield probabilities for every class found in `y_train` or `y_train_binarized`.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
@@ -2,19 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Prepare the feature and target variables
 X = ecoli.iloc[:, 1:-1]
 y = ecoli.iloc[:, -1]
-y = y.replace(list(np.unique(y)), range(len(np.unique(y))))
+# Transform target labels into binary array
+lb = LabelBinarizer()
+y = lb.fit_transform(y)
+# If y has only 1 column, i.e., only 2 unique labels, reshape it to (n_samples,)
+if y.shape[1] == 1:
+    y = y.reshape(-1)
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Train the logistic regression model
-log_reg = LogisticRegression(max_iter=1000).fit(X_train, y_train)
+log_reg = LogisticRegression(max_iter=1000)
+if len(y_train.shape) > 1 and y_train.shape[1] > 1:
+    # If y_train has more than 1 column, i.e., more than 2 unique labels
+    from sklearn.multiclass import OneVsRestClassifier
+    cls = OneVsRestClassifier(log_reg)
+else:
+    cls = log_reg
+cls.fit(X_train, y_train)
 def predict_label(raw_data_sample):
-    proba = log_reg.predict_proba(raw_data_sample.reshape(1, -1))
+    proba = cls.predict_proba(raw_data_sample.reshape(1, -1))
     return proba[0]
