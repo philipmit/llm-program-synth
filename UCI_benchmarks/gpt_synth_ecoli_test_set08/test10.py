96dcc21ea7741b33a1f9ea287096c9475eb1eee5
philipmit <pschro@mit.edu>
2023-11-13 23:32:19 -0500

    Output 17 (phase 3): Response to improving AUC of 0.7211382636546195
    AUC from previous code:
    0.7211382636546195
    
    Response from GPT:
    ```
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.preprocessing import LabelEncoder
    from sklearn.linear_model import LogisticRegression
    from sklearn.metrics import roc_auc_score
    from sklearn.ensemble import GradientBoostingClassifier
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Get features and labels
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # replace strings with numbers in y
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    
    # Standardize the data
    scaler = StandardScaler()
    X = scaler.fit_transform(X)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
    
    # Initialize and train GradientBoostingClassifier for better performance
    clf = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.05, max_depth=5, random_state=42)
    clf.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(sample):
        sample_reshaped = np.array(sample).reshape(1, -1)
        # Standardize the test sample
        sample_reshaped = scaler.transform(sample_reshaped)
        predicted_probabilities = clf.predict_proba(sample_reshaped)
        # Add zero-probabilities for classes that were not in the training data
        complete_probabilities = np.zeros(len(label_encoder.classes_))
        for class_index, probability in enumerate(predicted_probabilities[0]):
            complete_probabilities[class_index] = probability
        return complete_probabilities
    ```
    This updated version has switched out the AdaBoost model for a Gradient Boosting classifier, as the latter often performs better on imbalanced datasets, which is often the case in biomedical data. Moreover, a smaller learning rate was chosen to ensure more stable training, and the number of estimators was increased to give the model more complexity and to accommodate the smaller learning rate. The data split between training and testing was also changed to leave more data for training (80%) and less for testing (20%), in order to give the model more training data and hopefully boost its performance, hence may result in an increased validation AUC score.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
@@ -1,12 +1,10 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import GradientBoostingClassifier
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -20,17 +18,16 @@ y = label_encoder.fit_transform(y)
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Initialize and train AdaBoostClassifier with Random Forest base estimator
-rf_model = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=42)
-ada_model = AdaBoostClassifier(base_estimator=rf_model, n_estimators=150, learning_rate=0.1, random_state=42)
-ada_model.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Initialize and train GradientBoostingClassifier for better performance
+clf = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.05, max_depth=5, random_state=42)
+clf.fit(X_train, y_train)
 # Define the predict_label function
 def predict_label(sample):
     sample_reshaped = np.array(sample).reshape(1, -1)
     # Standardize the test sample
     sample_reshaped = scaler.transform(sample_reshaped)
-    predicted_probabilities = ada_model.predict_proba(sample_reshaped)
+    predicted_probabilities = clf.predict_proba(sample_reshaped)
     # Add zero-probabilities for classes that were not in the training data
     complete_probabilities = np.zeros(len(label_encoder.classes_))
     for class_index, probability in enumerate(predicted_probabilities[0]):
