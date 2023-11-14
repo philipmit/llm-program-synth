8513c25f3d377f3a694fde8fde859241e259491e
philipmit <pschro@mit.edu>
2023-11-13 22:32:11 -0500

    Output 16 (phase 3): Response to improving AUC of 0.8544806776265685
    AUC from previous code:
    0.8544806776265685
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder, StandardScaler
    from sklearn.multiclass import OneVsRestClassifier
    from sklearn.pipeline import make_pipeline
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare the feature and target variables
    X = ecoli.iloc[:, 1:-1]     # All rows, all columns excluding the first and last one
    y = ecoli.iloc[:, -1]       # All rows, only the last column
    
    # Transform the target string labels into numbers using LabelEncoder
    label_encoder = LabelEncoder()
    y_encoded = label_encoder.fit_transform(y)
    n_classes = np.unique(y_encoded).shape[0]
    
    # Perform train test split
    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded,
                                                        stratify=y_encoded, test_size=0.5, random_state=42)
    
    # Normalization of features using standard scaler
    std_scaler = StandardScaler()
    
    # Define the logistic regression model with one-vs-rest classifier
    # and increased number of iterations for SGD convergence
    log_reg_model = LogisticRegression(multi_class='ovr', solver='saga', max_iter=5000, random_state=42)
    ovr_log_reg = OneVsRestClassifier(make_pipeline(std_scaler, log_reg_model))
    
    # Train the logistic regression model
    ovr_log_reg.fit(X_train, y_train)
    
    def predict_label(raw_data_sample):
        # Ensure the input data is 2D
        if len(raw_data_sample.shape) == 1:
          raw_data_sample = np.expand_dims(raw_data_sample, 0)
    
        # Predict the class probabilities (taking the first (and only) prediction when one sample is given)
        proba = ovr_log_reg.predict_proba(raw_data_sample)[0]
    
        # aligning the output probabilities with original class labels
        proba_dict = dict(zip(range(8), [0]*8))
        for c_index, c_proba in zip(ovr_log_reg.classes_, proba):
            proba_dict[c_index] = c_proba
        return list(proba_dict.values())
    ```
    The improved code uses SGD solver since it's efficient for larger datasets and performs better when data is scaled. More iterations (max_iter) will ensure better convergence. Stratified train-test split was introduced to maintain the proportion of different classes in both training and testing dataset.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
@@ -2,29 +2,35 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.pipeline import make_pipeline
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Prepare the feature and target variables
-X = ecoli.iloc[:, 1:-1]   # All rows, all columns excluding the first and last one
-y = ecoli.iloc[:, -1]     # All rows, only the last column
+X = ecoli.iloc[:, 1:-1]     # All rows, all columns excluding the first and last one
+y = ecoli.iloc[:, -1]       # All rows, only the last column
 # Transform the target string labels into numbers using LabelEncoder
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y)
 n_classes = np.unique(y_encoded).shape[0]
 # Perform train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.5, random_state=42)
-# Define the logistic regression model with one-vs-rest classifier
-ovr_log_reg = OneVsRestClassifier(LogisticRegression(multi_class='ovr', solver='lbfgs', max_iter=1000, random_state=42))
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded,
+                                                    stratify=y_encoded, test_size=0.5, random_state=42)
+# Normalization of features using standard scaler
+std_scaler = StandardScaler()
+# Define the logistic regression model with one-vs-rest classifier 
+# and increased number of iterations for SGD convergence
+log_reg_model = LogisticRegression(multi_class='ovr', solver='saga', max_iter=5000, random_state=42)
+ovr_log_reg = OneVsRestClassifier(make_pipeline(std_scaler, log_reg_model))
 # Train the logistic regression model
 ovr_log_reg.fit(X_train, y_train)
 def predict_label(raw_data_sample):
     # Ensure the input data is 2D
     if len(raw_data_sample.shape) == 1:
-        raw_data_sample = np.expand_dims(raw_data_sample, 0)
-    # Predict the class probabilities taking the first (and only) prediction when one sample is given
+      raw_data_sample = np.expand_dims(raw_data_sample, 0)
+    # Predict the class probabilities (taking the first (and only) prediction when one sample is given)
     proba = ovr_log_reg.predict_proba(raw_data_sample)[0]
     # aligning the output probabilities with original class labels
     proba_dict = dict(zip(range(8), [0]*8))
