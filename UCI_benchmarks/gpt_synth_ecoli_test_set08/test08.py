3c55c243765b15b6549fef6854bc524ee78e6012
philipmit <pschro@mit.edu>
2023-11-13 23:10:48 -0500

    Output 5 (phase 3): Response to improving AUC of 0.9501247587027464
    AUC from previous code:
    0.9501247587027464
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    import xgboost as xgb
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare feature (X) and target (y) arrays
    X = ecoli.iloc[:, 1:-1].values
    y = ecoli.iloc[:, -1].values
    
    # Encode class labels as numbers
    label_encoder = LabelEncoder()
    y_labeled = label_encoder.fit_transform(y)
    
    # Split the data into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y_labeled, test_size=0.2, random_state=42)
    
    # Standardize the features
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    X_test = scaler.transform(X_test)
    
    # Use XGBoost for prediction, with a high number of rounds and a learning rate adapted for better performance
    params = {
        "objective":"multi:softprob",
        "max_depth":8,
        "silent":1,
        "eta":0.01, # learning rate
        "num_class":8,
        "n_estimators":600
    }
    dtrain = xgb.DMatrix(data=X_train, label=y_train)
    dtest = xgb.DMatrix(data=X_test)
    xgb_model = xgb.train(params, dtrain, num_boost_round=800)
    
    # Define the predict_label function
    def predict_label(X):
        # Assume the input X is a list with 7 elements
        # Convert it to a numpy array with shape (1, 7)
        X = np.array(X).reshape(1, -1)
        # Standardize the features
        X = scaler.transform(X)
        # Transform it to DMatrix for XGBoost
        ddata = xgb.DMatrix(data = X)
        # Get probabilities as output of the XGBoost model
        probas = xgb_model.predict(ddata)
        return probas
    ```
    This code refines the prediction model by using XGBoost instead of RandomForest. XGBoost is a popular machine learning algorithm that tends to perform well on a wide variety of problems. The learning rate (`eta`) has been selected as 0.01, and the number of rounds for ensemble learning is high (800) as a way to potentially improve the model. Besides, XGBoost was set to run silently (`'silent': 1`), and the objective was set to output probabilities for multi-class classification (`'objective':'multi:softprob'`). By making these changes, there is a possibility of achieving better performance and a higher validation AUC.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test08.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test08.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-# Load dataset
+import xgboost as xgb
+# Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-# preparing feature (X) and target (y) arrays
+# Prepare feature (X) and target (y) arrays
 X = ecoli.iloc[:, 1:-1].values  
 y = ecoli.iloc[:, -1].values 
 # Encode class labels as numbers
@@ -19,16 +19,27 @@ X_train, X_test, y_train, y_test = train_test_split(X, y_labeled, test_size=0.2,
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-# Use RandomForestClassifier to predict
-random_forest = RandomForestClassifier(n_estimators=200, max_depth=8, random_state=42)
-random_forest.fit(X_train, y_train)
+# Use XGBoost for prediction, with a high number of rounds and a learning rate adapted for better performance
+params = {
+    "objective":"multi:softprob",
+    "max_depth":8,
+    "silent":1,
+    "eta":0.01, # learning rate
+    "num_class":8,
+    "n_estimators":600
+}
+dtrain = xgb.DMatrix(data=X_train, label=y_train)
+dtest = xgb.DMatrix(data=X_test)
+xgb_model = xgb.train(params, dtrain, num_boost_round=800)
 # Define the predict_label function
 def predict_label(X):
     # Assume the input X is a list with 7 elements
-    # Convert it to (1, 7) shape numpy.ndarray
+    # Convert it to a numpy array with shape (1, 7)
     X = np.array(X).reshape(1, -1)
     # Standardize the features
     X = scaler.transform(X)
-    # Get probabilities as output of the Random Forest model
-    probas = random_forest.predict_proba(X)[0]
+    # Transform it to DMatrix for XGBoost
+    ddata = xgb.DMatrix(data = X)
+    # Get probabilities as output of the XGBoost model
+    probas = xgb_model.predict(ddata)
     return probas
