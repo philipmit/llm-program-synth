0c19f0b77f84e3e310cca17d37d05c7257cf99ee
philipmit <pschro@mit.edu>
2023-11-13 22:45:10 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The issue here seems to be that the output of the predict_label function is a 1d array with probabilities for each existing class in the training set. This issue could occur when there are classes in the test set (y_test) that were not present in the training set (y_train). The prediction won't produce probabilities for these classes which leads to the dimensions mismatch.
    
    A workaround is to include a check in the predict_label function to return a flat array of zeros for classes that were not in the training dataset. Below is the modified code:
    
    ```python
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    import numpy as np
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    # Convert dataframe to numpy array
    X= X.to_numpy()
    y= y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the datasets using StandardScaler
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the logistic regression model
    model = LogisticRegression(random_state=42, multi_class='auto', solver='lbfgs', max_iter=1000)
    model.fit(X_train, y_train)
    
    # Function to predict label
    def predict_label(X_raw):
        num_class = len(np.unique(y))  # Compute the number of unique classes in the dataset
        X_raw_np_array = np.array(X_raw, dtype=float).reshape(1, -1)
        X_raw_scaled = scaler.transform(X_raw_np_array)
    
        # Compute prediction probabilities
        prediction = model.predict_proba(X_raw_scaled)[0]
    
        # Handle case where prediction probabilities have less classes than provided in dataset
        if len(prediction) != num_class:
            diff = num_class - len(prediction)  # Compute the difference between the number of classes in dataset and classes predicted
            return np.concatenate([prediction, np.zeros(diff)])  # Append zeros at the end of the prediction array to match class count
        else:
            return prediction
    ```
    This modification in the model will output predicted probability for each class in the dataset, including the classes that were not in the training dataset. It fills the probability for these classes with zero since our model is not trained to predict them.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test04.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test04.py
@@ -7,7 +7,7 @@ import numpy as np
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows, all the columns except the last one
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
 # replace strings with numbers in y
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
@@ -24,7 +24,14 @@ model = LogisticRegression(random_state=42, multi_class='auto', solver='lbfgs', 
 model.fit(X_train, y_train)
 # Function to predict label
 def predict_label(X_raw):
+    num_class = len(np.unique(y))  # Compute the number of unique classes in the dataset
     X_raw_np_array = np.array(X_raw, dtype=float).reshape(1, -1)
     X_raw_scaled = scaler.transform(X_raw_np_array)
-    prediction = model.predict_proba(X_raw_scaled)[0] # Access the first element to avoid extra wrapping into list
-    return prediction
+    # Compute prediction probabilities
+    prediction = model.predict_proba(X_raw_scaled)[0]
+    # Handle case where prediction probabilities have less classes than provided in dataset
+    if len(prediction) != num_class:
+        diff = num_class - len(prediction)  # Compute the difference between the number of classes in dataset and classes predicted
+        return np.concatenate([prediction, np.zeros(diff)])  # Append zeros at the end of the prediction array to match class count 
+    else:
+        return prediction
