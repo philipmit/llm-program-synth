4fcec8de45ade704dd70aad3b7049cd58f5afd5f
philipmit <pschro@mit.edu>
2023-11-14 00:02:36 -0500

    Output 14 (phase 3): Response to improving AUC of 0.9489873522172585
    AUC from previous code:
    0.9489873522172585
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.pipeline import Pipeline
    from sklearn.feature_selection import RFECV
    from sklearn.metrics import roc_auc_score
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Prepare the data
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    # Transform y from categorical to numerical values
    le = LabelEncoder()
    y = le.fit_transform(y)
    # Stratified Split of data into Training and Test set ensuring all classes are represented
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
    
    # Training a logistic regression model with feature selection and data standardization
    model = Pipeline([
      ('feature_selection', RFECV(LogisticRegression(max_iter=1000, multi_class='ovr'))),
      ('standardize', StandardScaler()),
      ('log_reg', LogisticRegression(max_iter=5000, multi_class='ovr', solver='saga', penalty='l1', C=0.1))
    ])
    model.fit(X_train, y_train)
    
    def predict_label(raw_data):
        """
        Predict probabilities for a given raw unprocessed data.
        """
        raw_data = np.array(raw_data).reshape(1, -1)
        probabilities = model.predict_proba(raw_data)
        return probabilities[0]
    ```
    In the code above, I've made the following changes to improve the validation AUC:
    
    - I've used feature selection (Recursive Feature Elimination with Cross-Validation) and data standardization as part of a pipeline. This can help improve the model performance by eliminating irrelevant features and making sure features are on the same scale.
    - I've increased the maximum number of iterations to 5000 to allow the model to converge better.
    - I've changed the solver to 'saga' which is more efficient for large datasets and supports L1 penalty.
    - I've used L1 ('l1') regularization (penalty) which can help prevent overfitting by adding a penalty for complexity. By making some of the coefficients of the less important features exactly zero, L1 regularization performs feature selection and allows the model to ignore these features.
    - I've set C to 0.1 to increase the regularization strength. Smaller values of C specify stronger regularization.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
@@ -2,25 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn import preprocessing
-# Load the digits dataset
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import RFECV
+from sklearn.metrics import roc_auc_score
+# Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Prepare the data 
-X = ecoli.iloc[:, 1:-1].values  
-y = ecoli.iloc[:, -1].values 
+X = ecoli.iloc[:, 1:-1]  
+y = ecoli.iloc[:, -1]
 # Transform y from categorical to numerical values
-le = preprocessing.LabelEncoder()
+le = LabelEncoder()
 y = le.fit_transform(y)
 # Stratified Split of data into Training and Test set ensuring all classes are represented
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
-# Train Logistic Regression Model
-log_reg = LogisticRegression(max_iter=1000, multi_class='ovr')
-log_reg.fit(X_train, y_train)
+# Training a logistic regression model with feature selection and data standardization
+model = Pipeline([
+  ('feature_selection', RFECV(LogisticRegression(max_iter=1000, multi_class='ovr'))),
+  ('standardize', StandardScaler()),
+  ('log_reg', LogisticRegression(max_iter=5000, multi_class='ovr', solver='saga', penalty='l1', C=0.1))
+])
+model.fit(X_train, y_train)
 def predict_label(raw_data):
     """ 
     Predict probabilities for a given raw unprocessed data. 
     """
     raw_data = np.array(raw_data).reshape(1, -1)
-    probabilities = log_reg.predict_proba(raw_data)
+    probabilities = model.predict_proba(raw_data)
     return probabilities[0]
