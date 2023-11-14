52b14c9aa2cc431ad8e1bea64f78d169d7c130c0
philipmit <pschro@mit.edu>
2023-11-13 19:10:03 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    Based on the error message, it seems that the number of classes in the true labels (y_true) does not match the number of columns in the prediction results (y_score). Since we train Logistic Regression model on y_train, the prediction results will only have as many columns as there are unique classes in y_train.
    
    In the provided training split, there might be a situation where some of the classes are not represented. To fix the issue, we need to ensure that the split is stratified such that all classes are represented both in the training and testing sets.
    
    Here's the corrected Python code, applying stratified split:
    
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn import preprocessing
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1].to_numpy()
    y = ecoli.iloc[:, -1]
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
    
    # Split the dataset into training and testing sets using stratified split
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
    
    # Train the Logistic Regression model
    log_reg = LogisticRegression(max_iter=200)
    log_reg.fit(X_train, y_train)
    
    def predict_label(data):
        # Assumes data is a np array with shape (no. of features)
        preprocessed_data = data.reshape(1, -1)
        # Predict probabilities and squeeze off the extra dimension
        probabilities = np.squeeze(log_reg.predict_proba(preprocessed_data))
        return probabilities
    ```
    
    In the model training, I tuned the parameter `max_iter` considering that the default value might not be sufficient for the model to converge.

--- UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
@@ -6,16 +6,14 @@ from sklearn import preprocessing
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-# All rows, all columns except the last one
 X = ecoli.iloc[:, 1:-1].to_numpy()
-# All rows, only the last column
 y = ecoli.iloc[:, -1]
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Split the dataset into training and testing sets using stratified split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
 # Train the Logistic Regression model
-log_reg = LogisticRegression()
-log_reg.fit(X_train, y_train) 
+log_reg = LogisticRegression(max_iter=200)
+log_reg.fit(X_train, y_train)
 def predict_label(data):
     # Assumes data is a np array with shape (no. of features)
     preprocessed_data = data.reshape(1, -1)
