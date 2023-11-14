fcca406547fa7cd00220151de6149288499f213b
philipmit <pschro@mit.edu>
2023-11-13 23:57:18 -0500

    Output 6 (phase 2): Response to AttributeError
    Error from previous code:
      File "<stdin>", line 147, in <module>
      File "<string>", line 15, in <module>
    <class 'AttributeError'>
    'numpy.ndarray' object has no attribute 'to_numpy'
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # All rows, all columns except the last one for features
    # All rows, only the last column for the target
    X = ecoli.iloc[:, 1:-1].values
    y = ecoli.iloc[:, -1].values
    
    # replace strings with numbers in y
    unique_classes = np.unique(y)
    for i, class_name in enumerate(unique_classes):
        y[y == class_name] = i
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(
        X, y, test_size=0.5, random_state=42)
    
    # Train a Logistic Regression Model
    log_reg = LogisticRegression(max_iter=1000, multi_class='ovr')
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_data):
        """ Predict probabilities for a given raw unprocessed data. """
    
        raw_data = np.array(raw_data).reshape(1, -1)
        probabilities = log_reg.predict_proba(raw_data)
        return probabilities[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
@@ -1,25 +1,27 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-# Load the dataset
-ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+# Load the Ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
+                    delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
-y = ecoli.iloc[:, -1]   # All rows, only the last column
-# Encode class labels to integers to avoid any inconsistencies in the model training
-le = LabelEncoder()
-y = le.fit_transform(y)
-X = X.to_numpy()
-y = y.to_numpy()
+# All rows, all columns except the last one for features
+# All rows, only the last column for the target
+X = ecoli.iloc[:, 1:-1].values  
+y = ecoli.iloc[:, -1].values
+# replace strings with numbers in y
+unique_classes = np.unique(y)
+for i, class_name in enumerate(unique_classes):
+    y[y == class_name] = i
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Create logistic regression object
-logreg = LogisticRegression(multi_class='ovr', max_iter=10000)
-# Train the model using the training sets
-logreg.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=42)
+# Train a Logistic Regression Model
+log_reg = LogisticRegression(max_iter=1000, multi_class='ovr')
+log_reg.fit(X_train, y_train)
 def predict_label(raw_data):
-    # Predict probabilities
-    probabilities = logreg.predict_proba(raw_data.reshape(1, -1))
+    """ Predict probabilities for a given raw unprocessed data. """
+    raw_data = np.array(raw_data).reshape(1, -1)
+    probabilities = log_reg.predict_proba(raw_data)
     return probabilities[0]
