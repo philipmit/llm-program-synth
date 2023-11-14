6a44db7ed791e45b5fa3687be4af2d45379bdda0
philipmit <pschro@mit.edu>
2023-11-13 19:00:44 -0500

    Output 3 (phase 3): Response to improving AUC of 0.943631998253847
    AUC from previous code:
    0.943631998253847
    
    Response from GPT:
    ```
    from sklearn.datasets import load_breast_cancer
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split, GridSearchCV
    from sklearn.preprocessing import StandardScaler
    from sklearn.pipeline import make_pipeline
    from sklearn.metrics import make_scorer, roc_auc_score
    
    # Load the Breast Cancer dataset
    breast_cancer = load_breast_cancer()
    X = breast_cancer.data
    y = breast_cancer.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Prepare a pipeline with StandardScaler and LogisticRegression
    pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000))
    
    # Create a dictionary of hyperparameters to search
    # C: A smaller value of C leads to more regularization, while a larger value leads to less regularization
    grid_param = {"logisticregression__C": [0.01, 0.1, 1, 10, 100], "logisticregression__solver":["newton-cg", "lbfgs", "liblinear"]}
    
    # Initializing AUC scorer
    scorer = make_scorer(roc_auc_score, greater_is_better=True, needs_proba=True)
    
    # Initialize GridSearchCV object with the hyperparameters to search and cross-validation setting
    gcv = GridSearchCV(pipeline, grid_param, cv=5, scoring=scorer)
    
    # Fit the GridSearchCV object to the data
    gcv.fit(X_train, y_train)
    
    # Best model selected
    best_model = gcv.best_estimator_
    
    def predict_icu_mortality(raw_sample):
        # predict label's probability for a single sample
        single_sample_reshape = raw_sample.reshape(1, -1)
        prob = best_model.predict_proba(single_sample_reshape)[0, 1]
        return prob
    ```
    In this code, I used `GridSearchCV` to tune the hyperparameters of the Logistic Regression model. I adjusted the inverse of regularization strength parameter `C`, and logistic regression solver 'solver'. The Grid Search perform exhaustive search over the specified parameter values and the best parameters are chosen based on the cross-validation score, with the aim to improve the model's performance and therefore the AUC score.

--- UCI_benchmarks/gpt_synth_breast_cancer_test_set01/test13.py
+++ UCI_benchmarks/gpt_synth_breast_cancer_test_set01/test13.py
@@ -1,21 +1,30 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics import make_scorer, roc_auc_score
 # Load the Breast Cancer dataset
 breast_cancer = load_breast_cancer()
 X = breast_cancer.data
 y = breast_cancer.target
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Make a pipeline that standardizes the input features and trains a Logistic Regression model
-# Higher penalty leads to more regularization, smaller C --> more regularization (C is inverse of reg. strength)
-model = make_pipeline(StandardScaler(), LogisticRegression(C=0.01, max_iter=5000))
-# Train the model with the training data
-model.fit(X_train, y_train)
+# Prepare a pipeline with StandardScaler and LogisticRegression
+pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000))
+# Create a dictionary of hyperparameters to search
+# C: A smaller value of C leads to more regularization, while a larger value leads to less regularization
+grid_param = {"logisticregression__C": [0.01, 0.1, 1, 10, 100], "logisticregression__solver":["newton-cg", "lbfgs", "liblinear"]}
+# Initializing AUC scorer
+scorer = make_scorer(roc_auc_score, greater_is_better=True, needs_proba=True)
+# Initialize GridSearchCV object with the hyperparameters to search and cross-validation setting
+gcv = GridSearchCV(pipeline, grid_param, cv=5, scoring=scorer)
+# Fit the GridSearchCV object to the data
+gcv.fit(X_train, y_train)
+# Best model selected
+best_model = gcv.best_estimator_
 def predict_icu_mortality(raw_sample):
     # predict label's probability for a single sample
     single_sample_reshape = raw_sample.reshape(1, -1)
-    prob = model.predict_proba(single_sample_reshape)[0, 1]
+    prob = best_model.predict_proba(single_sample_reshape)[0, 1]
     return prob
