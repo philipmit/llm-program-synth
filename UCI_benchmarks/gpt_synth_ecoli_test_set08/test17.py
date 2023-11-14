4b8d828ffac5980de45975fd010c0bc814fab628
philipmit <pschro@mit.edu>
2023-11-14 10:54:56 -0500

    Output 7 (phase 3): Response to improving AUC of 0.8508468480915071
    AUC from previous code:
    0.8508468480915071
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.pipeline import make_pipeline
    from sklearn.model_selection import GridSearchCV
    from sklearn.ensemble import RandomForestClassifier
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Apply label encoding to the target variable
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y.astype(str))
    
    # Split data into train and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create a pipeline that scales the data and then trains a RandomForest model
    pipeline = make_pipeline(
        StandardScaler(),
        RandomForestClassifier(n_estimators=1000, random_state=42)
    )
    
    # Use GridSearchCV to find the best hyperparameters
    param_grid = {
        'randomforestclassifier__max_depth': [3, 5, 10, 20],
        'randomforestclassifier__min_samples_split': [2, 5, 10]
    }
    grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='roc_auc')
    grid_search.fit(X_train, y_train)
    
    # Get the best model
    model = grid_search.best_estimator_
    
    def predict_label(raw_sample):
        # Preprocess the input
        sample = np.array(raw_sample).reshape(1, -1)
    
        # Use the trained Random Forest model to predict the probabilities
        predicted_probabilities = model.predict_proba(sample)
    
        # Adding probabilities for missing classes
        full_proba = np.zeros(len(label_encoder.classes_))
        for class_idx, proba in zip(model.classes_, predicted_probabilities[0]):
            full_proba[class_idx] = proba
    
        return full_proba
    ```
    
    In this improved version, I replaced the Logistic Regression model with a Random Forest model. Random Forest has the advantage of handling a wide variety of data without the need for scaling. This method also prevents overfitting most of the time, by creating random subsets of the features and building smaller trees using these subsets, which often leads to better prediction accuracy. In addition, I added a new parameter `scoring='roc_auc'` to the GridSearchCV object, this makes the grid search objective to maximize the Area Under the Receiver Operating Characteristic Curve (ROC AUC) score which is suitable for binary classification tasks and should improve the overall performance of the model.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test17.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test17.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -16,22 +17,24 @@ label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y.astype(str))
 # Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Create a pipeline that scales the data and then trains a logistic regression model
+# Create a pipeline that scales the data and then trains a RandomForest model
 pipeline = make_pipeline(
     StandardScaler(),
-    LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=1000)
+    RandomForestClassifier(n_estimators=1000, random_state=42)
 )
 # Use GridSearchCV to find the best hyperparameters
-param_grid = {'logisticregression__C': [0.001, 0.01, 0.1, 1, 10, 100],
-              'logisticregression__penalty': ['l1', 'l2']}
-grid_search = GridSearchCV(pipeline, param_grid, cv=5)
+param_grid = {
+    'randomforestclassifier__max_depth': [3, 5, 10, 20],
+    'randomforestclassifier__min_samples_split': [2, 5, 10]
+}
+grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='roc_auc')
 grid_search.fit(X_train, y_train)
 # Get the best model
 model = grid_search.best_estimator_
 def predict_label(raw_sample):
     # Preprocess the input
     sample = np.array(raw_sample).reshape(1, -1)
-    # Use the trained logistic regression model to predict the probabilities
+    # Use the trained Random Forest model to predict the probabilities
     predicted_probabilities = model.predict_proba(sample)
     # Adding probabilities for missing classes
     full_proba = np.zeros(len(label_encoder.classes_))
