d7344cb15f268c022fac52aa5ca1ae8a836045a2
philipmit <pschro@mit.edu>
2023-11-13 23:17:58 -0500

    Initial output: Sure, here's the code you can use to create a logistic regression model and a function to predict the label for a single input sample:
    ```python
    import numpy as np
    import pandas as pd
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize Logistic Regression model
    model = LogisticRegression(max_iter=1000)
    
    # Fit the model to the training data
    model.fit(X_train, y_train)
    
    # Function that predicts label for a single input sample
    def predict_label(sample):
        # Use the trained model to predict the sample label probabilities
        proba = model.predict_proba(sample.reshape(1, -1))
        return proba
    ```
    This `predict_label()` function takes in a single sample (a numpy array). This sample needs to be reshaped into a 2D array before being passed to the `predict_proba()` method of the LogisticRegression model since it expects a 2D array as input. This function then returns the predicted probabilities of each class for that sample.

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+# Load the dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
+                    delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# replace strings with numbers in y
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X=X.to_numpy()
+y=y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Initialize Logistic Regression model
+model = LogisticRegression(max_iter=1000)
+# Fit the model to the training data
+model.fit(X_train, y_train)
+# Function that predicts label for a single input sample
+def predict_label(sample):
+    # Use the trained model to predict the sample label probabilities
+    proba = model.predict_proba(sample.reshape(1, -1))
+    return proba
