ed8a1ed7f5ba4b2330be1d5d6ef87895208125b8
philipmit <pschro@mit.edu>
2023-11-13 22:06:55 -0500

    Initial output: Here's the final code for you:
    ```python
    import numpy as np
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the data
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    # Train the logistic regression model
    clf = LogisticRegression(random_state=0, multi_class='ovr')
    clf.fit(X_train, y_train)
    
    # Define a function that predicts label for new data
    def predict_label(sample):
        sample = np.array(sample).reshape(1,-1)
        sample = sc.transform(sample)  # We need to apply the same scaling to the new data
        probabilities = clf.predict_proba(sample)
        return probabilities
    ```
    
    This will define a function called predict_label, which you can use to predict the probabilities associated with a new data sample.
    
    Please note that the path '/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data' is used to load the Ecoli dataset, you need to replace it with the path where your Ecoli dataset file is located.
    
    Remember that this function expects a sample to be a list or numpy array with 7 values, corresponding to the features 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2'. The feature scaling that is part of the process (for the StandardScaler object) is included in the function, so you don't have to worry about doing this yourself.

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the Ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]
+y = ecoli.iloc[:, -1]
+# Replace strings with numbers in y
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X = X.to_numpy()
+y = y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Scale the data
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+# Train the logistic regression model
+clf = LogisticRegression(random_state=0, multi_class='ovr')
+clf.fit(X_train, y_train)
+# Define a function that predicts label for new data
+def predict_label(sample):
+    sample = np.array(sample).reshape(1,-1)
+    sample = sc.transform(sample)  # We need to apply the same scaling to the new data
+    probabilities = clf.predict_proba(sample)
+    return probabilities
