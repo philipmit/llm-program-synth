db8b52c105676437081e18df29f0033e8c36dc37
philipmit <pschro@mit.edu>
2023-11-13 23:19:46 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    -------------------------------------
    Response 2 from GPT:
    ```
    import numpy as np
    import pandas as pd
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    from sklearn.preprocessing import OneHotEncoder
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]    # All rows, only the last column
    
    # replace strings with numbers in y
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    
    # make a OneHotEncoder and fit it to the known classes
    enc = OneHotEncoder()
    enc.fit(y.reshape(-1, 1))
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize Logistic Regression model
    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
    
    # Fit the model to the training data
    model.fit(X_train, y_train)
    
    def predict_label(sample):
        # the predict_proba function returns probabilities per class
        proba = model.predict_proba(sample.reshape(1, -1))
    
        # we need to duplicate the probability array to match the same form as the OneHotEncoder
        proba = np.repeat(proba, enc.transform([[0]]).shape[1])
    
        return proba
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
@@ -2,14 +2,20 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]    # All rows, only the last column
-# Replace strings with numbers in y
-y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+# replace strings with numbers in y
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+# make a OneHotEncoder and fit it to the known classes
+enc = OneHotEncoder()
+enc.fit(y.reshape(-1, 1))
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the dataset into training and testing sets
@@ -18,7 +24,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
 # Fit the model to the training data
 model.fit(X_train, y_train)
-# Function that predicts label for a single input sample
 def predict_label(sample):
+    # the predict_proba function returns probabilities per class
     proba = model.predict_proba(sample.reshape(1, -1))
-    return proba[0]
+    # we need to duplicate the probability array to match the same form as the OneHotEncoder
+    proba = np.repeat(proba, enc.transform([[0]]).shape[1])
+    return proba
