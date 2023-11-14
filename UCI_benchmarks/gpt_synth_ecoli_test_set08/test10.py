0a0795e542a9664f19c7d02526ac2563c9767984
philipmit <pschro@mit.edu>
2023-11-13 23:27:20 -0500

    Output 14 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    It seems that we are predicting only a single column, whereas our y_true contains multiple classes. The error may have been caused by the fact that we are not predicting probabilities for each class during prediction. So we will update the predict_label function to return the probabilities for each class instead of a single column.
    
    The issue is arising because the predict_proba method in our logistic regression model is returning the probabilities related to each of the classes as an array, not a single value like predict would do.
    
    The code block below performs the Logistic Regression model training as well as defines the new predict_label function with the fix implemented:
    
    ```
    import numpy as np
    import pandas as pd
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Get features and labels
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the first and last
    y = ecoli.iloc[:, -1]    # All rows, only the last column
    
    # Replace string labels with numeric labels
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    
    # Convert pandas DataFrame to NumPy arrays
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize Logistic Regression model
    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
    model.fit(X_train, y_train)
    
    # Function to predict labels
    def predict_label(sample):
        # The sample is already all features, without the labels or 'Sequence Name'
        sample_reshaped = np.array(sample).reshape(1, -1)
        # Calculate probability for each class
        predicted_probabilities = model.predict_proba(sample_reshaped)
        # Return the array of probabilities
        return predicted_probabilities[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
@@ -2,14 +2,16 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Get features and labels
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the first and last 
 y = ecoli.iloc[:, -1]    # All rows, only the last column
-# replace strings with numbers in y
-y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+# Replace string labels with numeric labels
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
 # Convert pandas DataFrame to NumPy arrays
 X = X.to_numpy()
 y = y.to_numpy()
@@ -19,9 +21,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
 model.fit(X_train, y_train)
 # Function to predict labels
-def predict_label(single_sample):
+def predict_label(sample):
     # The sample is already all features, without the labels or 'Sequence Name'
-    sample_reshaped = np.array(single_sample).reshape(1, -1)
+    sample_reshaped = np.array(sample).reshape(1, -1)
     # Calculate probability for each class
     predicted_probabilities = model.predict_proba(sample_reshaped)
     # Return the array of probabilities
