da39553c2d036f1fcda160e07712a72028426ef4
philipmit <pschro@mit.edu>
2023-11-13 19:00:40 -0500

    Initial output: ``` from sklearn.datasets import load_wine from sklearn.model_selection import train_test_split from sklearn.linear_model import LogisticRegression from sklearn.preprocessing import StandardScaler
    # Load the wine dataset
    wine = load_wine()
    X = wine.data
    y = wine.target
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Normalize the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    X_test = scaler.transform(X_test)
    
    # Train the logistic regression model
    model = LogisticRegression(multi_class='ovr', solver='liblinear')
    model.fit(X_train, y_train)
    
    def predict_label(raw_sample):
        # Process the raw sample
        processed_sample = scaler.transform([raw_sample])
    
        # Return the predicted probabilities
        return model.predict_proba(processed_sample)
    ```

---
+++ UCI_benchmarks/gpt_synth_wine_test_set01/test17.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Normalize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+# Train the logistic regression model
+model = LogisticRegression(multi_class='ovr', solver='liblinear')
+model.fit(X_train, y_train)
+def predict_label(raw_sample):
+    # Process the raw sample
+    processed_sample = scaler.transform([raw_sample])
+    # Return the predicted probabilities
+    return model.predict_proba(processed_sample)
