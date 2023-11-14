1554401ba631382894b0cdf5e0ecd17b49ef929f
philipmit <pschro@mit.edu>
2023-11-13 19:10:45 -0500

    Output 4 (phase 3): Response to improving AUC of 0.9425393795721805
    AUC from previous code:
    0.9425393795721805
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Random Seed for reproducibility
    np.random.seed(42)
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare feature matrix X and target vector y
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1].replace(np.unique(y), list(range(len(np.unique(y)))))
    
    # Split the dataset into training and testing sets using stratified split
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
    
    # Normalization of values in train set for improved model convergence
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Creating an instance of Logistic Regression with increased iteration count and L2 regularization
    model = LogisticRegression(max_iter=1000, penalty='l2')
    
    # Fitting the data to the model
    model.fit(X_train, y_train)
    
    # Define the function
    def predict_label(raw_data):
        # Data preprocessing
        ready_data = scaler.transform(raw_data.reshape(1, -1))
        # Perform prediction
        probabilities = model.predict_proba(ready_data)
        return probabilities
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
@@ -2,21 +2,28 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn import preprocessing
-# Load the ecoli dataset
+from sklearn.preprocessing import StandardScaler
+# Random Seed for reproducibility
+np.random.seed(42)
+# Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1].to_numpy()
-y = ecoli.iloc[:, -1]
-y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
+# Prepare feature matrix X and target vector y
+X = ecoli.iloc[:, 1:-1]
+y = ecoli.iloc[:, -1].replace(np.unique(y), list(range(len(np.unique(y)))))
 # Split the dataset into training and testing sets using stratified split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
-# Train the Logistic Regression model
-log_reg = LogisticRegression(max_iter=200)
-log_reg.fit(X_train, y_train)
-def predict_label(data):
-    # Assumes data is a np array with shape (no. of features)
-    preprocessed_data = data.reshape(1, -1)
-    # Predict probabilities and squeeze off the extra dimension
-    probabilities = np.squeeze(log_reg.predict_proba(preprocessed_data))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+# Normalization of values in train set for improved model convergence
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Creating an instance of Logistic Regression with increased iteration count and L2 regularization
+model = LogisticRegression(max_iter=1000, penalty='l2')
+# Fitting the data to the model
+model.fit(X_train, y_train)
+# Define the function
+def predict_label(raw_data):
+    # Data preprocessing
+    ready_data = scaler.transform(raw_data.reshape(1, -1))
+    # Perform prediction
+    probabilities = model.predict_proba(ready_data)
     return probabilities
