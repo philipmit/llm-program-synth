bbed7e7125b6c4cdcccdd56718efd0b7b8e55538
philipmit <pschro@mit.edu>
2023-11-14 11:27:34 -0500

    Output 5 (phase 3): Response to improving AUC of 0.8544806776265685
    AUC from previous code:
    0.8544806776265685
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder, StandardScaler
    from sklearn.pipeline import make_pipeline
    from sklearn.ensemble import RandomForestClassifier
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1].values    # All rows, all columns except the last one
    y = ecoli.iloc[:, -1].values      # All rows, only the last column
    
    # Preprocess y using Label Encoder
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize features by removing the mean and scaling to unit variance
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    X_test = scaler.transform(X_test)
    
    # Train RandomForestClassifier instead of LogisticRegression to enhance predictive power
    rf_clf = RandomForestClassifier(n_estimators=200, max_depth=None,
                                    random_state=0)
    # Fit
    rf_clf.fit(X_train, y_train)
    
    # Predict
    def predict_label(sample):
        sample = np.array(sample).reshape(1, -1)
        # Scale the sample
        sample = scaler.transform(sample)
        predicted_probabilities = rf_clf.predict_proba(sample)
        # Create an array to hold probabilities for each class
        probabilities = np.zeros(len(le.classes_))
        # Assign predicted probabilities to respective index
        probabilities[rf_clf.classes_] = predicted_probabilities[0]
        return probabilities
    ```
    This new approach uses a RandomForestClassifier model instead of LogisticRegression for training. RandomForest Classifier generally has higher predictive power due to its ensemble learning method. It creates multiple decision trees and combines their outputs. Also, RandomForest manages the bias-variance tradeoff very well, which results in improved generalization.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
@@ -2,27 +2,37 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1].values    # All rows, all columns except the last one
-y = ecoli.iloc[:, -1].values   # All rows, only the last column
-# Replace strings with numbers in y using label encoder
+y = ecoli.iloc[:, -1].values      # All rows, only the last column
+# Preprocess y using Label Encoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-# Split the dataset into training and testing sets
+# Split dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Create a logistic regression model and fit with training data
-log_reg = LogisticRegression(max_iter=2000, multi_class='ovr')
-log_reg.fit(X_train, y_train)
-# Function to predict label
+# Standardize features by removing the mean and scaling to unit variance
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+# Train RandomForestClassifier instead of LogisticRegression to enhance predictive power
+rf_clf = RandomForestClassifier(n_estimators=200, max_depth=None, 
+                                random_state=0)
+# Fit
+rf_clf.fit(X_train, y_train)
+# Predict
 def predict_label(sample):
     sample = np.array(sample).reshape(1, -1)
-    predicted_probabilities = log_reg.predict_proba(sample)
+    # Scale the sample
+    sample = scaler.transform(sample)
+    predicted_probabilities = rf_clf.predict_proba(sample)
     # Create an array to hold probabilities for each class
     probabilities = np.zeros(len(le.classes_))
     # Assign predicted probabilities to respective index
-    probabilities[log_reg.classes_] = predicted_probabilities[0]
+    probabilities[rf_clf.classes_] = predicted_probabilities[0]
     return probabilities
