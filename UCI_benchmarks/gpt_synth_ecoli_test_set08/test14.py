90a192bfb5713f1d9a5f763962fab415f3e48803
philipmit <pschro@mit.edu>
2023-11-14 00:04:29 -0500

    Output 14 (phase 3): Response to improving AUC of 0.9525937658038366
    AUC from previous code:
    0.9525937658038366
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.pipeline import Pipeline
    from sklearn.feature_selection import SelectFromModel
    from sklearn.metrics import roc_auc_score
    from xgboost import XGBClassifier
    from sklearn.model_selection import GridSearchCV
    from sklearn.ensemble import GradientBoostingClassifier
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Prepare the data
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    # Transform y from categorical to numerical values
    le = LabelEncoder()
    y = le.fit_transform(y)
    # Stratified Split of data into Training and Test set ensuring all classes are represented
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
    # Training a Gradient Boosting model with feature selection and data standardization
    # Use GridSearchCV to optimize hyperparameters
    param_grid = {
        'learning_rate': [0.01, 0.1, 0.5, 1],
        'n_estimators': [100, 200, 500],
        'max_depth': [3, 5, 10]
    }
    gb = GradientBoostingClassifier(random_state=42)
    clf = GridSearchCV(gb, param_grid, cv=5)
    clf.fit(X_train, y_train)
    # Get the best model
    best_gb = clf.best_estimator_
    model = Pipeline([
      ('standardize', StandardScaler()),
      ('feature_selection', SelectFromModel(best_gb)),
      ('gradient_boost', best_gb)
    ])
    model.fit(X_train, y_train)
    def predict_label(raw_data):
        """
        Predict probabilities for a given raw unprocessed data.
        """
        raw_data = np.array(raw_data).reshape(1, -1)
        probabilities = model.predict_proba(raw_data)
        return probabilities[0]
    ```
    This new model utilizes a Gradient Boosting Classifier which is a powerful ensemble learning algorithm that could potentially provide better performance than a single Logistic Regression model. The code also includes hyperparameter tuning via GridSearchCV to optimize the model's parameters. Also, the model pipeline now includes standardization as the first step, feature selection using the trained model, followed by the prediction. The train-test split size is also optimized to 80-20 to give the model extra training data which might help improve AUC.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
@@ -4,10 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import RFECV
+from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import roc_auc_score
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -18,19 +19,23 @@ y = ecoli.iloc[:, -1]
 le = LabelEncoder()
 y = le.fit_transform(y)
 # Stratified Split of data into Training and Test set ensuring all classes are represented
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
-# Training a logistic regression model with feature selection and data standardization
-# Use GridSearchCV to optimize logistic regression hyperparameters
-param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100], 'penalty': ['l1', 'l2']}
-lr = LogisticRegression(multi_class='ovr', solver='saga', max_iter=5000)
-clf = GridSearchCV(lr, param_grid, cv=5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+# Training a Gradient Boosting model with feature selection and data standardization
+# Use GridSearchCV to optimize hyperparameters
+param_grid = {
+    'learning_rate': [0.01, 0.1, 0.5, 1],
+    'n_estimators': [100, 200, 500],
+    'max_depth': [3, 5, 10]
+}
+gb = GradientBoostingClassifier(random_state=42)
+clf = GridSearchCV(gb, param_grid, cv=5)
 clf.fit(X_train, y_train)
 # Get the best model
-best_lr = clf.best_estimator_
+best_gb = clf.best_estimator_
 model = Pipeline([
-  ('feature_selection', RFECV(best_lr)),
   ('standardize', StandardScaler()),
-  ('log_reg', best_lr)
+  ('feature_selection', SelectFromModel(best_gb)),
+  ('gradient_boost', best_gb)
 ])
 model.fit(X_train, y_train)
 def predict_label(raw_data):
