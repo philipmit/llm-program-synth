560db860d0c73accb80e7be48f1b663ff9ed56da
philipmit <pschro@mit.edu>
2023-11-14 11:18:34 -0500

    Initial output: ```python import pandas as pd import numpy as np from sklearn.linear_model import LogisticRegression from sklearn.model_selection import train_test_split from sklearn.preprocessing import MinMaxScaler
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Replace strings with numbers in y
    np.unique(y)
    len(list(np.unique(y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Feature scaling for normalization
    scaler = MinMaxScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the model
    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
    model.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(sample):
        global scaler, model
        sample = np.array(sample).reshape(1, -1)
        sample = scaler.transform(sample)
        prediction = model.predict_proba(sample)
        return prediction
    ```
    The function `predict_label` will accept a raw input sample and return the predicted probabilities after preprocessing the input and running it through the logistic regression model. Remember that the input to this function should be a 1-dimensional array-like object containing the predictor variables for a single Ecoli sample.

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+# Load the ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# Replace strings with numbers in y
+np.unique(y)
+len(list(np.unique(y)))
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X=X.to_numpy()
+y=y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Feature scaling for normalization
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+# Train the model
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+model.fit(X_train, y_train)
+# Define the predict_label function
+def predict_label(sample):
+    global scaler, model
+    sample = np.array(sample).reshape(1, -1)
+    sample = scaler.transform(sample)
+    prediction = model.predict_proba(sample)
+    return prediction
