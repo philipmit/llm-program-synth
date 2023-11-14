c74cc1ff03387c2e16048f47e7b1318c2298ba27
philipmit <pschro@mit.edu>
2023-11-14 11:25:10 -0500

    Output 2 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 605, in roc_auc_score
        y_score = check_array(y_score, ensure_2d=False)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 953, in check_array
        raise ValueError(
    <class 'ValueError'>
    Found array with dim 3. None expected <= 2.
    
    Response from GPT:
    The error occurs because the `predict_label` function, when called in the validation script, returns a nested list (3D array) for each prediction rather than a 1D array of probabilities. To solve this, ensure that the `predict_label` function outputs just a single list of probabilities for each sample.
    
    Below is the corrected code:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Replace strings with numbers in y
    np.unique(y)
    len(list(np.unique(y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create a logistic regression model and fit with training data
    log_reg = LogisticRegression(max_iter = 2000)
    log_reg.fit(X_train, y_train)
    
    # Function to predict label
    def predict_label(sample):
        sample = np.array(sample).reshape(1, -1)
        return log_reg.predict_proba(sample)[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
@@ -5,15 +5,14 @@ from sklearn.linear_model import LogisticRegression
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-ecoli
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
 # Replace strings with numbers in y
 np.unique(y)
 len(list(np.unique(y)))
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
-X=X.to_numpy()
-y=y.to_numpy()
+X = X.to_numpy()
+y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Create a logistic regression model and fit with training data
@@ -22,4 +21,4 @@ log_reg.fit(X_train, y_train)
 # Function to predict label
 def predict_label(sample):
     sample = np.array(sample).reshape(1, -1)
-    return log_reg.predict_proba(sample)
+    return log_reg.predict_proba(sample)[0]
