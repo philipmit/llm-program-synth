939a7a184dd8303428d77264f2cebe7320824ecf
philipmit <pschro@mit.edu>
2023-11-13 23:15:44 -0500

    Output 5 (phase 3): Response to improving AUC of 0.9601844518722107
    AUC from previous code:
    0.9601844518722107
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    from sklearn.pipeline import make_pipeline
    
    # Load dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Split into predictors and target
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Convert class labels to numeric
    y_labels = np.unique(y)
    y = y.replace(y_labels, np.arange(len(y_labels)))
    
    # Split the data, stratifying on y to ensure balance
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
    
    # Define pipeline (with not only standard scaling but also PCA for dimension reduction)
    from sklearn.decomposition import PCA
    pipeline = make_pipeline(StandardScaler(), PCA(n_components = 5), LogisticRegression(multi_class='ovr', max_iter=500, class_weight='balanced', solver='liblinear'))
    
    # Use the pipeline to fit and predict
    pipeline.fit(X_train, y_train)
    
    # Define prediction function
    def predict_label(raw_sample):
        # Reshape the sample
        raw_sample = raw_sample.reshape(1, -1)
        # Predict the probabilities for each class
        probabilities = pipeline.predict_proba(raw_sample)
        return probabilities[0]
    ```
    
    In this version of the code, incorporated a few techniques to try and improve the model performance:
    
     - Added PCA to the pipeline for dimension Reduction.
     - Increased the maximum number of iterations in Logistic Regression to 500.
     - Used 'liblinear' as the solver in Logistic Regression.
     - Added 'class_weight' parameter as 'balanced' in Logistic Regression which helps in handling unbalanced datasets.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test09.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test09.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
 # Load dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -14,19 +15,15 @@ y_labels = np.unique(y)
 y = y.replace(y_labels, np.arange(len(y_labels)))
 # Split the data, stratifying on y to ensure balance
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
-# Standardize the data
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-# Define and train model
-model = LogisticRegression(multi_class='ovr', max_iter=200)
-model.fit(X_train, y_train)
+# Define pipeline (with not only standard scaling but also PCA for dimension reduction)
+from sklearn.decomposition import PCA
+pipeline = make_pipeline(StandardScaler(), PCA(n_components = 5), LogisticRegression(multi_class='ovr', max_iter=500, class_weight='balanced', solver='liblinear'))
+# Use the pipeline to fit and predict
+pipeline.fit(X_train, y_train)
 # Define prediction function
 def predict_label(raw_sample):
     # Reshape the sample
     raw_sample = raw_sample.reshape(1, -1)
-    # Standardize the raw sample
-    raw_sample = scaler.transform(raw_sample)
     # Predict the probabilities for each class
-    probabilities = model.predict_proba(raw_sample)
+    probabilities = pipeline.predict_proba(raw_sample)
     return probabilities[0]
