835aca4d206e3c3aa6379bc65ade9f6e5e1e985b
philipmit <pschro@mit.edu>
2023-11-13 23:54:08 -0500

    Initial output: Here is the complete code that fulfills your requirements:
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # replace strings with numbers in y
    np.unique( y)
    len(list(np.unique( y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create logistic regression object
    logreg = LogisticRegression(max_iter=10000)
    
    # Train the model using the training sets
    logreg.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Predict probabilities
        probabilities = logreg.predict_proba(raw_data.reshape(1, -1))
        return probabilities
    ```

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+# Load the ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# replace strings with numbers in y
+np.unique( y)
+len(list(np.unique( y)))
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X=X.to_numpy()
+y=y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Create logistic regression object
+logreg = LogisticRegression(max_iter=10000)
+# Train the model using the training sets
+logreg.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Predict probabilities
+    probabilities = logreg.predict_proba(raw_data.reshape(1, -1))
+    return probabilities
