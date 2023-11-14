1414a74974996a1e45070bdcd8876831b4dc853f
philipmit <pschro@mit.edu>
2023-11-13 22:09:56 -0500

    Output 5 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The provided error message 'Number of classes in y_true not equal to the number of columns in 'y_score'' typically occurs when the model is trying to predict a different number of class probabilities than actually exist in the data.
    
    Looking at the original code you've provided, it seems like your label data consists of 8 classes (as per replacement operation using list(np.unique(y))). However, during model training, "LabelBinarizer()" is used, which might not be correctly identifying all the classes.
    
    I'll modify the section that binarizes the label to use the unique classes we've identified from 'y' and ensure that our logistic regression model accounts for multiclass data correctly. Here's the adjusted code:
    
    ```python
    import numpy as np
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Replace strings with numbers in y
    classes = list(np.unique(y))
    y = y.replace(classes, list(range(len(classes))))
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the logistic regression model with 'multinomial' option for multi-class
    clf = LogisticRegression(random_state=0, multi_class='multinomial', max_iter=1000)
    clf.fit(X_train, y_train)
    
    # Define a function that predicts label for new data
    def predict_label(sample):
        sample = np.array(sample).reshape(1,-1)
        sample = scaler.transform(sample)  # We need to apply the same scaling to the new data
        probabilities = clf.predict_proba(sample)
        return probabilities[0]
    ```
    
    This updated script should now function correctly with the validation code you provided.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
@@ -2,30 +2,28 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler, LabelBinarizer
+from sklearn.preprocessing import StandardScaler
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]
 y = ecoli.iloc[:, -1]
-# Convert labels into binary format
-label_binarizer = LabelBinarizer()
-y = label_binarizer.fit_transform(y)
-# Compress the 2D array into 1D
-y = np.array([np.argmax(y[i]) for i in range(len(y))])
+# Replace strings with numbers in y
+classes = list(np.unique(y))
+y = y.replace(classes, list(range(len(classes))))
 X = X.to_numpy()
+y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Scale the data
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Train the logistic regression model
-clf = LogisticRegression(random_state=0, multi_class='ovr')
+# Train the logistic regression model with 'multinomial' option for multi-class
+clf = LogisticRegression(random_state=0, multi_class='multinomial', max_iter=1000)
 clf.fit(X_train, y_train)
 # Define a function that predicts label for new data
 def predict_label(sample):
     sample = np.array(sample).reshape(1,-1)
     sample = scaler.transform(sample)  # We need to apply the same scaling to the new data
     probabilities = clf.predict_proba(sample)
-    probabilities = probabilities.tolist()
     return probabilities[0]
