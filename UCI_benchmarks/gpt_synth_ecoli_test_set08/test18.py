0f4b0da13bb79892bf4ffa78078b1324295d50f6
philipmit <pschro@mit.edu>
2023-11-14 11:17:44 -0500

    Output 6 (phase 3): Response to improving AUC of 0.7247715630594651
    AUC from previous code:
    0.7247715630594651
    
    Response from GPT:
    ```
    import pandas as pd
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.pipeline import make_pipeline
    from sklearn.feature_selection import SelectFromModel
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.metrics import roc_auc_score
    import numpy as np
    
    # Load Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare predictors and target variables
    X = ecoli.iloc[:, 1:-1].copy()
    y = ecoli.iloc[:, -1].copy()
    
    # Use label encoder to transform textual classes/labels to numerical
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Use pipeline to handle preprocessing steps including feature selection and scaling
    pipe = make_pipeline(SelectFromModel(estimator=RandomForestClassifier(n_estimators=100)),
                         StandardScaler(),
                         LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000))
    
    # Train a logistic regression model
    clf = pipe.fit(X_train, y_train)
    
    def predict_label(raw_sample):
        # Preprocessing the single raw sample
        sample = np.array(raw_sample).reshape(1, -1)
    
        # Use clf to predict the sample's probabilities
        predicted_probabilities = clf.predict_proba(sample)
    
        # Return an array of zeroes for classes not present in the training set
        full_pred = np.zeros(len(le.classes_))
        full_pred[:predicted_probabilities.shape[1]] = predicted_probabilities[0]
    
        return full_pred
    ```
    This revised version of code includes few of advanced techniques to improve the AUC score:
    
    1. Feature Selection: Before modeling, a feature selection step is included using `SelectFromModel` with RandomForestClassifier. This step is to select important features which will simplify our model, improve accuracy and reduce overfitting.
    
    2. Increase iterations: The max_iter parameter in LogisticRegression model is increased to 1000. Increasing number of iterations can give model more probability to find the best values of parameters.
    
    3. Pipelines: It is used to assemble several steps like feature selection, scalar transformation and classifier model that can be cross-validated together.
    
    These changes has the potential to result in a higher logistic regression model and hence a higher validation AUC score.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test18.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test18.py
@@ -1,10 +1,13 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
 import numpy as np
-# Load the Ecoli dataset
+# Load Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Prepare predictors and target variables
@@ -15,18 +18,17 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Scale features
-scaler = StandardScaler().fit(X_train)
-X_train_scaled = scaler.transform(X_train)
+# Use pipeline to handle preprocessing steps including feature selection and scaling
+pipe = make_pipeline(SelectFromModel(estimator=RandomForestClassifier(n_estimators=100)),
+                     StandardScaler(),
+                     LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000))
 # Train a logistic regression model
-clf = LogisticRegressionCV(cv=5, random_state=0, multi_class='multinomial').fit(X_train_scaled, y_train)
-# The predict_label function
+clf = pipe.fit(X_train, y_train)
 def predict_label(raw_sample):
     # Preprocessing the single raw sample
     sample = np.array(raw_sample).reshape(1, -1)
-    processed_sample = scaler.transform(sample)
     # Use clf to predict the sample's probabilities
-    predicted_probabilities = clf.predict_proba(processed_sample)
+    predicted_probabilities = clf.predict_proba(sample)
     # Return an array of zeroes for classes not present in the training set
     full_pred = np.zeros(len(le.classes_))
     full_pred[:predicted_probabilities.shape[1]] = predicted_probabilities[0]
