fddb92f5abb65799994701c73a6ca983fbbca352
philipmit <pschro@mit.edu>
2023-11-13 22:23:43 -0500

    Output 5 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 693, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
    
    Response from GPT:
    From the validation code, the logistic regression model is expected to make multi-class predictions in the form of probabilities that sum up to 1. In the initial provided code, the predictions do not meet this requirement because they are made in a one-vs-rest method (OvR) which doesn't ensure that the probabilities sum up to 1.
    
    You should train a logistic regression model in a multi-class setting by directly setting the `multi_class` parameter to 'multinomial' and solver to 'lbfgs'. Here is the corrected code:
    
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare the feature and target variables
    X = ecoli.iloc[:, 1:-1].values  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Transform target labels into number using LabelEncoder
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    log_reg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_data_sample):
        proba = log_reg.predict_proba(raw_data_sample.reshape(1, -1))
        return proba[0]
    ```
    
    The trained logistic regression model will make predictions in the multi-class setting, and the prediction of each sample will be a probability distribution which sums up to 1.0, as required by the provided validation code.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
@@ -2,30 +2,21 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Prepare the feature and target variables
-X = ecoli.iloc[:, 1:-1].values  # convert DataFrame to numpy array using .values instead of .to_numpy()
-y = ecoli.iloc[:, -1]
-# Transform target labels into binary array
-lb = LabelBinarizer()
-y = lb.fit_transform(y)
-# If y has only 1 column, i.e., only 2 unique labels, reshape it to (n_samples,)
-if y.shape[1] == 1:
-    y = y.reshape(-1)
+X = ecoli.iloc[:, 1:-1].values  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# Transform target labels into number using LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Train the logistic regression model
-log_reg = LogisticRegression(max_iter=1000)
-if len(y_train.shape) > 1 and y_train.shape[1] > 1:
-    # If y_train has more than 1 column, i.e., more than 2 unique labels
-    from sklearn.multiclass import OneVsRestClassifier
-    cls = OneVsRestClassifier(log_reg)
-else:
-    cls = log_reg
-cls.fit(X_train, y_train)
+log_reg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+log_reg.fit(X_train, y_train)
 def predict_label(raw_data_sample):
-    proba = cls.predict_proba(raw_data_sample.reshape(1, -1))
+    proba = log_reg.predict_proba(raw_data_sample.reshape(1, -1))
     return proba[0]
