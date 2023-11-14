e5f9bc3e66823d824769dbcc78c7a976bd2df64d
philipmit <pschro@mit.edu>
2023-11-14 10:52:54 -0500

    Output 6 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Apply label encoding to the target variable
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y.astype(str))
    
    # Split data into train and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize logistic regression model
    model = LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=1000)
    
    # Training the model
    model.fit(X_train, y_train)
    
    # Initialize standard scaler
    scaler = StandardScaler().fit(X_train)
    
    def predict_label(raw_sample):
        # Preprocess the input
        sample = np.array(raw_sample).reshape(1, -1)
        sample = scaler.transform(sample)
        # Use the trained logistic regression model to predict the probabilities
        predicted_probabilities = model.predict_proba(sample)
        # Adding probabilities for missing classes
        full_proba = np.zeros(len(label_encoder.classes_))
        for class_idx, proba in zip(model.classes_, predicted_probabilities[0]):
            full_proba[class_idx] = proba
        return full_proba
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test17.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test17.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-# Separating the features and target variable
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
-# Apply label encoding to class labels
+# Apply label encoding to the target variable
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y.astype(str))
-# No need to convert DataFrame to numpy array. It will automatically be converted in the train_test_split function.
-# Split the dataset into training and testing sets
+# Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Initialize logistic regression model
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+model = LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=1000)
 # Training the model
 model.fit(X_train, y_train)
 # Initialize standard scaler
@@ -27,4 +26,8 @@ def predict_label(raw_sample):
     sample = scaler.transform(sample)
     # Use the trained logistic regression model to predict the probabilities
     predicted_probabilities = model.predict_proba(sample)
-    return predicted_probabilities[0]
+    # Adding probabilities for missing classes
+    full_proba = np.zeros(len(label_encoder.classes_))
+    for class_idx, proba in zip(model.classes_, predicted_probabilities[0]):
+        full_proba[class_idx] = proba
+    return full_proba
