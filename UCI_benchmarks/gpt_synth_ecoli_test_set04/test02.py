3a7574099255be49df9ebabceb1d8b650da033d2
philipmit <pschro@mit.edu>
2023-11-13 19:57:17 -0500

    Output 9 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all the features
    y = ecoli.iloc[:, -1]   # All rows, the class label
    
    # replace string class labels with unique integers
    class_labels = np.unique(y)
    label_dict = {label: index for index, label in enumerate(class_labels)}
    y = y.replace(label_dict)
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the Ecoli dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Defining the scaler
    scaler = StandardScaler()
    
    # Scaling the X_train data
    X_train = scaler.fit_transform(X_train)
    
    # Define and Fit logistic regression model
    log_reg = LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=10000)
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_sample):
        # reshape raw_sample if it's only 1D (a single sample)
        if len(raw_sample.shape) == 1:
            raw_sample = raw_sample.reshape(1, -1)
        # normalize the sample
        sample = scaler.transform(raw_sample)
        # use the fitted model to predict the probabilities
        probas = log_reg.predict_proba(sample)
    
        # coerce probabilities to length of label dictionary
        if len(probas[0]) != len(label_dict):
            temp = np.zeros((len(label_dict),))
            temp[:len(probas[0])] = probas[0]
            probas[0] = temp
        return probas[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
@@ -6,21 +6,22 @@ from sklearn.linear_model import LogisticRegression
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
-y = ecoli.iloc[:, -1]   # All rows, only the last column
-# replace strings with numbers in y
-y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X = ecoli.iloc[:, 1:-1]  # All rows, all the features
+y = ecoli.iloc[:, -1]   # All rows, the class label
+# replace string class labels with unique integers
+class_labels = np.unique(y)
+label_dict = {label: index for index, label in enumerate(class_labels)}
+y = y.replace(label_dict)
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the Ecoli dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Define the scaler
+# Defining the scaler
 scaler = StandardScaler()
-# Scale the X_train data
+# Scaling the X_train data
 X_train = scaler.fit_transform(X_train)
-# Define the Logistic Regression model (Removed 'classes' argument as it is not valid)
-log_reg = LogisticRegression(multi_class='ovr', solver='liblinear')
-# Fit the model
+# Define and Fit logistic regression model
+log_reg = LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=10000)
 log_reg.fit(X_train, y_train)
 def predict_label(raw_sample):
     # reshape raw_sample if it's only 1D (a single sample)
@@ -28,7 +29,11 @@ def predict_label(raw_sample):
         raw_sample = raw_sample.reshape(1, -1)
     # normalize the sample
     sample = scaler.transform(raw_sample)
-    # use the fitted model to predict_proba
+    # use the fitted model to predict the probabilities
     probas = log_reg.predict_proba(sample)
-    # return the probabilities
+    # coerce probabilities to length of label dictionary
+    if len(probas[0]) != len(label_dict):
+        temp = np.zeros((len(label_dict),))
+        temp[:len(probas[0])] = probas[0]
+        probas[0] = temp
     return probas[0]
