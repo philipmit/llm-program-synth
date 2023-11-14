760903bbc173afdf5964ce6514c2bc7926935b65
philipmit <pschro@mit.edu>
2023-11-13 20:04:03 -0500

    Output 15 (phase 3): Response to improving AUC of 0.9591326210654094
    AUC from previous code:
    0.9591326210654094
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.feature_selection import SelectFromModel
    from sklearn.pipeline import Pipeline
    from sklearn.ensemble import RandomForestClassifier
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True,
    header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.drop(['Sequence Name', 'class'], axis=1)
    y = ecoli['class']
    
    # Replace strings with numbers in y
    class_labels = np.unique(y)
    label_dict = {label: index for index, label in enumerate(class_labels)}
    y = y.replace(label_dict)
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.4, random_state=42)
    
    # Normalize the dataset and add feature selection in the pipeline
    pipe = Pipeline([
      ('scaler', StandardScaler()),
      ('feature_selection', SelectFromModel(RandomForestClassifier(n_estimators=100,random_state=42))),
      ('classification', LogisticRegression(C=0.9,max_iter=5000, multi_class='multinomial',solver='lbfgs'))])
    
    pipe.fit(X_train,y_train)
    
    def predict_icu_mortality(raw_sample):
        """Predicts the mortality in ICU of a single sample using the trained model."""
        raw_sample = np.array(raw_sample).reshape(1, -1)
        probas = pipe.predict_proba(raw_sample)
        return probas[0]
    ```
    
    In the new approach, I've added a feature selection step in the pipeline which uses RandomForestClassifier to select important features. Additionally, the `C` parameter in Logistic Regression has been tuned to `0.9` (indicating stronger regularization) and increased `max_iter` to `5000` to allow convergence for more complex models. This might give better performance on the validation set. Also, the split ratio for train-test split has been changed to 60:40 from 50:50 to provide more data for training.

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
@@ -3,8 +3,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectFromModel
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
 # Load the Ecoli dataset
-ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, 
+header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.drop(['Sequence Name', 'class'], axis=1)  
 y = ecoli['class']  
@@ -15,18 +19,15 @@ y = y.replace(label_dict)
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.5, random_state=42)
-# normalize the dataset
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-# Define Logistic Regression model
-# The 'multi_class' parameter is set to 'multinomial' as we have multiple classes in the target label. 
-# The solver is initialized as 'lbfgs' which is suitable for multiclass problems.
-log_model = LogisticRegression(max_iter=1000, multi_class='multinomial', solver='lbfgs')
-log_model.fit(X_train, y_train)
-def predict_label(raw_sample):
-    """Predicts the label of a single sample using the trained model."""
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.4, random_state=42)
+# Normalize the dataset and add feature selection in the pipeline
+pipe = Pipeline([
+  ('scaler', StandardScaler()), 
+  ('feature_selection', SelectFromModel(RandomForestClassifier(n_estimators=100,random_state=42))),
+  ('classification', LogisticRegression(C=0.9,max_iter=5000, multi_class='multinomial',solver='lbfgs'))])
+pipe.fit(X_train,y_train)
+def predict_icu_mortality(raw_sample):
+    """Predicts the mortality in ICU of a single sample using the trained model."""
     raw_sample = np.array(raw_sample).reshape(1, -1)
-    sample = scaler.transform(raw_sample)
-    probas = log_model.predict_proba(sample)
+    probas = pipe.predict_proba(raw_sample)
     return probas[0]
