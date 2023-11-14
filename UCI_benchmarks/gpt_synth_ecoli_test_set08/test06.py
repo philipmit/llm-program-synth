52bd74640baeff827ffe07e163215c83fd00de1c
philipmit <pschro@mit.edu>
2023-11-13 22:55:57 -0500

    Output 10 (phase 3): Response to improving AUC of 0.6528330267189502
    AUC from previous code:
    0.6528330267189502
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.pipeline import make_pipeline
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Pre-process the dataset
    X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
    y = ecoli.iloc[:, -1].values # All rows, only the last column
    
    # Replace string labels with numbers using LabelEncoding
    from sklearn.preprocessing import LabelEncoder
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Define a pipeline that includes Standard Scaler and logistic regression model
    # It standardizes features by removing the mean and scaling to unit variance and then applies logistic regression.
    # Also, increased the iterations or max_iter to 5000
    # Here, an l2 penalty is also used which is a very simple way to improve performance was added to avoid overfitting
    pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000, penalty='l2', solver='liblinear'))
    
    # Then train the model
    pipeline.fit(X_train, y_train)
    
    # Still using predict_proba method but modified function to work with the pipeline
    def predict_label(X_sample):
    
        # Reshaping the input to match the input data shape for Logistic Regression model
        X_sample = np.array(X_sample).reshape(1, -1)
    
        # Get the model's predicted probabilities with predict_proba
        pred_proba = pipeline.predict_proba(X_sample)
    
        return pred_proba[0]
    ```
    The code above introduces few strategies to improve model performance:
    
    1. It uses a pipeline to first standardize the features by removing the mean and scaling to unit variance, a process known as standardization. Scaling the features so they all have the same range is important in many machine learning models and can significantly improve performance.
    
    2. The model uses an L2 penalty in the logistic regression. Regularization like L1 or L2 is a common method to avoid overfitting.
    
    3. We've also increased the number of maximum iterations for the logistic regression model to give more chance for the model to find the optimal solution, and used the 'liblinear' solver which is suitable for smaller datasets and binary classification, which may help in improving AUC.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
@@ -2,28 +2,32 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 # Load the ecoli dataset
-ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
+                    delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Pre-process the dataset
 X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
 y = ecoli.iloc[:, -1].values # All rows, only the last column
-# Replace string labels with numbers
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
+# Replace string labels with numbers using LabelEncoding
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Define logistic regression model and train the model
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
-# Explicitly providing unique classes to model
-model.classes_ = np.unique(y)
-# Define function to predict labels
+# Define a pipeline that includes Standard Scaler and logistic regression model
+# It standardizes features by removing the mean and scaling to unit variance and then applies logistic regression.
+# Also, increased the iterations or max_iter to 5000
+# Here, an l2 penalty is also used which is a very simple way to improve performance was added to avoid overfitting
+pipeline = make_pipeline(StandardScaler(), LogisticRegression(max_iter=5000, penalty='l2', solver='liblinear'))
+# Then train the model
+pipeline.fit(X_train, y_train)
+# Still using predict_proba method but modified function to work with the pipeline
 def predict_label(X_sample):
-    # reshaping the input to match the input shape for sklearn ML models
+    # Reshaping the input to match the input data shape for Logistic Regression model
     X_sample = np.array(X_sample).reshape(1, -1)
-    # getting the model's predicted probabilities with predict_proba
-    pred_proba = np.zeros((1,len(model.classes_)))
-    pred_proba[0][model.predict(X_sample)[0]] = 1
+    # Get the model's predicted probabilities with predict_proba
+    pred_proba = pipeline.predict_proba(X_sample)
     return pred_proba[0]
