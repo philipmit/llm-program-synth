dfb2e02e0423663d8d917411e0484b41029af1d4
philipmit <pschro@mit.edu>
2023-11-13 19:01:27 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 16, in <module>
      File "<string>", line 27, in predict_label
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_set_output.py", line 140, in wrapped
        data_to_wrap = f(self, X, *args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/preprocessing/_label.py", line 137, in transform
        return _encode(y, uniques=self.classes_)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_encode.py", line 227, in _encode
        raise ValueError(f"y contains previously unseen labels: {str(e)}")
    <class 'ValueError'>
    y contains previously unseen labels: 0
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Define features and target
    X = ecoli.drop(['Sequence Name', 'class'], axis=1)
    y = ecoli['class']
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    # Cast to numpy arrays
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the Logistic Regression model
    lr = LogisticRegression(max_iter=1000, multi_class='ovr')
    lr.fit(X_train, y_train)
    
    def predict_label(data):
        data = np.array(data).reshape(1, -1)
        proba = lr.predict_proba(data)[0]
        return proba
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set03/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test01.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Define features and target
 X = ecoli.drop(['Sequence Name', 'class'], axis=1)
 y = ecoli['class']
-# Replace strings with numbers in y
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+# Cast to numpy arrays
+X = X.to_numpy()
+y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Train the Logistic Regression model
@@ -20,12 +20,4 @@ lr.fit(X_train, y_train)
 def predict_label(data):
     data = np.array(data).reshape(1, -1)
     proba = lr.predict_proba(data)[0]
-    # Ensures predicted probabilities are for all classes
-    full_proba = np.zeros(len(np.unique(y)))
-    # Index of predicted classes are found after ranking 
-    classes_ = lr.classes_
-    ranks = label_encoder.transform(classes_)
-    # Setting prediction only for predicted classes
-    for rank, prob in zip(ranks, proba):
-        full_proba[rank] = prob
-    return full_proba
+    return proba
