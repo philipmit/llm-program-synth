eeb22f0e7b686e757ba2efbefb63599761daed1e
philipmit <pschro@mit.edu>
2023-11-13 19:51:46 -0500

    Initial output: Here is a Python script that fits a logistic regression model to the Ecoli dataset and defines a function predict_label that computes predicted probabilities for a single sample.
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    
    # Load the digits dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # scale the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Fit a logistic regression model
    log_reg = LogisticRegression(multi_class='ovr',solver='liblinear')
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_sample):
        sample = scaler.transform(raw_sample.reshape(1, -1)) # scale the raw_sample
        return log_reg.predict_proba(sample) # use the model to obtain predictions
    ```

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+# Load the digits dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# replace strings with numbers in y
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X=X.to_numpy()
+y=y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# scale the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+# Fit a logistic regression model
+log_reg = LogisticRegression(multi_class='ovr',solver='liblinear')
+log_reg.fit(X_train, y_train)
+def predict_label(raw_sample):
+    sample = scaler.transform(raw_sample.reshape(1, -1)) # scale the raw_sample
+    return log_reg.predict_proba(sample) # use the model to obtain predictions
