11617137ff1fd7acb1d9fafdcc04bd7ca298de52
philipmit <pschro@mit.edu>
2023-11-14 11:23:06 -0500

    Output 5 (phase 3): Response to ValueError
    Error from previous code:
      File "<stdin>", line 292, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The above error occurs because in the original model, it generates probability scores only for the classes it has seen during the training (classes it has not seen get zero probability). Therefore, it might not generate scores for as many classes as exist in y_true when evaluating on the test set, hence causing an error. We need to make sure that the model is capable of predicting all possible classes. We will choose a logistic regression model for this, as it naturally handles multi-class problems. Moreover, Logistic Regression directly expresses its multi-class decision function in terms of class-wise probabilities.
    
    Below is the corrected code snippet:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # Replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0, 1, 2, 3, 4, 5, 6, 7])
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scaling the data for normalization to improve model performance
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Applying Logistic Regression with multi_class parameter set to 'ovr' for binary problems
    lr_model = LogisticRegression(random_state=42, multi_class='ovr')
    lr_model.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(sample):
        global scaler, lr_model
        sample = np.array(sample).reshape(1, -1)
        sample = scaler.transform(sample)
        prediction = lr_model.predict_proba(sample)
        # Generate a probabilities array for all possible classes
        full_probs = [0]*8
        for idx, prob in enumerate(prediction[0]):
            full_probs[idx] = prob
        return full_probs
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingClassifier
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -18,13 +17,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Scaling the data for normalization to improve model performance
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Switching to Gradient Boosting Classifier with carefully selected hyperparameters to improve performance
-gbc_model = GradientBoostingClassifier(learning_rate=0.1, n_estimators=100, max_depth=3, random_state=42)
-gbc_model.fit(X_train, y_train)
+# Applying Logistic Regression with multi_class parameter set to 'ovr' for binary problems 
+lr_model = LogisticRegression(random_state=42, multi_class='ovr')
+lr_model.fit(X_train, y_train)
 # Define the predict_label function
 def predict_label(sample):
-    global scaler, gbc_model
+    global scaler, lr_model
     sample = np.array(sample).reshape(1, -1)
     sample = scaler.transform(sample)
-    prediction = gbc_model.predict_proba(sample)
-    return prediction.flatten()
+    prediction = lr_model.predict_proba(sample)
+    # Generate a probabilities array for all possible classes
+    full_probs = [0]*8
+    for idx, prob in enumerate(prediction[0]):
+        full_probs[idx] = prob
+    return full_probs
