470375a15510ec160e3c7c6f728a23f2d81dbdc0
philipmit <pschro@mit.edu>
2023-11-13 19:58:15 -0500

    Output 10 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 16, in <module>
      File "<string>", line 38, in predict_label
    <class 'ValueError'>
    could not broadcast input array from shape (8,) into shape (6,)
    
    Response from GPT:
    The error is related to the predict_label function, where the model attempts to broadcast a prediction array of size (8,) to an array of shape (6,). It means that the model is predicting 8 probabilities whereas the test data has only 6 distinct classes. In this case, we need to ensure that the model only predicts probabilities for the classes it was trained on. Let's take a look how we could adjust the training stage and the predict_label function.
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, the features
    y = ecoli.iloc[:, -1]   # All rows, the class label
    
    # replace string class labels with unique integers
    class_labels = np.unique(y)
    label_dict = {label: index for index, label in enumerate(class_labels)}
    y = y.replace(label_dict)
    
    # Convert the features and labels data to numpy
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the Ecoli dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Only train on data that contains representive labels
    unq_train_labels = np.unique(y_train)
    train_indices = np.isin(y, unq_train_labels)
    X_train = X[train_indices]
    y_train = y[train_indices]
    
    # Defining the scaler
    scaler = StandardScaler()
    
    # Scaling the X_train data
    X_train = scaler.fit_transform(X_train)
    
    # Define and Fit logistic regression model
    log_reg = LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=10000)
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_sample):
        # reshape raw_sample if it's only 1D (it's a single sample)
        if len(raw_sample.shape) == 1:
            raw_sample = raw_sample.reshape(1, -1)
    
        # normalize the sample
        sample = scaler.transform(raw_sample)
    
        # use the fitted model to predict the probabilities
        probas = log_reg.predict_proba(sample)
    
        # Ensure probabilities are only returned for represented classes
        proba_dict = {label: 0 for label in label_dict.values()}
        for i, prob in zip(log_reg.classes_, probas[0]):
            proba_dict[i] = prob
        probas = np.array(list(proba_dict.values()))
    
        return probas
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test02.py
@@ -6,16 +6,22 @@ from sklearn.linear_model import LogisticRegression
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows, all the features
+X = ecoli.iloc[:, 1:-1]  # All rows, the features
 y = ecoli.iloc[:, -1]   # All rows, the class label
 # replace string class labels with unique integers
 class_labels = np.unique(y)
 label_dict = {label: index for index, label in enumerate(class_labels)}
 y = y.replace(label_dict)
+# Convert the features and labels data to numpy
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the Ecoli dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Only train on data that contains representive labels
+unq_train_labels = np.unique(y_train)
+train_indices = np.isin(y, unq_train_labels)
+X_train = X[train_indices]
+y_train = y[train_indices]
 # Defining the scaler
 scaler = StandardScaler()
 # Scaling the X_train data
@@ -24,16 +30,16 @@ X_train = scaler.fit_transform(X_train)
 log_reg = LogisticRegression(multi_class='ovr', solver='liblinear', max_iter=10000)
 log_reg.fit(X_train, y_train)
 def predict_label(raw_sample):
-    # reshape raw_sample if it's only 1D (a single sample)
+    # reshape raw_sample if it's only 1D (it's a single sample)
     if len(raw_sample.shape) == 1:
         raw_sample = raw_sample.reshape(1, -1)
     # normalize the sample
     sample = scaler.transform(raw_sample)
     # use the fitted model to predict the probabilities
     probas = log_reg.predict_proba(sample)
-    # coerce probabilities to length of label dictionary
-    if len(probas[0]) != len(label_dict):
-        temp = np.zeros((len(label_dict),))
-        temp[:len(probas[0])] = probas[0]
-        probas[0] = temp
-    return probas[0]
+    # Ensure probabilities are only returned for represented classes
+    proba_dict = {label: 0 for label in label_dict.values()}
+    for i, prob in zip(log_reg.classes_, probas[0]):
+        proba_dict[i] = prob
+    probas = np.array(list(proba_dict.values()))
+    return probas
