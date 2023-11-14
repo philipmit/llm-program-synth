bf90703f70b2b0c3dcc15187b462c86d8707f0ac
philipmit <pschro@mit.edu>
2023-11-13 20:16:54 -0500

    Output 7 (phase 3): Response to improving AUC of 0.855143933191609
    AUC from previous code:
    0.855143933191609
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.model_selection import GridSearchCV
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Convert categorical target to numerical values
    y = ecoli.iloc[:, -1].values
    unique_y = np.unique(y)
    mapping = {y:i for i, y in enumerate(unique_y)}
    y = np.array([mapping[i] for i in y])
    # Features
    X = ecoli.iloc[:, 1:-1].values
    # Split into a training and testing set
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Standardization of the features
    scaler = StandardScaler().fit(X_train)
    X_train = scaler.transform(X_train)
    # Hyperparameters tuning using GridSearchCV
    parameters = {'max_depth': [5, 10, 15], 'n_estimators': [50, 100, 200]}
    rf = RandomForestClassifier(random_state=42)
    clf = GridSearchCV(rf, parameters)
    clf.fit(X_train, y_train)
    # Extract best_model
    best_model = clf.best_estimator_
    def predict_label(input_data):
        # input_data should be raw data for single sample, array of the format [mcg, gvh, lip, chg, aac, alm1, alm2]
        # Scale the input
        input_data = scaler.transform([input_data])
        # Return the predicted probabilities for each class, ensuring there is a probability for each class in y_train
        prediction = best_model.predict_proba(input_data)[0]
        full_prediction = np.zeros(len(unique_y))
        for index, prob in zip(best_model.classes_, prediction):
            full_prediction[index] = prob
        return full_prediction
    ```
    The modifications done are as follows:
    1. Changed MinMaxScaler to StandardScaler which standardizes features by removing the mean and scaling to unit variance, often giving better results when the data follows a normal distribution.
    2. Used RandomForestClassifier instead of LogisticRegression since RandomForest, being an ensemble method, often results in a more robust model and can capture complex patterns.
    3. Performed hyperparameters tuning using GridSearchCV. This helps to find the optimal parameters for the model, which leads to better performance. The parameters used here are n_estimators (Number of trees in the forest) and max_depth (The maximum depth of the tree). Different values could be selected as per the dataset.
    4. Returned "best_estimator_"  from GridSearchCV to use realistically optimal model parameters for prediction.

--- UCI_benchmarks/gpt_synth_ecoli_test_set05/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set05/test01.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -15,19 +17,23 @@ y = np.array([mapping[i] for i in y])
 X = ecoli.iloc[:, 1:-1].values
 # Split into a training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Scaling the features as logistic regression requires features to be on similar scale
-scaler = MinMaxScaler().fit(X_train)
+# Standardization of the features
+scaler = StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
-# Define the Logistic Regression model
-model = LogisticRegression(random_state=42, max_iter=5000, multi_class='ovr')
-model.fit(X_train, y_train)
+# Hyperparameters tuning using GridSearchCV
+parameters = {'max_depth': [5, 10, 15], 'n_estimators': [50, 100, 200]}
+rf = RandomForestClassifier(random_state=42)
+clf = GridSearchCV(rf, parameters)
+clf.fit(X_train, y_train)
+# Extract best_model
+best_model = clf.best_estimator_
 def predict_label(input_data):
     # input_data should be raw data for single sample, array of the format [mcg, gvh, lip, chg, aac, alm1, alm2]
     # Scale the input
     input_data = scaler.transform([input_data])
     # Return the predicted probabilities for each class, ensuring there is a probability for each class in y_train
-    prediction = model.predict_proba(input_data)[0]
+    prediction = best_model.predict_proba(input_data)[0]
     full_prediction = np.zeros(len(unique_y))
-    for index, prob in zip(model.classes_, prediction):
+    for index, prob in zip(best_model.classes_, prediction):
         full_prediction[index] = prob
     return full_prediction
