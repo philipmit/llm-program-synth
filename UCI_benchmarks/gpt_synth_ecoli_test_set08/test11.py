bde18f217a99341e8039585c3aeb34521d8a7946
philipmit <pschro@mit.edu>
2023-11-13 23:32:53 -0500

    Initial output: ```python import pandas as pd import numpy as np from sklearn.model_selection import train_test_split from sklearn.linear_model import LogisticRegression from sklearn.preprocessing import StandardScaler
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows and all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows and only the last column
    
    # Replace categorical classes with numerical labels in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    # Convert pandas DataFrame to numpy array
    X = X.values
    y = y.values
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the features to have mean=0 and variance=1
    scaler = StandardScaler().fit(X_train)
    X_train = scaler.transform(X_train)
    
    # Logistic Regression model training
    model = LogisticRegression(max_iter=10000)
    model.fit(X_train, y_train)
    
    def predict_label(raw_data):
        # Preprocess the raw data
        raw_data = np.array(raw_data)
        raw_data = scaler.transform(raw_data.reshape(1, -1))
        # Predict the probabilities
        predicted_probabilities = model.predict_proba(raw_data)
        return predicted_probabilities[0]
    ```

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test11.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+# Load the ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
+                    delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]  # All rows and all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows and only the last column
+# Replace categorical classes with numerical labels in y
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+# Convert pandas DataFrame to numpy array
+X = X.values
+y = y.values
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Standardize the features to have mean=0 and variance=1
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+# Logistic Regression model training
+model = LogisticRegression(max_iter=10000)
+model.fit(X_train, y_train)
+def predict_label(raw_data):
+    # Preprocess the raw data
+    raw_data = np.array(raw_data)
+    raw_data = scaler.transform(raw_data.reshape(1, -1))
+    # Predict the probabilities
+    predicted_probabilities = model.predict_proba(raw_data)
+    return predicted_probabilities[0]
