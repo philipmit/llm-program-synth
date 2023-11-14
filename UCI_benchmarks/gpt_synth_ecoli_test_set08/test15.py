bd1e88ef82e69f7f23400d4bde2ee69de2fd04b3
philipmit <pschro@mit.edu>
2023-11-14 10:37:15 -0500

    Output 2 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error indicates an inconsistency between the number of classes in the true labels and the number of columns in the predicted probabilities. This happens because the Logistic Regression model in sklearn automatically detects the number of classes in the target variable of the training set, and when it doesn't train with some classes, it does not predict probabilities for them.
    
    In this case, since we're using a 50-50 train-test split, there's a chance that some classes may only be present in the testing set and are not seen during training.
    
    One simple solution would be to ensure that all classes are represented in the training data. Using stratified sampling while splitting the dataset can achieve this. It makes sure that every class is represented in the train and test sets according to its proportion in the whole dataset.
    
    Here's the corrected code using stratified sampling:
    
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # replace strings with numbers in y
    np.unique(y)
    len(list(np.unique(y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X = X.to_numpy()
    y = y.to_numpy()
    # Split the dataset into training and testing sets using Stratification
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
    # Train the logistic regression model
    lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=400)
    lr.fit(X_train, y_train)
    def predict_label(sample):
        """
        Predict the class probabilities for a given sample.
        Parameters:
        sample -- the raw unprocessed data for a sample
        Returns:
        the predicted probabilities for the sample
        """
        return lr.predict_proba([sample])[0]
    ```
    Please note that this solution relies on the assumption that every class has more than one instance. If a class has only one instance, it would not be guaranteed to appear in the training set even with stratified sampling.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test15.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test15.py
@@ -13,8 +13,8 @@ len(list(np.unique(y)))
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
 X = X.to_numpy()
 y = y.to_numpy()
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Split the dataset into training and testing sets using Stratification
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
 # Train the logistic regression model
 lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=400)
 lr.fit(X_train, y_train)
