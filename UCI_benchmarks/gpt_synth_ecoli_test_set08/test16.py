e3d8c3599d6c064781eea0d1f895db7ff7595d12
philipmit <pschro@mit.edu>
2023-11-14 10:47:56 -0500

    Output 6 (phase 3): Response to improving AUC of 0.7323698287866371
    AUC from previous code:
    0.7323698287866371
    
    Response from GPT:
    ```
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder, StandardScaler
    import numpy as np
    from sklearn.ensemble import GradientBoostingClassifier
    
    class GradientBoostingModel:
        def __init__(self):
            # Using GradientBoostingClassifier for better performance
            # due to better handling of imbalanced data sets and outliers
            self.model = GradientBoostingClassifier(n_estimators=1000,
                                                    learning_rate=0.01,
                                                    max_depth=5,
                                                    random_state=42)
            self.le = LabelEncoder()
            self.scaler = StandardScaler()
            self.fitted = False
        def load_data(self):
            ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
            ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
            y = ecoli.iloc[:, -1]
            self.le.fit(np.unique(y))  # ensure label encoder is fitted with all existing classes
            y = self.le.transform(y)
            X = ecoli.iloc[:, 1:-1].to_numpy()
            X = self.scaler.fit_transform(X) # Standard scale the input for better convergence in GBC
            X_train, _, y_train, _ = train_test_split(X, y, test_size=0.5, random_state=42)
            return X_train, y_train
        def train_model(self, X_train, y_train):
            self.model.fit(X_train, y_train)
            self.fitted = True
        def predict_label(self, raw_input):
            assert self.fitted, "Model not yet fitted"
            raw_input = self.scaler.transform(raw_input.reshape(1, -1))
            probabilities = self.model.predict_proba(raw_input)
            # ensure the returned probabilities have probabilities for all classes
            return np.append(probabilities, [0]*(len(lself.e.classes_) - len(probabilities[0])))
    
    # create an instance of the model and train it
    my_model = GradientBoostingModel()
    X_train, y_train = my_model.load_data()
    my_model.train_model(X_train, y_train)
    # redefine predict_label to use the instance of GradientBoostedModel
    def predict_label(raw_input):
        return my_model.predict_label(raw_input)
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test16.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test16.py
@@ -3,13 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-class RandomForestModel:
+from sklearn.ensemble import GradientBoostingClassifier
+class GradientBoostingModel:
     def __init__(self):
-        # Random Forest automatically takes care of feature correlations and could improve performance
-        # Increasing number of estimators (trees) could increase performance
-        # Setting max_depth to a high value to allow the estimator to make complex decisions
-        self.model = RandomForestClassifier(n_estimators=1000, max_depth=20, random_state=42)
+        # Using GradientBoostingClassifier for better performance 
+        # due to better handling of imbalanced data sets and outliers
+        self.model = GradientBoostingClassifier(n_estimators=1000, 
+                                                learning_rate=0.01, 
+                                                max_depth=5, 
+                                                random_state=42)
         self.le = LabelEncoder()
         self.scaler = StandardScaler()
         self.fitted = False
@@ -20,7 +22,7 @@ class RandomForestModel:
         self.le.fit(np.unique(y))  # ensure label encoder is fitted with all existing classes
         y = self.le.transform(y)
         X = ecoli.iloc[:, 1:-1].to_numpy()
-        X = self.scaler.fit_transform(X) # Standard scale the input for better convergence in RF
+        X = self.scaler.fit_transform(X) # Standard scale the input for better convergence in GBC
         X_train, _, y_train, _ = train_test_split(X, y, test_size=0.5, random_state=42)
         return X_train, y_train
     def train_model(self, X_train, y_train):
@@ -31,11 +33,11 @@ class RandomForestModel:
         raw_input = self.scaler.transform(raw_input.reshape(1, -1))
         probabilities = self.model.predict_proba(raw_input)
         # ensure the returned probabilities have probabilities for all classes
-        return np.append(probabilities, [0]*(len(self.le.classes_) - len(probabilities[0])))
+        return np.append(probabilities, [0]*(len(lself.e.classes_) - len(probabilities[0])))
 # create an instance of the model and train it
-my_model = RandomForestModel()
+my_model = GradientBoostingModel()
 X_train, y_train = my_model.load_data()
 my_model.train_model(X_train, y_train)
-# redefine predict_label to use the instance of RandomForestModel
+# redefine predict_label to use the instance of GradientBoostedModel
 def predict_label(raw_input):
     return my_model.predict_label(raw_input)
