91521b7bdaa87356d62de2900ef0954b4762d4ea
philipmit <pschro@mit.edu>
2023-11-13 22:27:20 -0500

    Output 10 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error occurs because the predict_label function is not considering previously unseen category labels in the test set. Therefore, when the roc_auc_score function checks for congruence between the number of unique labels in y_true and the prediction y_score, they do not match.
    
    In order to address this issue, I will fit a one-hot encoder to y_train before training the classifier, and use this fitted encoder to transform both y_train and y_test. With this approach, the classifier will be trained to predict probabilities for all labels present in the dataset. Here's the updated version of the GPT code:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Prepare the feature and target variables
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns excluding the first and last one
    y = ecoli.iloc[:, -1]    # All rows, only the last column
    
    # Transform target labels into numbers using LabelEncoder
    label_encoder = LabelEncoder()
    one_hot_encoder = OneHotEncoder(sparse=False)
    
    y_encoded = label_encoder.fit_transform(y)
    y_encoded = np.reshape(y_encoded, (-1,1))
    y_encoded_hot = one_hot_encoder.fit_transform(y_encoded)
    
    # Split the dataset into a training set and a test set
    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded_hot, test_size=0.5, random_state=42)
    
    # Define the logistic regression model
    log_reg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000, random_state=42)
    
    # Fit the model with the train data
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_data_sample):
        # Ensure the input data is 2D
        if len(raw_data_sample.shape) == 1:
            raw_data_sample = np.expand_dims(raw_data_sample, 0)
        # Predict the class probabilities and return the maximum along axis 1
        proba = log_reg.predict_proba(raw_data_sample)
    
        # Create an empty array with shape (1, number of classes)
        probs = np.zeros((1, y_encoded_hot.shape[1]))
    
        # Assign the predicted probabilities to the relevant indices
        probs[0, log_reg.classes_] = proba[0, :]
    
        return probs
    ```
    
    You might want to include some checks to handle edge cases (for instance, if the classifier predicts a class that is not among the training labels).

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test03.py
@@ -1,26 +1,34 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Prepare the feature and target variables
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns excluding the first and last one
-y = ecoli.iloc[:, -1]  # All rows, only the last column
+y = ecoli.iloc[:, -1]    # All rows, only the last column
 # Transform target labels into numbers using LabelEncoder
-le = LabelEncoder()
-y = le.fit_transform(y)
+label_encoder = LabelEncoder()
+one_hot_encoder = OneHotEncoder(sparse=False)
+y_encoded = label_encoder.fit_transform(y)
+y_encoded = np.reshape(y_encoded, (-1,1))
+y_encoded_hot = one_hot_encoder.fit_transform(y_encoded)
 # Split the dataset into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded_hot, test_size=0.5, random_state=42)
 # Define the logistic regression model
 log_reg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000, random_state=42)
-# Fit the model with train data
-log_reg.fit(X=X_train, y=y_train)
+# Fit the model with the train data
+log_reg.fit(X_train, y_train)
 def predict_label(raw_data_sample):
     # Ensure the input data is 2D
     if len(raw_data_sample.shape) == 1:
         raw_data_sample = np.expand_dims(raw_data_sample, 0)
-    # Predict the class probabilities
-    proba = log_reg.predict_proba(raw_data_sample)[0]
-    return proba
+    # Predict the class probabilities and return the maximum along axis 1
+    proba = log_reg.predict_proba(raw_data_sample)
+    # Create an empty array with shape (1, number of classes)
+    probs = np.zeros((1, y_encoded_hot.shape[1]))
+    # Assign the predicted probabilities to the relevant indices
+    probs[0, log_reg.classes_] = proba[0, :]
+    return probs
