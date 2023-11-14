c65f32b51a440a4ea5840b094169c12bc195a149
philipmit <pschro@mit.edu>
2023-11-13 20:15:34 -0500

    Output 5 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    Based on the error message, it seems like the predict_label function isn't providing predictions for all classes in your target variable. The shape of the array returned by the function must match the number of classes in the y_train.
    
    Here is a revised version of the GPT-3 code that should address the issue:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import MinMaxScaler
    from sklearn.linear_model import LogisticRegression
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Convert categorical target to numerical values
    y = ecoli.iloc[:, -1].values
    unique_y = np.unique(y)
    mapping = {y:i for i, y in enumerate(unique_y)}
    y = np.array([mapping[i] for i in y])
    
    # Features
    X = ecoli.iloc[:, 1:-1].values
    
    # Split into a training and testing set
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scaling the features as logistic regression requires features to be on similar scale
    scaler = MinMaxScaler().fit(X_train)
    X_train = scaler.transform(X_train)
    
    # Creating and training the logistic regression model
    model = LogisticRegression(random_state=42, max_iter=5000, multi_class='ovr')
    model.fit(X_train, y_train)
    
    def predict_label(input_data):
        # input_data should be raw data for single sample, array of the format [mcg, gvh, lip, chg, aac, alm1, alm2]
        # Scale the input
        input_data = scaler.transform([input_data])
        # Return the predicted probabilities for each class
        prediction = model.predict_proba(input_data)
        return prediction[0]
    ```
    
    In this code, we are mapping each class label in y to a unique integer. Moreover, while training the logistic regression model, we ensure it's aware of the multi-class nature of our problem by setting multi_class='ovr' in the LogisticRegression function. This should ensure the model returns predictions for each class when predict_proba is called.

--- UCI_benchmarks/gpt_synth_ecoli_test_set05/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set05/test01.py
@@ -7,27 +7,24 @@ from sklearn.linear_model import LogisticRegression
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Convert categorical target to numerical values
-y = ecoli.iloc[:, -1]
-unique_values = np.unique(y)
-y = y.replace(unique_values, np.arange(len(unique_values)))
+y = ecoli.iloc[:, -1].values
+unique_y = np.unique(y)
+mapping = {y:i for i, y in enumerate(unique_y)}
+y = np.array([mapping[i] for i in y])
 # Features
-X = ecoli.iloc[:, 1:-1]
-# Convert to numpy arrays
-X = X.values
-y = y.values
+X = ecoli.iloc[:, 1:-1].values
 # Split into a training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Scaling the features as logistic regression requires features to be on similar scale
 scaler = MinMaxScaler().fit(X_train)
 X_train = scaler.transform(X_train)
-# Creating and Training the Logistic Regression model
-model = LogisticRegression(random_state=42, max_iter=5000)
+# Creating and training the logistic regression model
+model = LogisticRegression(random_state=42, max_iter=5000, multi_class='ovr')
 model.fit(X_train, y_train)
 def predict_label(input_data):
-    # input_data should be a list of the format [mcg, gvh, lip, chg, aac, alm1, alm2]
+    # input_data should be raw data for single sample, array of the format [mcg, gvh, lip, chg, aac, alm1, alm2]
     # Scale the input
     input_data = scaler.transform([input_data])
+    # Return the predicted probabilities for each class
     prediction = model.predict_proba(input_data)
-    # shape of prediction is (1, n_classes), 
-    # taking the 0-index to reduce dimension
     return prediction[0]
