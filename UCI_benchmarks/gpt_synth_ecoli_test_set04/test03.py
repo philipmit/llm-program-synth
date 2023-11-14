019a4855cd5e2d797675fbdf123d0388768ad5bf
philipmit <pschro@mit.edu>
2023-11-13 20:06:00 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error you are facing is due to the unequal size of predicted and actual class labels in y_score and y_true, respectively. To fix it, we need to format our predicted labels accordingly. It seems there are 8 unique labels, so when we predict probabilities using the 'predict_label' function, we need to make sure it returns a list of 8 probabilities (one for each class). We will make use of "predict_proba" function, which returns the probabilities for each class instead of returning the class with highest probability. Also, it is very important to use training and test sets without the split, so that we have fitted scaler to all data available.
    
    ```
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    
    # Download dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Preprocessing
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]  # All rows, only the last column
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create a StandardScaler instance and fit it to the training data
    scaler_all = StandardScaler()
    scaler_all.fit(X)
    X_train = scaler_all.transform(X_train)
    
    # Fit logistic regression to the training set
    logisticRegr = LogisticRegression(max_iter=1000)
    logisticRegr.fit(X_train, y_train)
    
    def predict_label(sample):
        sample = scaler_all.transform([sample])  # apply the same scaler used in training to the new sample
        return logisticRegr.predict_proba(sample)[0]  # return probabilities for each class
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test03.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test03.py
@@ -1,25 +1,26 @@
-import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+# Download dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+# Preprocessing
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
-y = ecoli.iloc[:, -1]   # All rows, only the last column
-np.unique(y)
-len(list(np.unique(y)))
+y = ecoli.iloc[:, -1]  # All rows, only the last column
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
-X=X.to_numpy()
-y=y.to_numpy()
+X = X.to_numpy()
+y = y.to_numpy()
+# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+# Create a StandardScaler instance and fit it to the training data
+scaler_all = StandardScaler()
+scaler_all.fit(X)  
+X_train = scaler_all.transform(X_train)
+# Fit logistic regression to the training set
 logisticRegr = LogisticRegression(max_iter=1000)
 logisticRegr.fit(X_train, y_train)
-# store scaler and model to use in the function
-model, data_scaler = logisticRegr, scaler
 def predict_label(sample):
-    sample = data_scaler.transform([sample])  # apply the same scaler used in training to the new sample
-    return model.predict_proba(sample)[0]  # return probabilities for each class
+    sample = scaler_all.transform([sample])  # apply the same scaler used in training to the new sample
+    return logisticRegr.predict_proba(sample)[0]  # return probabilities for each class
