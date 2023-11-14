0825e6c8bb164a714ac3d786cc9ab0af76c97377
Philip Herman Schroeder <pschro@sls-1080-3.csail.mit.edu>
2023-11-13 21:01:37 -0500

    Output 42 (phase 3): Response to RuntimeError
    Error from previous code:
      File "<stdin>", line 345, in <module>
      File "<string>", line 61, in <module>
      File "<string>", line 43, in train_model
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1194, in _call_impl
        return forward_call(*input, **kwargs)
      File "<string>", line 33, in forward
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1194, in _call_impl
        return forward_call(*input, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/torch/nn/modules/rnn.py", line 765, in forward
        raise RuntimeError(msg)
    <class 'RuntimeError'>
    For unbatched 2-D input, hx and cx should also be 2-D but got (3-D, 3-D) tensors
    
    Response from GPT:
    The error is indicating that you are feeding a tensor of incorrect shape to the LSTM. This might be because you are enclosing your tensor inside an extra dimension using `unsqueeze(0)`, which makes your input a 2-D tensor instead of a 3-D tensor (batch_size, sequence_length, num_features) that's expected by LSTM. You need to reshape your tensor accordingly. Further, also ensure your labels have the correct dimensions. Below, I've slightly modified the `train_model` and `LSTM` class to rectify these issues.
    
    ```python
    import os
    import pandas as pd
    import numpy as np
    import torch
    import torch.nn as nn
    from torch.utils.data import DataLoader
    from sklearn.preprocessing import StandardScaler
    
    # File paths
    TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
    LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
    
    class LSTM(nn.Module):
        def __init__(self, input_size, hidden_size, num_layers, output_size):
            super(LSTM, self).__init__()
            self.hidden_size = hidden_size
            self.num_layers = num_layers
            self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
            self.fc = nn.Linear(hidden_size, output_size)
    
        def forward(self, x):
            h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
            c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
            out, _ = self.lstm(x, (h0, c0))
            out = self.fc(out[:, -1, :])
            return out.squeeze()
    
    def train_model(model, data_loader, criterion, optimizer, num_epochs):
        model.train()
        for epoch in range(num_epochs):
            for i, (seq, labels) in enumerate(data_loader):
                optimizer.zero_grad()
                seq, labels = seq.to(device), labels.to(device)
                output = model(seq)
                loss = criterion(output, labels.view(-1, 1))
                loss.backward()
                optimizer.step()
            if epoch % 10 == 0:
                print('Epoch: {}/{}, Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
    
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
    model = LSTM(input_size=13, hidden_size=64, num_layers=2, output_size=1).to(device)
    dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
    data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
    criterion = nn.BCEWithLogitsLoss()
    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
    num_epochs = 100
    train_model(model, data_loader, criterion, optimizer, num_epochs)
    
    def predict_icu_mortality(patient_data):
        model.eval()
        with torch.no_grad():
            prediction = model(patient_data)
            return torch.sigmoid(prediction).item()
    ```
    
    Please replace the existing LSTM and train_model definitions with the updated ones.
    
    This code trains the LSTM model for `num_epochs` and prints the loss every 10 epochs. Once trained, you can use `predict_icu_mortality` for patients' data prediction.

--- mimicIII/gpt_synth_icu_mortality_demo_test_set02/test01.py
+++ mimicIII/gpt_synth_icu_mortality_demo_test_set02/test01.py
@@ -5,22 +5,9 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from sklearn.preprocessing import StandardScaler
+# File paths
 TRAIN_DATA_PATH = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/"
 LABEL_FILE = "/data/sls/scratch/pschro/p2/data/benchmark_output_demo2/in-hospital-mortality/train/listfile.csv"
-class ICUData(torch.utils.data.Dataset):
-    def __init__(self, data_path, label_file):
-        self.data_path = data_path
-        self.file_names = pd.read_csv(label_file)['stay']
-        self.labels = torch.tensor(pd.read_csv(label_file)['y_true'].values, dtype=torch.float32)
-        self.scaler = StandardScaler()
-    def __len__(self):
-        return len(self.file_names)
-    def __getitem__(self, idx):
-        file_path = os.path.join(self.data_path, self.file_names[idx])
-        data = pd.read_csv(file_path).drop('Hours', axis=1)
-        data = data.apply(pd.to_numeric, errors='coerce').fillna(0)
-        data = self.scaler.fit_transform(data)
-        return torch.FloatTensor(data), self.labels[idx]
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTM, self).__init__()
@@ -29,28 +16,23 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
     def forward(self, x):
-        h0, c0 = [torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device) for _ in range(2)]
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
-        return out.squeeze()  # change: adjust dimension
+        return out.squeeze()
 def train_model(model, data_loader, criterion, optimizer, num_epochs):
     model.train()
-    for epoch in range(1, num_epochs+1):
-        for seq, labels in data_loader:
-            seq = seq.unsqueeze(0).to(device)  # change:  adjust dimension
-            labels = labels.to(device)
+    for epoch in range(num_epochs):
+        for i, (seq, labels) in enumerate(data_loader):
             optimizer.zero_grad()
+            seq, labels = seq.to(device), labels.to(device)
             output = model(seq)
-            loss = criterion(output, labels.view(-1, 1))  # change: adjust dimension
+            loss = criterion(output, labels.view(-1, 1))
             loss.backward()
             optimizer.step()
         if epoch % 10 == 0:
-            print(f'Epoch {epoch}/{num_epochs}, Loss: {loss.item()}')
-def predict_icu_mortality(patient_data):
-    model.eval()
-    with torch.no_grad():
-        prediction = model(patient_data.unsqueeze(0))
-        return torch.sigmoid(prediction).item()
+            print('Epoch: {}/{}, Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = LSTM(input_size=13, hidden_size=64, num_layers=2, output_size=1).to(device)
 dataset = ICUData(TRAIN_DATA_PATH, LABEL_FILE)
@@ -59,3 +41,8 @@ criterion = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 num_epochs = 100
 train_model(model, data_loader, criterion, optimizer, num_epochs)
+def predict_icu_mortality(patient_data):
+    model.eval()
+    with torch.no_grad():
+        prediction = model(patient_data)
+        return torch.sigmoid(prediction).item()
