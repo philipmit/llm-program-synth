133ca04a79e43c53f9ed56672a46a6df73daf108
philipmit <pschro@mit.edu>
2023-11-13 23:29:23 -0500

    Output 17 (phase 3): Response to improving AUC of 0.7513373431667689
    AUC from previous code:
    0.7513373431667689
    
    Response from GPT:
    ```
    import numpy as np
    import pandas as pd
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    from sklearn.preprocessing import StandardScaler
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Get features and labels
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # replace strings with numbers in y
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    
    # Standardize the data
    scaler = StandardScaler()
    scaler.fit(X)
    X = scaler.transform(X)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initialize and train RandomForestClassifier
    rf_model = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=42)
    rf_model.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(sample):
        sample_reshaped = np.array(sample).reshape(1, -1)
        # Standardize the test sample
        sample_reshaped = scaler.transform(sample_reshaped)
        predicted_probabilities = rf_model.predict_proba(sample_reshaped)
        # Add zero-probabilities for classes that were not in the training data
        complete_probabilities = np.zeros(len(label_encoder.classes_))
        for class_index, probability in enumerate(predicted_probabilities[0]):
            complete_probabilities[class_index] = probability
        return complete_probabilities
    ```
    In this code, I made the following changes:
    1. I used RandomForestClassifier instead of LogisticRegression as the model, which can often achieve higher performance.
    2. I applied Standard Scaler to the data - this can help many types of models perform better.
    3. I increased the number of estimators and defined the maximum depth in the RandomForestClassifier to have control over the complexity of the model.
    4. I standardized the test sample inside the predict_label function to ensure the test data has the same scale as the training data.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test10.py
@@ -1,27 +1,33 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Get features and labels
-X = ecoli.iloc[:, 1:-1]  
-y = ecoli.iloc[:, -1]    
+X = ecoli.iloc[:, 1:-1]
+y = ecoli.iloc[:, -1]
 # replace strings with numbers in y
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
+# Standardize the data
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Initialize and train Logistic Regression model
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
-model.fit(X_train, y_train)
+# Initialize and train RandomForestClassifier
+rf_model = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=42)
+rf_model.fit(X_train, y_train)
 # Define the predict_label function
 def predict_label(sample):
     sample_reshaped = np.array(sample).reshape(1, -1)
-    predicted_probabilities = model.predict_proba(sample_reshaped)
+    # Standardize the test sample
+    sample_reshaped = scaler.transform(sample_reshaped)
+    predicted_probabilities = rf_model.predict_proba(sample_reshaped)
     # Add zero-probabilities for classes that were not in the training data
     complete_probabilities = np.zeros(len(label_encoder.classes_))
     for class_index, probability in enumerate(predicted_probabilities[0]):
