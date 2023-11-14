9ceb3b0203abd349f050bf4c3cf195227ce5bad8
philipmit <pschro@mit.edu>
2023-11-14 11:21:02 -0500

    Output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    This error arises because the `roc_auc_score` function expects the predicted probabilities (`prediction_label_list`) to have the same number of columns as the number of unique classes in the true labels (`true_label_list`). The scikit-learn Logistic Regression model will only output probabilities for the classes it has seen during training, but if there are classes in the test set which are not seen during training, this will cause a discrepancy causing the ValueError.
    
    The solution here is to ensure that during training, the model sees all classes that may appear in the test set. This is usually achieved by stratified splitting, that ensures an approximately equal representation of each class both in the training and test set.
    
    This can be fixed by modifying model training as follows:
    
    ```Python
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
    from sklearn.preprocessing import MinMaxScaler
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # Replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0, 1, 2, 3, 4, 5, 6, 7])
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Stratified Split the dataset into training and testing sets
    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
    for train_index, test_index in sss.split(X, y):
        X_train, X_test = X[train_index], X[test_index]
        y_train, y_test = y[train_index], y[test_index]
    
    # Feature scaling for normalization
    scaler = MinMaxScaler()
    X_train = scaler.fit_transform(X_train)
    # Train the model with one-vs-rest strategy
    model = LogisticRegression(multi_class='ovr', solver='lbfgs', max_iter=1000)
    model.fit(X_train, y_train)
    # Define the predict_label function
    def predict_label(sample):
        global scaler, model
        sample = np.array(sample).reshape(1, -1)
        sample = scaler.transform(sample)
        prediction = model.predict_proba(sample)
        return prediction.flatten()
    ```
    
    Please ensure to restart the kernel / environment before rerunning the validation code.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from sklearn.preprocessing import MinMaxScaler
 # Load the ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
@@ -12,8 +12,11 @@ y = ecoli.iloc[:, -1]   # All rows, only the last column
 y = y.replace(list(np.unique(y)), [0, 1, 2, 3, 4, 5, 6, 7])
 X = X.to_numpy()
 y = y.to_numpy()
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Stratified Split the dataset into training and testing sets
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
+for train_index, test_index in sss.split(X, y):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 # Feature scaling for normalization
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
