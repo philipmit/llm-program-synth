8ec67bdc6ddb6984de33dfecd61bb0e0059b721e
philipmit <pschro@mit.edu>
2023-11-13 22:11:29 -0500

    Output 7 (phase 3): Response to improving AUC of 0.7061078468291679
    AUC from previous code:
    0.7061078468291679
    
    Response from GPT:
    ```
    import numpy as np
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.ensemble import RandomForestClassifier
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Replace strings with numbers in y
    classes = list(np.unique(y))
    y = y.replace(classes, list(range(len(classes))))
    
    # Convert to numpy arrays
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the Random Forest Classifier model instead of Logistic Regression. This may lead to a better validation AUC
    clf = RandomForestClassifier(n_estimators=500, max_depth=10, random_state=0)
    clf.fit(X_train, y_train)
    
    # Define a function that predicts label for new data
    def predict_label(sample):
        sample = np.array(sample).reshape(1,-1)
    
        # Apply the same scaling to the sample
        sample = scaler.transform(sample)
    
        # Function returns a list of prediction probabilities
        probabilities = clf.predict_proba(sample)
        padding = [0]*(len(classes)-len(probabilities[0]))
        probabilities = np.append(probabilities,padding)
    
        return probabilities
    ```
    In the code above, I have changed the model from Linear Regression to Random Forest Classifier, which may help to improve the AUC. This is because Random Forests tends to work well even for complex classification tasks, unlike Logistic Regression which may not perform well if the classes are not linearly separable. I also increased the number of trees in the Random Forest (`n_estimators`) to 500, and set the maximum depth of these trees (`max_depth`) to 10. These changes may help the model to fit the training data in a more nuanced way, and thereby improve its generalization to unseen data.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
@@ -19,8 +20,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Scale the data
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Train the logistic regression model with 'multinomial' option for multi-class
-clf = LogisticRegression(random_state=0, multi_class='multinomial', max_iter=1000)
+# Train the Random Forest Classifier model instead of Logistic Regression. This may lead to a better validation AUC
+clf = RandomForestClassifier(n_estimators=500, max_depth=10, random_state=0)
 clf.fit(X_train, y_train)
 # Define a function that predicts label for new data
 def predict_label(sample):
