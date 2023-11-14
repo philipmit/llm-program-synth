0184a68a75c255e0cec8940651f0833383611722
philipmit <pschro@mit.edu>
2023-11-13 23:48:16 -0500

    Output 4 (phase 3): Response to improving AUC of 0.8608159975089622
    AUC from previous code:
    0.8608159975089622
    
    Response from GPT:
    ```
    import numpy as np
    import pandas as pd
    from sklearn.ensemble import GradientBoostingClassifier
    from sklearn.preprocessing import StandardScaler, LabelBinarizer
    from sklearn.model_selection import train_test_split
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns excluding sequence name and class
    y = ecoli.iloc[:, -1]   # All rows, only the class column
    
    # Replace string labels in y with numbers
    lb = LabelBinarizer()
    y = lb.fit_transform(y)
    y = [np.where(r==1)[0][0] for r in y]  # convert y from 2D array to 1D
    
    X = X.to_numpy()
    y = np.array(y)
    
    # Split dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # 80-20 split for more training data
    
    # Standardize train features
    scaler = StandardScaler()
    X_train_sc = scaler.fit_transform(X_train)
    
    # Initialising GradientBoostingClassifier for higher performance
    model = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.02, max_depth=3, random_state=42)
    model.fit(X_train_sc, y_train)
    
    # Define the predict_label function
    def predict_label(raw_input_sample):
        # Ensure raw_input_sample is numpy array and reshape(-1, 1) if necessary
        if isinstance(raw_input_sample, list):
            raw_input_sample = np.array(raw_input_sample)
        elif len(raw_input_sample.shape) == 1:
            raw_input_sample = raw_input_sample.reshape(1, -1)
    
        # Scale raw input sample
        sample = scaler.transform(raw_input_sample)
    
        # Perform prediction and return the probabilities for each class
        predicted_proba = model.predict_proba(sample)
    
        # Create a full length probabilities list for classes not predicted by the model
        full_length_proba = [0]*len(lb.classes_)
        for k, v in zip(model.classes_, predicted_proba[0]):
            full_length_proba[k] = v
    
        return full_length_proba
    ```
    
    In this updated version of the code, I've made the following changes:
    1. Replaced the RandomForestClassifier with the GradientBoostingClassifier
    2. Decreased the test size to 0.2 from 0.5 to provide more training data to the model
    3. Added learning rate to the GradientBoostingClassifier and adjusted the max depth
    These changes should improve the validation AUC.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test12.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test12.py
@@ -1,41 +1,40 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import StandardScaler, LabelBinarizer
 from sklearn.model_selection import train_test_split
 # Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
-y = ecoli.iloc[:, -1]   # All rows, only the last column
-# Label binarize the string labels in y
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns excluding sequence name and class
+y = ecoli.iloc[:, -1]   # All rows, only the class column
+# Replace string labels in y with numbers
 lb = LabelBinarizer()
-y = lb.fit_transform(y)
-# converting the 2-D array to 1-D
-y = [np.where(r==1)[0][0] for r in y]
+y = lb.fit_transform(y)  
+y = [np.where(r==1)[0][0] for r in y]  # convert y from 2D array to 1D
 X = X.to_numpy()
 y = np.array(y)
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Normalize the features
+# Split dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # 80-20 split for more training data
+# Standardize train features
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-# Train the RandomForestClassifier model
-model = RandomForestClassifier(n_estimators=1000, max_depth=None, random_state=42)
-model.fit(X_train, y_train)
-# Define a predict function
+X_train_sc = scaler.fit_transform(X_train)  
+# Initialising GradientBoostingClassifier for higher performance
+model = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.02, max_depth=3, random_state=42)
+model.fit(X_train_sc, y_train)
+# Define the predict_label function
 def predict_label(raw_input_sample):
-    # Ensure the raw_input_sample is numpy array and reshape(-1, 1) if necessary
+    # Ensure raw_input_sample is numpy array and reshape(-1, 1) if necessary
     if isinstance(raw_input_sample, list):
         raw_input_sample = np.array(raw_input_sample)
     elif len(raw_input_sample.shape) == 1:
         raw_input_sample = raw_input_sample.reshape(1, -1)
-    # Normalize the raw input sample
+    # Scale raw input sample
     sample = scaler.transform(raw_input_sample)
-    # Perform prediction and return the probability for each class
-    probabilities = model.predict_proba(sample)
-    # Creating a full length probabilities list for classes not predicted by the model
+    # Perform prediction and return the probabilities for each class
+    predicted_proba = model.predict_proba(sample)
+    # Create a full length probabilities list for classes not predicted by the model
     full_length_proba = [0]*len(lb.classes_)
-    for k, v in zip(model.classes_, probabilities[0]):
+    for k, v in zip(model.classes_, predicted_proba[0]):
         full_length_proba[k] = v
     return full_length_proba
