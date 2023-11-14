4883e6112c6e762aa7d5c9286e0f306e54aa0b26
philipmit <pschro@mit.edu>
2023-11-13 23:52:40 -0500

    Output 5 (phase 3): Response to improving AUC of 0.7275666766896205
    AUC from previous code:
    0.7275666766896205
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.ensemble import GradientBoostingClassifier
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Replace string-type class labels with integer labels
    classes = list(np.unique(y))
    y = y.replace(classes, range(len(classes)))
    
    # Convert the pandas DataFrame to numpy array
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Standardize the training data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Attempt to improve validation AUC by using GradientBoostingClassifier which generally provides better performance than RandomForestClassifier
    # Adjust the learning rate, n_estimators, and max_depth parameters to achieve optimal results
    model = GradientBoostingClassifier(learning_rate=0.1, n_estimators=500, max_depth=5, random_state=42)
    model.fit(X_train, y_train)
    
    def predict_label(sample):
        # Reshape sample data into correct dimensions and scale it
        sample = np.array(sample).reshape(1, -1)
        sample = scaler.transform(sample)
    
        # Get class probabilities
        probabilities = model.predict_proba(sample)[0]
    
        # Manually append zeros to probabilities array if necessary
        if len(probabilities) < len(classes):
            probabilities = np.append(probabilities, [0]*(len(classes) - len(probabilities)))
    
        return probabilities
    ```
    In the code, I have replaced RandomForestClassifier with GradientBoostingClassifier and fine-tuned the model parameters for potentially better performance. The learning rate, number of estimators, and maximum tree depth might need to be tuned further depending on the specific characteristics of the data.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test13.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test13.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 # Load the Ecoli dataset
@@ -8,9 +8,10 @@ ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
-# replace strings with numbers in y
+# Replace string-type class labels with integer labels
 classes = list(np.unique(y))
 y = y.replace(classes, range(len(classes)))
+# Convert the pandas DataFrame to numpy array
 X = X.to_numpy()
 y = y.to_numpy()
 # Split the dataset into training and testing sets
@@ -18,15 +19,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Standardize the training data
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
-# Initialize RandomForestClassifier and fit it to the training data instead of Logistic Regression
-model = RandomForestClassifier(n_estimators=500, random_state=42, max_depth=100)
+# Attempt to improve validation AUC by using GradientBoostingClassifier which generally provides better performance than RandomForestClassifier
+# Adjust the learning rate, n_estimators, and max_depth parameters to achieve optimal results
+model = GradientBoostingClassifier(learning_rate=0.1, n_estimators=500, max_depth=5, random_state=42)
 model.fit(X_train, y_train)
 def predict_label(sample):
-    # Convert the sample data to array, reshape and standardize it
+    # Reshape sample data into correct dimensions and scale it
     sample = np.array(sample).reshape(1, -1)
     sample = scaler.transform(sample)
-    # Predict probabilities and manually append zeros to match dimensions if necessary
+    # Get class probabilities
     probabilities = model.predict_proba(sample)[0]
+    # Manually append zeros to probabilities array if necessary
     if len(probabilities) < len(classes):
-        probabilities = np.append(probabilities, [0]*(len(classes)-len(probabilities)))
+        probabilities = np.append(probabilities, [0]*(len(classes) - len(probabilities)))
     return probabilities
