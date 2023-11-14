86aeb8d658497dabd6a360067045c9d50a2d2308
philipmit <pschro@mit.edu>
2023-11-13 19:09:34 -0500

    Output 2 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 605, in roc_auc_score
        y_score = check_array(y_score, ensure_2d=False)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 953, in check_array
        raise ValueError(
    <class 'ValueError'>
    Found array with dim 3. None expected <= 2.
    
    Response from GPT:
    The error is coming from the predict_label function in the provided GPT code. The output of the method predict_proba in the function is a set of probabilities for each label, so the return shape is two-dimensional (num_samples, num_classes). However, the function predict_label takes in one sample at a time, so the returned array has three dimensions (num_samples = 1, num_classes). But for evaluation, the roc_auc_score method from sklearn expects probabilities with shape (num_samples, num_classes) as input, hence the error.
    
    To solve this, the extra dimension in the output of the logistic regression predict_proba method can be squeezed off. Here is the corrected code:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn import preprocessing
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # All rows, all columns except the last one
    X = ecoli.iloc[:, 1:-1].to_numpy()
    # All rows, only the last column
    y = ecoli.iloc[:, -1]
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Train the Logistic Regression model
    log_reg = LogisticRegression()
    log_reg.fit(X_train, y_train)
    
    def predict_label(data):
        # Assumes data is a np array with shape (no. of features)
        preprocessed_data = data.reshape(1, -1)
        # Predict probabilities and squeeze off the extra dimension
        probabilities = np.squeeze(log_reg.predict_proba(preprocessed_data))
        return probabilities
    ```
    Now running the validation code should not throw any error as the predict_label function now returns a 2D Numpy array as expected by the roc_auc_score function.

--- UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
@@ -9,7 +9,7 @@ ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'al
 # All rows, all columns except the last one
 X = ecoli.iloc[:, 1:-1].to_numpy()
 # All rows, only the last column
-y = ecoli.iloc[:, -1]   
+y = ecoli.iloc[:, -1]
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
@@ -17,8 +17,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 log_reg = LogisticRegression()
 log_reg.fit(X_train, y_train) 
 def predict_label(data):
-    # assumes data is a np array with shape (1, no. of features) or a pandas Series with 'no. of features' elements
+    # Assumes data is a np array with shape (no. of features)
     preprocessed_data = data.reshape(1, -1)
-    # Predict probabilities
-    probabilities = log_reg.predict_proba(preprocessed_data)
+    # Predict probabilities and squeeze off the extra dimension
+    probabilities = np.squeeze(log_reg.predict_proba(preprocessed_data))
     return probabilities
