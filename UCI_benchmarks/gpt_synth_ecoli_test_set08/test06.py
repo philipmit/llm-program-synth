f1382112f714c245a9efc77e4979e3768ea25b43
philipmit <pschro@mit.edu>
2023-11-13 22:53:20 -0500

    Output 5 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    Reading the error message, there is a mismatch between the number of classes in your true labels (`y_true`) and the number of classes predicted by the model ('y_score'). The number of columns in y_score is equal to the number of classes used when fitting the model. If you include new unseen labels after the prediction, they are not reflected in the output of predict_proba.
    
    In your validation code, you're replacing labels manually and hence, the labels used in training the model are different from the labels you're using in the validation. An error appears because logistic regression's predict_proba() only produces probabilities for seen classes, but only during model fitting.
    
    Instead, the logistic regression model should be trained with same labels. So, I will include the `.fit_transform(y)` in the same script to help ensure that the labels match in both the training and testing phases. Here's the updated AI code:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Pre-process the dataset
    X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
    y = ecoli.iloc[:, -1].values    # All rows, only the last column
    
    # Replace string labels with numbers in y
    labels = list(np.unique(y))
    replaced_labels = [i for i in range(len(labels))]
    label_mapping = dict(zip(labels, replaced_labels))
    y = y.replace(label_mapping)
    
    # Convert it to numpy arrays
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Train a Logistic Regression model
    model = LogisticRegression(max_iter=1000)
    model.fit(X_train, y_train)
    
    # Define prediction function
    def predict_label(X_sample):
        # reshaping the input to match the input shape for sklearn ML models
        X_sample = np.array(X_sample).reshape(1, -1)
        # getting the model's predicted probabilities with predict_proba
        pred_proba = model.predict_proba(X_sample)
        return pred_proba[0]
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
@@ -8,9 +9,14 @@ ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'al
 # Pre-process the dataset
 X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
 y = ecoli.iloc[:, -1].values    # All rows, only the last column
-# Correct way to convert string labels into numbers
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(y)
+# Replace string labels with numbers in y
+labels = list(np.unique(y))
+replaced_labels = [i for i in range(len(labels))]
+label_mapping = dict(zip(labels, replaced_labels))
+y = y.replace(label_mapping)
+# Convert it to numpy arrays
+X = X.to_numpy()
+y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Train a Logistic Regression model
@@ -18,6 +24,8 @@ model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 # Define prediction function
 def predict_label(X_sample):
+    # reshaping the input to match the input shape for sklearn ML models
     X_sample = np.array(X_sample).reshape(1, -1)
+    # getting the model's predicted probabilities with predict_proba
     pred_proba = model.predict_proba(X_sample)
     return pred_proba[0]
