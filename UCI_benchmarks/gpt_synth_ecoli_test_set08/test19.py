f466e4286eb132ca52957e6d217f3982cb040038
philipmit <pschro@mit.edu>
2023-11-14 11:20:28 -0500

    Output 3 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    To fix the ValueError, we need to ensure the predicted probabilities are consistent with the list of unique labels in the dataset. This error occurs because the prediction is based on labels present in y_train, which might not include all the 8 labels (0-7). We can guarantee this by reframing our Logistic Regression with a One Vs Rest (OvR) strategy where a binary problem is fit for each unique label in the dataset. Instead of 'multinomial', we'll set multi_class to 'ovr'.
    
    Below is the modified GPT code.
    
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import MinMaxScaler
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    
    # Replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0, 1, 2, 3, 4, 5, 6, 7])
    
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Feature scaling for normalization
    scaler = MinMaxScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the model with one-vs-rest strategy
    model = LogisticRegression(multi_class='ovr', solver='lbfgs', max_iter=1000)
    model.fit(X_train, y_train)
    
    # Define the predict_label function
    def predict_label(sample):
        global scaler, model
        sample = np.array(sample).reshape(1, -1)
        sample = scaler.transform(sample)
        prediction = model.predict_proba(sample)
        return prediction.flatten()
    ```
    Please note that the Logistic Regression model is now trained to predict probabilities for each of the 8 classes (even though some may not appear in the training data). So the issue relating to the class count discrepancy should no longer happen.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test19.py
@@ -9,8 +9,6 @@ ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'al
 X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1]   # All rows, only the last column
 # Replace strings with numbers in y
-np.unique(y)
-len(list(np.unique(y)))
 y = y.replace(list(np.unique(y)), [0, 1, 2, 3, 4, 5, 6, 7])
 X = X.to_numpy()
 y = y.to_numpy()
@@ -19,8 +17,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Feature scaling for normalization
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
-# Train the model
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+# Train the model with one-vs-rest strategy
+model = LogisticRegression(multi_class='ovr', solver='lbfgs', max_iter=1000)
 model.fit(X_train, y_train)
 # Define the predict_label function
 def predict_label(sample):
@@ -28,4 +26,4 @@ def predict_label(sample):
     sample = np.array(sample).reshape(1, -1)
     sample = scaler.transform(sample)
     prediction = model.predict_proba(sample)
-    return prediction.flatten() #flatten method is used here to make a 1D array.
+    return prediction.flatten()
