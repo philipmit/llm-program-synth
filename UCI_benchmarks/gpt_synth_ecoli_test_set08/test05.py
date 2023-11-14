31a7cab9ad47767ab9122a5ccbed4678f4b9f8ab
philipmit <pschro@mit.edu>
2023-11-13 22:50:24 -0500

    Output 4 (phase 3): Response to improving AUC of 0.9425393795721805
    AUC from previous code:
    0.9425393795721805
    
    Response from GPT:
    ```
    import pandas as pd
    from sklearn.model_selection import StratifiedShuffleSplit
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1].to_numpy()  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1].replace(list(np.unique(ecoli.iloc[:, -1])), [0,1,2,3,4,5,6,7]).to_numpy()   # All rows, only the last column which is replaced with integers
    
    # Standardizing the X
    scaler = StandardScaler()
    X = scaler.fit_transform(X)
    
    # Use StratifiedShuffleSplit to make sure all classes are represented in the train/test split
    strat_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
    for train_index, test_index in strat_split.split(X, y):
        X_train, X_test = X[train_index], X[test_index]
        y_train, y_test = y[train_index], y[test_index]
    
    # Training the model
    model = LogisticRegression(multi_class='multinomial', max_iter=5000, solver='saga', penalty='l1', C=0.5)
    model.fit(X_train, y_train)
    
    # Define the function predict_label
    def predict_label(sample):
        sample = scaler.transform([sample]) # Standardize the new inputs as well
        return model.predict_proba(sample)[0]
    ```
    In this optimized code, I have made three key changes:
    
    1. I have added a standard scaling procedure before splitting the data to make it more suitable for logistic regression.
    2. I have changed the test split size from 0.5 to 0.2 to give more data for training the model.
    3. I have increased the `max_iter` value to 5000 to allow for more convergence and changed the solver to 'saga' which is faster for larger datasets along with including the l1 penalty to produce sparser models and reduced the inverse of regularization strength 'C' to 0.5 to increase the regularization effect.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test05.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test05.py
@@ -1,19 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1].to_numpy()  # All rows, all columns except the last one
 y = ecoli.iloc[:, -1].replace(list(np.unique(ecoli.iloc[:, -1])), [0,1,2,3,4,5,6,7]).to_numpy()   # All rows, only the last column which is replaced with integers
+# Standardizing the X
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 # Use StratifiedShuffleSplit to make sure all classes are represented in the train/test split
-strat_split = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in strat_split.split(X, y):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 # Training the model
-model = LogisticRegression(multi_class='multinomial', max_iter=1000, solver='lbfgs')
+model = LogisticRegression(multi_class='multinomial', max_iter=5000, solver='saga', penalty='l1', C=0.5)
 model.fit(X_train, y_train)
 # Define the function predict_label
 def predict_label(sample):
-    return model.predict_proba([sample])[0]
+    sample = scaler.transform([sample]) # Standardize the new inputs as well
+    return model.predict_proba(sample)[0]
