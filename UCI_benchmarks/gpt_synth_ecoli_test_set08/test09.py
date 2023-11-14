057226d516a98fdd0a3393fc4671d50608564edc
philipmit <pschro@mit.edu>
2023-11-13 23:17:23 -0500

    Output 5 (phase 3): Response to improving AUC of 0.9202971064520504
    AUC from previous code:
    0.9202971064520504
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    from sklearn.ensemble import GradientBoostingClassifier
    from sklearn.pipeline import make_pipeline
    from sklearn.decomposition import PCA
    
    # Load the dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Split the data into features and target
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Replace string labels with numbers in y
    label_keys = np.unique(y)
    label_values = range(len(label_keys))
    y = y.replace(label_keys, label_values)
    
    # Split the data into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, stratify=y, random_state = 42)
    
    # Define a pipeline that standardize the data, applies PCA, and then uses Gradient Boosting Classifier
    pipeline = make_pipeline(StandardScaler(),
                             PCA(n_components = 5),
                             GradientBoostingClassifier(n_estimators=500, learning_rate=0.01, max_depth=5, random_state=42))
    
    # Train the model
    pipeline.fit(X_train, y_train)
    
    def predict_label(raw_sample):
        # Reshape the sample
        raw_sample = raw_sample.reshape(1, -1)
        # Predict the probabilities for each class
        probabilities = pipeline.predict_proba(raw_sample)[0]
    
        return probabilities
    ```
    In this code, replacing the RandomForest by the GradientBoostingClassifier and decreasing the learning rate to 0.01 while increasing the number of trees to 500 could potentially increase the AUC score by improving the performance of the model.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test09.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test09.py
@@ -3,31 +3,30 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.decomposition import PCA
-#Load dataset
+# Load the dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-#Split into predictors and target
-X = ecoli.iloc[:, 1:-1] 
-y = ecoli.iloc[:, -1] 
-#Convert class labels to numeric
-y_labels = np.unique(y)
-y = y.replace(y_labels, np.arange(len(y_labels)))
-#Split the data, stratifying on y to ensure balance
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
-# Define pipeline (with not only standard scaling but also PCA for dimension reduction)
-# Use Random Forest Classifier for potentially better performance
+# Split the data into features and target
+X = ecoli.iloc[:, 1:-1]
+y = ecoli.iloc[:, -1]
+# Replace string labels with numbers in y
+label_keys = np.unique(y)
+label_values = range(len(label_keys))
+y = y.replace(label_keys, label_values)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, stratify=y, random_state = 42)
+# Define a pipeline that standardize the data, applies PCA, and then uses Gradient Boosting Classifier
 pipeline = make_pipeline(StandardScaler(), 
-                         PCA(n_components = 5), 
-                         RandomForestClassifier(n_estimators=500, max_depth=10, random_state=42))
-# Use the pipeline to fit and predict
+                         PCA(n_components = 5),
+                         GradientBoostingClassifier(n_estimators=500, learning_rate=0.01, max_depth=5, random_state=42))
+# Train the model
 pipeline.fit(X_train, y_train)
-# Define prediction function
 def predict_label(raw_sample):
     # Reshape the sample
     raw_sample = raw_sample.reshape(1, -1)
     # Predict the probabilities for each class
-    probabilities = pipeline.predict_proba(raw_sample)
-    return probabilities[0]
+    probabilities = pipeline.predict_proba(raw_sample)[0]
+    return probabilities 
