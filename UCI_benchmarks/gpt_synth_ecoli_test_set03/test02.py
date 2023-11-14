0aa4167cfec2f2d5537b7be8d39fa24096a2c482
philipmit <pschro@mit.edu>
2023-11-13 19:12:19 -0500

    Output 5 (phase 3): Response to ValueError
    Error from previous code:
      File "<stdin>", line 292, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    ```
    import numpy as np
    import pandas as pd
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all the columns except the last one
    y = ecoli.iloc[:, -1]  # All the rows, only the last
    
    # Transform string values to numerical
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into a training set and a test set
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Initializing a scalar and a model
    scaler = StandardScaler()
    model = LogisticRegression(max_iter=1000, penalty='l2')
    
    # Fitting the scalar and model on X_train
    X_train = scaler.fit_transform(X_train)
    model.fit(X_train, y_train)
    
    # Defining function that takes raw unprocessed data for a single sample
    def predict_label(raw_data):
        processed_data = scaler.transform(raw_data.reshape(1, -1))  # Preprocess the data
        probabilities = model.predict_proba(processed_data)  # Apply the model to make predictions
        # Create a probability list by padding zeros for absent classes to align length with 8 (Number of classes)
        p = probabilities[0].tolist() + [0]*(8 - len(probabilities[0]))
        return p
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
@@ -2,26 +2,27 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]  # All rows, all the columns except the last one 
 y = ecoli.iloc[:, -1]  # All the rows, only the last
-y = y.replace(np.unique(y), list(range(len(np.unique(y)))))
+# Transform string values to numerical
+le = LabelEncoder()
+y = le.fit_transform(y)
 # Split the dataset into a training set and a test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Initializing a scalar
+# Initializing a scalar and a model
 scaler = StandardScaler()
-# Fitting scalar on X_train
-X_train = scaler.fit_transform(X_train)
-# define the model
 model = LogisticRegression(max_iter=1000, penalty='l2')
-# fit the model
+# Fitting the scalar and model on X_train
+X_train = scaler.fit_transform(X_train)
 model.fit(X_train, y_train)
 # Defining function that takes raw unprocessed data for a single sample
-# function should return the predicted label for that sample
 def predict_label(raw_data):
-    ready_data = scaler.transform(raw_data.reshape(1, -1))  # Preprocess the data 
-    probabilities = model.predict_proba(ready_data)  # Apply the model to make predictions
-    return probabilities[0]  # Return the probabilities for the sample
+    processed_data = scaler.transform(raw_data.reshape(1, -1))  # Preprocess the data 
+    probabilities = model.predict_proba(processed_data)  # Apply the model to make predictions
+    # Create a probability list by padding zeros for absent classes to align length with 8 (Number of classes)
+    p = probabilities[0].tolist() + [0]*(8 - len(probabilities[0]))
+    return p 
