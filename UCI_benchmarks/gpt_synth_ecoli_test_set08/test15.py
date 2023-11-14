01036e44c1fd0c1c6c293a9888c7d2475e6b0edd
philipmit <pschro@mit.edu>
2023-11-14 10:36:12 -0500

    Initial output: Here is the solution:
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.model_selection import train_test_split
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
    y = ecoli.iloc[:, -1]   # All rows, only the last column
    # replace strings with numbers in y
    np.unique(y)
    len(list(np.unique(y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the logistic regression model
    lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=400)
    lr.fit(X_train, y_train)
    
    def predict_label(sample):
        """
        Predict the class probabilities for a given sample.
    
        Parameters:
        sample -- the raw unprocessed data for a sample
    
        Returns:
        the predicted probabilities for the sample
        """
        return lr.predict_proba([sample])[0]
    ```
    This code will train a logistic regression model using the provided training data, and it includes a function called "predict_label" that predict the probabilities of a given sample. In the function predict_label, the input is a vector with the feature values of a sample. The output of this function is an array with the predicted probabilities for each possible class of the sample.
    
    Please replace the file path in the read_csv function with the path where you've stored the Ecoli dataset. Also, ensure that the solver, multi_class, and max_iter parameters for the LogisticRegression classifier are suitable for your specific requirements.

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test15.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+# Load the Ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
+y = ecoli.iloc[:, -1]   # All rows, only the last column
+# replace strings with numbers in y
+np.unique(y)
+len(list(np.unique(y)))
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X = X.to_numpy()
+y = y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train the logistic regression model
+lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=400)
+lr.fit(X_train, y_train)
+def predict_label(sample):
+    """
+    Predict the class probabilities for a given sample.
+    Parameters:
+    sample -- the raw unprocessed data for a sample
+    Returns: 
+    the predicted probabilities for the sample
+    """
+    return lr.predict_proba([sample])[0]
