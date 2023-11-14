13da4b017493ee86617a53c0e6eb6ed519dd131e
philipmit <pschro@mit.edu>
2023-11-13 20:10:22 -0500

    Output 7 (phase 3): Response to improving AUC of 0.9601844518722107
    AUC from previous code:
    0.9601844518722107
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.ensemble import RandomForestClassifier
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Preprocess
    X = ecoli.iloc[:, 1:-1]  # All rows, all columns excluding the last one
    y = ecoli.iloc[:, -1]   # All rows, the last column only
    
    # Replace classes with integers
    labels = list(np.unique(y))
    y = y.replace(labels, list(range(len(labels))))
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into train and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
    
    # Standardize the features
    scaler = StandardScaler()
    scaler.fit(X_train)
    X_train = scaler.transform(X_train)
    
    # Fit a Random Forest Classifier - change in model architecture may improve performance
    clf = RandomForestClassifier(n_estimators=1000, max_depth=None, random_state=42)
    clf.fit(X_train, y_train)
    
    def predict_icu_mortality(sample):
        # Apply the same scaling to the sample
        sample = scaler.transform([sample])
        # Use the RandomForest classifier model to predict the label for the sample
        predicted_probabilities = clf.predict_proba(sample)
        return predicted_probabilities[0]
    ```
    The change here is the usage of Random Forest Classifier instead of Gradient Boosting Classifier. Random Forests can often provide better performance and generalization because they reduce variance by averaging numerous deep decision trees trained on different parts of the same training set. Using more number of estimators (trees) in the Random Forest and allowing unlimited depth for the trees may lead to better performance.  Random forests also handle imbalance in the data and outliers well.

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test03.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test03.py
@@ -2,29 +2,30 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Preprocess
-X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
-y = ecoli.iloc[:, -1] # All rows, only the last column
+X = ecoli.iloc[:, 1:-1]  # All rows, all columns excluding the last one
+y = ecoli.iloc[:, -1]   # All rows, the last column only
+# Replace classes with integers
 labels = list(np.unique(y))
 y = y.replace(labels, list(range(len(labels))))
 X = X.to_numpy()
 y = y.to_numpy()
-# Ensure that all classes have at least one sample in train set by using stratify
+# Split the dataset into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
-# Scale
+# Standardize the features
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
-# Gradient Boosting Classifier
-clf = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.01,max_depth=5, random_state=42)
+# Fit a Random Forest Classifier - change in model architecture may improve performance
+clf = RandomForestClassifier(n_estimators=1000, max_depth=None, random_state=42)
 clf.fit(X_train, y_train)
 def predict_icu_mortality(sample): 
     # Apply the same scaling to the sample
     sample = scaler.transform([sample])
-    # Use the gradient boosting classifier model to predict the label for the sample
+    # Use the RandomForest classifier model to predict the label for the sample
     predicted_probabilities = clf.predict_proba(sample)
     return predicted_probabilities[0]
