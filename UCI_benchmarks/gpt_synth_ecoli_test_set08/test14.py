e0a1c3f41a9b40b40ec973289648ef7113b8e913
philipmit <pschro@mit.edu>
2023-11-13 23:58:35 -0500

    Output 8 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The problem has to do with the predict_label function. The sklearn LogisticRegression predict_proba function returns arrays of shape `(n_samples, n_classes)`. If you only pass a single raw unprocessed sample to the function, it will reshape the sample into a (1, n_features) array and then return an array of shape (1, n_classes).
    
    Your validation code is only passing a single sample to predict_label at a time. This results in the `predict_proba` function only ever returns a probability distribution over the classes for a single sample at a time, which means it is only ever returning 1D arrays of shape `(n_classes,)`.
    
    Since roc_auc_score expects arrays of shape `(n_samples, n_classes)` for its `y_score` parameter, you need to modify the predict_label function to return 2D arrays of shape `(1, n_classes)` even when a single sample is passed to it.
    
    Here is your revised GPT code:
    ```py
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import LabelEncoder
    from sklearn.linear_model import LogisticRegression
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1].values
    y = ecoli.iloc[:, -1]
    
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train a Logistic Regression Model
    log_reg = LogisticRegression(max_iter=1000, multi_class='ovr')
    log_reg.fit(X_train, y_train)
    
    def predict_label(raw_data):
        """
        Predict probabilities for a given raw unprocessed data.
        """
        raw_data = np.array(raw_data).reshape(1, -1)
        probabilities = log_reg.predict_proba(raw_data)
        return probabilities
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test14.py
@@ -1,28 +1,25 @@
+py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 # Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
                     delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-# All rows, all columns except the last one for features
-# All rows, only the last column for the target
 X = ecoli.iloc[:, 1:-1].values  
-# replace strings with numbers in y
 y = ecoli.iloc[:, -1]
-unique_classes = np.unique(y)
-y = y.replace(unique_classes, list(range(len(unique_classes))), inplace=False)
-# Now convert to numpy array
-y = y.values
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.5, random_state=42)
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Train a Logistic Regression Model
 log_reg = LogisticRegression(max_iter=1000, multi_class='ovr')
 log_reg.fit(X_train, y_train)
 def predict_label(raw_data):
-    """ Predict probabilities for a given raw unprocessed data. """
+    """ 
+    Predict probabilities for a given raw unprocessed data. 
+    """
     raw_data = np.array(raw_data).reshape(1, -1)
     probabilities = log_reg.predict_proba(raw_data)
-    return probabilities[0]
+    return probabilities
