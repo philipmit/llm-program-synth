c7d58d720c392ef65302234214e11620c6a9aef6
philipmit <pschro@mit.edu>
2023-11-14 11:26:44 -0500

    Output 4 (phase 2): Response to AttributeError
    Error from previous code:
      File "<stdin>", line 147, in <module>
      File "<string>", line 15, in <module>
    <class 'AttributeError'>
    'numpy.ndarray' object has no attribute 'to_numpy'
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data',
                        delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1].values    # All rows, all columns except the last one
    y = ecoli.iloc[:, -1].values   # All rows, only the last column
    
    # Replace strings with numbers in y using label encoder
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create a logistic regression model and fit with training data
    log_reg = LogisticRegression(max_iter=2000, multi_class='ovr')
    log_reg.fit(X_train, y_train)
    
    # Function to predict label
    def predict_label(sample):
        sample = np.array(sample).reshape(1, -1)
        predicted_probabilities = log_reg.predict_proba(sample)
        # Create an array to hold probabilities for each class
        probabilities = np.zeros(len(le.classes_))
        # Assign predicted probabilities to respective index
        probabilities[log_reg.classes_] = predicted_probabilities[0]
        return probabilities
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test20.py
@@ -4,26 +4,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 # Load the ecoli dataset
-ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', 
+                    delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows, all columns except the last one
-y = ecoli.iloc[:, -1]   # All rows, only the last column
+X = ecoli.iloc[:, 1:-1].values    # All rows, all columns except the last one
+y = ecoli.iloc[:, -1].values   # All rows, only the last column
 # Replace strings with numbers in y using label encoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-X = X.to_numpy()
-y = y.to_numpy()
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Create a logistic regression model and fit with training data
-log_reg = LogisticRegression(max_iter = 2000, multi_class='ovr')
+log_reg = LogisticRegression(max_iter=2000, multi_class='ovr')
 log_reg.fit(X_train, y_train)
 # Function to predict label
 def predict_label(sample):
     sample = np.array(sample).reshape(1, -1)
     predicted_probabilities = log_reg.predict_proba(sample)
-    # create an array to hold probabilities for each class
+    # Create an array to hold probabilities for each class
     probabilities = np.zeros(len(le.classes_))
-    # assign predicted probabilities to respective index
-    probabilities[log_reg.classes_] = predicted_probabilities
+    # Assign predicted probabilities to respective index
+    probabilities[log_reg.classes_] = predicted_probabilities[0]
     return probabilities
