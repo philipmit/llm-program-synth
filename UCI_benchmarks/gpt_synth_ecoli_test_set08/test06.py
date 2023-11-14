0621466df1857b9c67eb521c4875d4ef699399c6
philipmit <pschro@mit.edu>
2023-11-13 22:54:30 -0500

    Output 8 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    The error arises because the Logistic Regression model is not determining the correct number of classes for the multi-class label in your dataset. This can be resolved by explicitly providing the unique classes' list to the Logistic Regression model using the `classes_` attribute. Here is the modified code:
    
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import LabelEncoder
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Pre-process the dataset
    X = ecoli.iloc[:, 1:-1].values  # All rows, all the columns except the last one
    y = ecoli.iloc[:, -1].values # All rows, only the last column
    
    # Replace string labels with numbers
    label_encoder = LabelEncoder()
    y = label_encoder.fit_transform(y)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Define logistic regression model and train the model
    model = LogisticRegression(max_iter=1000)
    model.fit(X_train, y_train)
    
    # Explicitly providing unique classes to model
    model.classes_ = np.unique(y)
    
    # Define function to predict labels
    def predict_label(X_sample):
        # reshaping the input to match the input shape for sklearn ML models
        X_sample = np.array(X_sample).reshape(1, -1)
        # getting the model's predicted probabilities with predict_proba
        pred_proba = model.predict_proba(X_sample)
        return pred_proba[0]
    ```
    The `model.classes_` line updates the classes the LogisticRegression object uses when it predicts probabilities for each class. This ensures that the classifier uses the same labels as presented in the original dataset.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test06.py
@@ -14,9 +14,12 @@ label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Train a Logistic Regression model
+# Define logistic regression model and train the model
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
+# Explicitly providing unique classes to model
+model.classes_ = np.unique(y)
+# Define function to predict labels
 def predict_label(X_sample):
     # reshaping the input to match the input shape for sklearn ML models
     X_sample = np.array(X_sample).reshape(1, -1)
