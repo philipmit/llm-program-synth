b529c766f12826ce2ce7dbe228903817d96a7a83
philipmit <pschro@mit.edu>
2023-11-13 19:09:03 -0500

    Initial output: Here is the complete code that loads the data, pre-processes the data, trains the Logistic Regression model, and defines a function to predict the label for a single sample:
    ```python
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn import preprocessing
    
    # Load the ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # All rows, all columns except the last one
    X = ecoli.iloc[:, 1:-1].to_numpy()
    
    # All rows, only the last column
    y = ecoli.iloc[:, -1]
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Train the Logistic Regression model
    log_reg = LogisticRegression()
    log_reg.fit(X_train, y_train)
    
    def predict_label(data):
        # assumes data is a np array with shape (1, no. of features) or a pandas Series with 'no. of features' elements
        preprocessed_data = data.reshape(1, -1)
    
        # Predict probabilities
        probabilities = log_reg.predict_proba(preprocessed_data)
    
        return probabilities
    ```
    In the above code, the `predict_label` function was provided with unprocessed data for a single sample of unknown class and returns the predicted probabilities for each class. The function accepts either a numpy array or a pandas series as input. Please ensure that the shape of the input array should be (1, number of features) i.e., it should be a 2D array with only 1 row and 7 columns in this case. The function returns an array whose elements are the probabilities that this sample belongs to each of the classes.

---
+++ UCI_benchmarks/gpt_synth_ecoli_test_set03/test02.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+# Load the ecoli dataset
+ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+# All rows, all columns except the last one
+X = ecoli.iloc[:, 1:-1].to_numpy()
+# All rows, only the last column
+y = ecoli.iloc[:, -1]   
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7]).to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Train the Logistic Regression model
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train) 
+def predict_label(data):
+    # assumes data is a np array with shape (1, no. of features) or a pandas Series with 'no. of features' elements
+    preprocessed_data = data.reshape(1, -1)
+    # Predict probabilities
+    probabilities = log_reg.predict_proba(preprocessed_data)
+    return probabilities
