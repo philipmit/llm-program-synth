e04bdd67be92eb6a5e8741ef238d72948e5b469f
philipmit <pschro@mit.edu>
2023-11-13 23:16:33 -0500

    Output 5 (phase 3): Response to improving AUC of 0.8949908873124469
    AUC from previous code:
    0.8949908873124469
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler
    from sklearn.linear_model import LogisticRegression
    from sklearn.ensemble import RandomForestClassifier
    from sklearn.pipeline import make_pipeline
    from sklearn.decomposition import PCA
    
    #Load dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    #Split into predictors and target
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    #Convert class labels to numeric
    y_labels = np.unique(y)
    y = y.replace(y_labels, np.arange(len(y_labels)))
    
    #Split the data, stratifying on y to ensure balance
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
    
    # Define pipeline (with not only standard scaling but also PCA for dimension reduction)
    # Use Random Forest Classifier for potentially better performance
    pipeline = make_pipeline(StandardScaler(),
                             PCA(n_components = 5),
                             RandomForestClassifier(n_estimators=500, max_depth=10, random_state=42))
    
    # Use the pipeline to fit and predict
    pipeline.fit(X_train, y_train)
    
    # Define prediction function
    def predict_label(raw_sample):
        # Reshape the sample
        raw_sample = raw_sample.reshape(1, -1)
        # Predict the probabilities for each class
        probabilities = pipeline.predict_proba(raw_sample)
        return probabilities[0]
    ```
    The difference here is that I've introduced RandomForestClassifier instead of the Logistic Regression model in the pipeline. Random Forest could provide better performance by leveraging multiple decision trees and outputting the class that is the mode of the classes output by individual trees. They also handle well with varying importance among features and are less liable to overfitting. This, in turn, could improve the AUC score. The number of estimators (n_estimators) and the maximum depth (max_depth) of the tree could now be configured for further tuning. I've also kept the PCA for dimensionality reduction considering the dataset might have multicollinearity effects.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test09.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test09.py
@@ -3,21 +3,25 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
-# Load dataset
+from sklearn.decomposition import PCA
+#Load dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-# Split into predictors and target
-X = ecoli.iloc[:, 1:-1]
-y = ecoli.iloc[:, -1]
-# Convert class labels to numeric
+#Split into predictors and target
+X = ecoli.iloc[:, 1:-1] 
+y = ecoli.iloc[:, -1] 
+#Convert class labels to numeric
 y_labels = np.unique(y)
 y = y.replace(y_labels, np.arange(len(y_labels)))
-# Split the data, stratifying on y to ensure balance
+#Split the data, stratifying on y to ensure balance
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
 # Define pipeline (with not only standard scaling but also PCA for dimension reduction)
-from sklearn.decomposition import PCA
-pipeline = make_pipeline(StandardScaler(), PCA(n_components = 5), LogisticRegression(multi_class='ovr', max_iter=500, class_weight='balanced', solver='liblinear'))
+# Use Random Forest Classifier for potentially better performance
+pipeline = make_pipeline(StandardScaler(), 
+                         PCA(n_components = 5), 
+                         RandomForestClassifier(n_estimators=500, max_depth=10, random_state=42))
 # Use the pipeline to fit and predict
 pipeline.fit(X_train, y_train)
 # Define prediction function
