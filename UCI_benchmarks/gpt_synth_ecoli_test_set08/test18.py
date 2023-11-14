8170392113a92ade5d8b410275e4b68de13451c0
philipmit <pschro@mit.edu>
2023-11-14 11:14:25 -0500

    Output 4 (phase 3): Response to improving AUC of 0.8540795162561916
    AUC from previous code:
    0.8540795162561916
    
    Response from GPT:
    ```
    from sklearn.ensemble import RandomForestClassifier
    import numpy as np
    import pandas as pd
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1].values  # All rows, all columns excluding 'Sequence Name' and 'class'; Features
    y = ecoli.iloc[:, -1].values    # All rows, only last column, 'class'; Target labels
    # Replace string labels with unique integers
    unique_classes, y = np.unique(y, return_inverse=True)
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the data; Fit the scaler only on the training set for unbiased transformation
    scaler = StandardScaler().fit(X_train)
    X_train = scaler.transform(X_train)
    # Train the RandomForestClassifier model
    rf = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=0)
    rf.fit(X_train, y_train)
    # Define the function to predict class probabilities for a single raw sample
    def predict_label(single_sample):
        # Preprocess the sample just as we did the training data
        processed_sample = scaler.transform(np.array(single_sample).reshape(1, -1))
        # Get predicted probabilities from the model
        predicted_probabilities = rf.predict_proba(processed_sample)
        # Ensure probabilities for all classes by manually zero-filling for missing classes
        all_probs = np.zeros(len(unique_classes))
        all_probs[rf.classes_] = predicted_probabilities
        return all_probs
    ```
    This code improves on the original by using a RandomForestClassifier instead of logistic regression. Random forest classifiers often perform better in practice compared to other algorithms because they are robust to overfitting and can model complex, non-linear relationships. The parameters for the Random Forest Classifier were also adjusted, the number of trees in the forest (n_estimators) was increased to improve the performance. The maximum depth of the trees is also set to prevent overfitting.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test18.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test18.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -15,16 +15,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Scale the data; Fit the scaler only on the training set for unbiased transformation
 scaler = StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
-# Train the logistic regression model
-lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', max_iter=4000)
-lr.fit(X_train, y_train)
+# Train the RandomForestClassifier model
+rf = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=0)
+rf.fit(X_train, y_train)
 # Define the function to predict class probabilities for a single raw sample
 def predict_label(single_sample):
     # Preprocess the sample just as we did the training data
     processed_sample = scaler.transform(np.array(single_sample).reshape(1, -1))
     # Get predicted probabilities from the model
-    predicted_probabilities = lr.predict_proba(processed_sample)
+    predicted_probabilities = rf.predict_proba(processed_sample)
     # Ensure probabilities for all classes by manually zero-filling for missing classes
     all_probs = np.zeros(len(unique_classes))
-    all_probs[lr.classes_] = predicted_probabilities
+    all_probs[rf.classes_] = predicted_probabilities
     return all_probs
