bd032dabca0b0a7285b26d35bfd2f60a85069d95
philipmit <pschro@mit.edu>
2023-11-13 22:18:12 -0500

    Output 5 (phase 3): Response to improving AUC of 0.7124322971034913
    AUC from previous code:
    0.7124322971034913
    
    Response from GPT:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.tree import DecisionTreeClassifier
    from sklearn.preprocessing import StandardScaler
    
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # All rows excluding the first and last columns
    X = ecoli.iloc[:, 1:-1]
    
    # All rows, only the last column
    y = ecoli.iloc[:, -1]
    
    encoder = LabelEncoder()
    y = encoder.fit_transform(y)
    
    # split dataset into training and testing set
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # standardize features
    std_scaler = StandardScaler()
    
    X_train = std_scaler.fit_transform(X_train)
    X_test = std_scaler.transform(X_test)
    
    # Implement DecisionTreeClassifier for better performance
    classifier = DecisionTreeClassifier(max_depth=6)
    classifier.fit(X_train, y_train)
    
    def predict_label(sample):
        sample = np.array(sample).reshape(1, -1)
        sample = std_scaler.transform(sample)
        probabilities = classifier.predict_proba(sample)
        return probabilities[0]
    ```
    In the provided code above, I have replaced the RandomForest with a DecisionTreeClassifier and set the max_depth to 6, which defines the maximum depth of the tree. The features are scaled using the StandardScaler() which standardizes features by removing the mean and scaling to unit variance. This, in many cases, can improve performance. Implementation of these changes may result in improved AUC on the validation data.

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test02.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test02.py
@@ -1,31 +1,27 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
-# Load the Ecoli dataset
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-X = ecoli.iloc[:, 1:-1]  # All rows excluding the first and last columns
-y = ecoli.iloc[:, -1]    # All rows, only the last column
-# Replace strings in y with numbers
-classes = list(np.unique(y))
-y = y.replace(classes, list(range(len(classes))))
+# All rows excluding the first and last columns
+X = ecoli.iloc[:, 1:-1]
+# All rows, only the last column
+y = ecoli.iloc[:, -1]
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+# split dataset into training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Feature scaling
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-# Create Random Forest object
-rfc = RandomForestClassifier(n_estimators=500)
-rfc.fit(X_train, y_train)
+# standardize features
+std_scaler = StandardScaler()
+X_train = std_scaler.fit_transform(X_train)
+X_test = std_scaler.transform(X_test)
+# Implement DecisionTreeClassifier for better performance
+classifier = DecisionTreeClassifier(max_depth=6)
+classifier.fit(X_train, y_train)
 def predict_label(sample):
     sample = np.array(sample).reshape(1, -1) 
-    sample = scaler.transform(sample)  
-    probabilities = rfc.predict_proba(sample)
-    #turn the probabilities into a list of length 8 (number of classes)
-    #filling missing values with 0
-    result = [0]*8
-    for index, probability in enumerate(probabilities[0]):
-        result[index] = probability
-    return result
+    sample = std_scaler.transform(sample)  
+    probabilities = classifier.predict_proba(sample)
+    return probabilities[0]
