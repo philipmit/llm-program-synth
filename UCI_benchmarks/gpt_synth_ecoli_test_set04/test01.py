15dc735b707bb29f645d04ae65c5f8a5f628dadc
philipmit <pschro@mit.edu>
2023-11-13 19:47:44 -0500

    Output 6 (phase 3): Response to improving AUC of 0.855735048560919
    AUC from previous code:
    0.855735048560919
    
    Response from GPT:
    #####################################
    Response 2 from GPT:
    
    Various ways of improving the model's performance can be tried. These can be from adjusting the regularization parameter (C), trying a different solver, using a model with a more sophisticated mechanism such as Random forest, Gradient Boosting, etc., optimizing hyperparameters with GridSearchCV or RandomizedSearchCV, or even gathering more data if possible.
    This is how the Logistic Regression model could be improved:
    ```
    import pandas as pd
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import GridSearchCV
    from sklearn.pipeline import Pipeline
    
    # Load the Ecoli data
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    # Create feature matrix (X) and target array (Y)
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # List to keep track of unique classes
    unique_classes = list(np.unique(y))
    # Convert string labels in y to numeric
    y = y.replace(unique_classes, list(range(len(unique_classes))))
    
    # Convert pandas objects to numpy arrays
    X, y = X.to_numpy(), y.to_numpy()
    
    # Split data into train and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Defining logistic regression model with a pipeline for scaling
    model = Pipeline([
        ('scaler', StandardScaler()),
        ('lr', LogisticRegression(solver='liblinear'))
    ])
    
    # Setting up a grid of parameters to optimize
    params = {
        "lr__C":np.logspace(-3,3,7),
        "lr__penalty":["l1", "l2"]
    }
    
    # Performing Grid Search to find the best parameters
    grid_search = GridSearchCV(estimator=model, param_grid=params, cv=5)
    grid_search.fit(X_train, y_train)
    
    def predict_label(input_data):
        """
        Function to predict the label for given input data using the optimized logistic regression model.
        input_data should be a numpy array.
        """
        prediction_probs = [0 for _ in range(len(unique_classes))]
        input_data_features = input_data.reshape(1, -1)
        predicted_probabilities = grid_search.predict_proba(input_data_features)[0]
        trained_classes = grid_search.best_estimator_.named_steps['lr'].classes_
        for i in range(len(trained_classes)):
            prediction_probs[trained_classes[i]] = predicted_probabilities[i]
        return prediction_probs
    ```
    The code now includes a grid search approach to find the optimal parameters for the logistic regression model. Note that GridSearchCV might increase the computational time depending on the size of the parameter grid and the dataset.

--- UCI_benchmarks/gpt_synth_ecoli_test_set04/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set04/test01.py
@@ -1,40 +1,46 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-# Load the Ecoli dataset
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+# Load the Ecoli data
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
-# Create feature matrix and target array
-X = ecoli.iloc[:, 1:-1]  
+# Create feature matrix (X) and target array (Y)
+X = ecoli.iloc[:, 1:-1]
 y = ecoli.iloc[:, -1]
 # List to keep track of unique classes
 unique_classes = list(np.unique(y))
 # Convert string labels in y to numeric
 y = y.replace(unique_classes, list(range(len(unique_classes))))
 # Convert pandas objects to numpy arrays
-X = X.to_numpy()
-y = y.to_numpy()
-# Split the dataset into training and testing sets
+X, y = X.to_numpy(), y.to_numpy()
+# Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Train the logistic regression model with new parameters
-lr = make_pipeline(StandardScaler(), LogisticRegression(C=0.9, penalty='l1', solver='liblinear', max_iter=5000))
-lr.fit(X_train, y_train)
+# Defining logistic regression model with a pipeline for scaling
+model = Pipeline([
+    ('scaler', StandardScaler()),
+    ('lr', LogisticRegression(solver='liblinear'))
+])
+# Setting up a grid of parameters to optimize
+params = {
+    "lr__C":np.logspace(-3,3,7),
+    "lr__penalty":["l1", "l2"]
+}
+# Performing Grid Search to find the best parameters
+grid_search = GridSearchCV(estimator=model, param_grid=params, cv=5)
+grid_search.fit(X_train, y_train)
 def predict_label(input_data):
     """
-    Function to predict the label for given input data using trained logistic regression model.
+    Function to predict the label for given input data using the optimized logistic regression model.
     input_data should be a numpy array.
     """
-    # Initialize a prediction list of 0's with a length equal to the total number of classes
     prediction_probs = [0 for _ in range(len(unique_classes))]
-    # Reshape the input data for prediction
     input_data_features = input_data.reshape(1, -1)
-    # Perform prediction
-    predicted_probabilities = lr.predict_proba(input_data_features)[0]
-    # Get classes that the model was trained on
-    trained_classes = lr.classes_
+    predicted_probabilities = grid_search.predict_proba(input_data_features)[0]
+    trained_classes = grid_search.best_estimator_.named_steps['lr'].classes_
     for i in range(len(trained_classes)):
         prediction_probs[trained_classes[i]] = predicted_probabilities[i]
     return prediction_probs
