1d57f7bf6f720c191b2a7f1519a9f2a9dde4bf8e
philipmit <pschro@mit.edu>
2023-11-13 22:07:31 -0500

    Output 2 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 93, in <module>
      File "<string>", line 29, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 605, in roc_auc_score
        y_score = check_array(y_score, ensure_2d=False)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 953, in check_array
        raise ValueError(
    <class 'ValueError'>
    Found array with dim 3. None expected <= 2.
    
    Response from GPT:
    ```
    import numpy as np
    import pandas as pd
    from sklearn.model_selection import train_test_split
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    
    # Load the Ecoli dataset
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # Replace strings with numbers in y
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X = X.to_numpy()
    y = y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the data
    scaler = StandardScaler()
    X_train = scaler.fit_transform(X_train)
    
    # Train the logistic regression model
    clf = LogisticRegression(random_state=0, multi_class='ovr')
    clf.fit(X_train, y_train)
    
    # Define a function that predicts label for new data
    def predict_label(sample):
        sample = np.array(sample).reshape(1,-1)
        sample = scaler.transform(sample)  # We need to apply the same scaling to the new data
        probabilities = clf.predict_proba(sample)
        print(probabilities)
        # As we need to provide a list of probabilities, if it's a single class, we reshape to have a 2D list
        probabilities = probabilities.reshape(-1, len(probabilities[0])).tolist()
        # Flatten the list of probabilities as roc_auc_score expects a 2D input but one-dimensional output.
        probabilities = [prob for sublist in probabilities for prob in sublist]
        return probabilities
    ```

--- UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
+++ UCI_benchmarks/gpt_synth_ecoli_test_set08/test01.py
@@ -15,14 +15,19 @@ y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Scale the data
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
 # Train the logistic regression model
 clf = LogisticRegression(random_state=0, multi_class='ovr')
 clf.fit(X_train, y_train)
 # Define a function that predicts label for new data
 def predict_label(sample):
     sample = np.array(sample).reshape(1,-1)
-    sample = sc.transform(sample)  # We need to apply the same scaling to the new data
+    sample = scaler.transform(sample)  # We need to apply the same scaling to the new data
     probabilities = clf.predict_proba(sample)
+    print(probabilities)
+    # As we need to provide a list of probabilities, if it's a single class, we reshape to have a 2D list
+    probabilities = probabilities.reshape(-1, len(probabilities[0])).tolist()
+    # Flatten the list of probabilities as roc_auc_score expects a 2D input but one-dimensional output.
+    probabilities = [prob for sublist in probabilities for prob in sublist]
     return probabilities
