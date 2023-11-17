ac57ce1c1d0a851005a48680e732687c4bf0a0e7
philipmit <pschro@mit.edu>
2023-11-17 15:36:36 -0500

    Initial output: #<PrevData> ######## Load and preview the dataset and datatypes # Import necessary libraries import pandas as pd # Read file df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, sep='\s+') # Preview dataset and datatypes print(df.shape) print(df.head()) print(df.info()) print(df.dtypes) for col in df.applymap(type).columns:     print(col, df.applymap(type)[col].unique()) print(df.isnull().sum()) #</PrevData>

---
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set10/test01.py
@@ -0,0 +1,15 @@
+#<PrevData>
+######## Load and preview the dataset and datatypes
+# Import necessary libraries
+import pandas as pd
+# Read file
+df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, sep='\s+')
+# Preview dataset and datatypes
+print(df.shape)
+print(df.head())
+print(df.info())
+print(df.dtypes)
+for col in df.applymap(type).columns:
+    print(col, df.applymap(type)[col].unique())
+print(df.isnull().sum())
+#</PrevData>
