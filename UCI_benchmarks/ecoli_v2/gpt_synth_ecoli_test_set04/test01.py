9754b7b0d08d5799a5c529c37fb93c25b89c0181
philipmit <pschro@mit.edu>
2023-11-17 15:00:44 -0500

    Initial output: #<PrevData> ######## Load and preview the dataset and datatypes # Import necessary libraries import pandas as pd # File path for your data file_path = '/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data' # Read file df = pd.read_csv(file_path, sep='\s+', header=None) # Preview dataset and datatypes print(df.shape) print(df.head()) print(df.info()) print(df.dtypes) for col in df.applymap(type).columns:     print(col, df.applymap(type)[col].unique()) print(df.isnull().sum()) #</PrevData>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set04/test01.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set04/test01.py
@@ -1,9 +1,11 @@
-#<PrevData>
+<PrevData>
 ######## Load and preview the dataset and datatypes
 # Import necessary libraries
 import pandas as pd
+# File path for your data
+file_path = '/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data'
 # Read file
-df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, delim_whitespace=True)
+df = pd.read_csv(file_path, sep='\s+', header=None)
 # Preview dataset and datatypes
 print(df.shape)
 print(df.head())
