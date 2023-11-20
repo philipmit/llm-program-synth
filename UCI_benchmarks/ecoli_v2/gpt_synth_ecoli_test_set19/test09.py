f42db05a90058978fc6424a056e4e82ccc6508e7
philipmit <pschro@mit.edu>
2023-11-20 13:23:19 -0500

    Validation output 8 (phase 4): Response to ValueError
    Error from previous code:
      File "<stdin>", line 10, in execute_val_code
      File "<string>", line 28, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 693, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, delim_whitespace=True)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    
    # Define features, X, and labels, y
    X = df.iloc[:, 1:-1]  # All rows, all columns except the first and the last one
    y = df.iloc[:, -1]   # All rows, only the last column
    y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
    X=X.to_numpy()
    y=y.to_numpy()
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
    
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    model = LogisticRegression(multi_class='multinomial', max_iter=10000)
    model.fit(X_train, y_train)
    #</Train>
    #<Test>
    ######## Validate the model using the testing data, X_test and y_test
    X_test = sc.transform(X_test)
    y_pred = model.predict(X_test)
    #</Test>
    
    #<Evaluate>
    ######## Evaluate the model using accuracy and confusion matrix
    from sklearn.metrics import accuracy_score, confusion_matrix
    accuracy = accuracy_score(y_test, y_pred)
    cm = confusion_matrix(y_test, y_pred)
    
    print('Accuracy: ', accuracy)
    print('Confusion Matrix: \n', cm)
    #</Evaluate>
    
    #<Predict>
    ######## Define a function that can be used to make new predictions given one raw sample of data
    def predict_label(raw_sample):
        # Standardize the raw_sample to match the data model was trained on
        raw_sample = sc.transform(raw_sample.reshape(1, -1))
        # Predicting the label probabilities as a 1D array
        return model.predict_proba(raw_sample)[0]
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test09.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test09.py
@@ -20,14 +20,17 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+
 # Define features, X, and labels, y
 X = df.iloc[:, 1:-1]  # All rows, all columns except the first and the last one
 y = df.iloc[:, -1]   # All rows, only the last column
 y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
 X=X.to_numpy()
 y=y.to_numpy()
+
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
 # Scale the features 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -62,6 +65,6 @@ print('Confusion Matrix: \n', cm)
 def predict_label(raw_sample):
     # Standardize the raw_sample to match the data model was trained on
     raw_sample = sc.transform(raw_sample.reshape(1, -1))
-    # Predicting the label and return.
-    return model.predict(raw_sample)  
+    # Predicting the label probabilities as a 1D array
+    return model.predict_proba(raw_sample)[0]  
 #</Predict>
