32edab215521c7d728333c6e686ade6992759428
philipmit <pschro@mit.edu>
2023-11-20 13:49:00 -0500

    Validation output 8 (phase 4): Response to ValueError
    Error from previous code:
      File "<stdin>", line 14, in <module>
      File "<string>", line 28, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, sep="\s+")
    df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    
    # Define features, X, and labels, y
    X = df.iloc[:, 1:-1]  # All rows, all columns except the first and the last one, first is an object and cannot be converted to float.
    y = df.iloc[:, -1]   # All rows, only the last column
    # replace strings with numbers in y
    np.unique(y)
    len(list(np.unique(y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X = X.to_numpy()
    y = y.to_numpy()
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', random_state=42, max_iter=200)
    model.fit(X_train, y_train)
    #</Train>
    
    #<Predict>
    ######## Define a function that can be used to make new predictions given one raw sample of data
    def predict_label(raw_sample):
        # Standardize the raw_sample to match the data model was trained on
        raw_sample = sc.transform(raw_sample.reshape(1, -1))
        # Return the class probabilities as a 1D array
        predicted_class = model.predict(raw_sample)[0]
        predicted_probs = model.predict_proba(raw_sample)[0]
        # Creating a zero array of size equal to number of classes
        pred = np.zeros(len(np.unique(y_train)))
        # Assigning the predicted probability to the respective class index
        pred[predicted_class] = predicted_probs[predicted_class]
        return pred
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set20/test01.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set20/test01.py
@@ -4,6 +4,7 @@
 import pandas as pd
 # Read file
 df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, sep="\s+")
+df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 # Preview dataset and datatypes
 print(df.shape)
 print(df.head())
@@ -13,6 +14,7 @@ for col in df.applymap(type).columns:
     print(col, df.applymap(type)[col].unique())
 print(df.isnull().sum())
 #</PrevData>
+
 #<PrepData>
 ######## Prepare the dataset for training
 # Import necessary packages
@@ -24,11 +26,12 @@ from sklearn.model_selection import train_test_split
 # Define features, X, and labels, y
 X = df.iloc[:, 1:-1]  # All rows, all columns except the first and the last one, first is an object and cannot be converted to float.
 y = df.iloc[:, -1]   # All rows, only the last column
-# Label encoding if necessary
-if y.dtype == object:
-    y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
-X=X.to_numpy(dtype=float)
-y=y.to_numpy()
+# replace strings with numbers in y
+np.unique(y)
+len(list(np.unique(y)))
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X = X.to_numpy()
+y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Scale the features 
@@ -39,16 +42,24 @@ print(y_train.shape)
 print(X_train[0:5])
 print(y_train[0:5])
 #</PrepData>
+
 #<Train>
 ######## Train the model using the training data, X_train and y_train
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs', random_state=42)
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', random_state=42, max_iter=200)
 model.fit(X_train, y_train)
 #</Train>
+
 #<Predict>
 ######## Define a function that can be used to make new predictions given one raw sample of data
 def predict_label(raw_sample):
     # Standardize the raw_sample to match the data model was trained on
     raw_sample = sc.transform(raw_sample.reshape(1, -1))
     # Return the class probabilities as a 1D array
-    return model.predict_proba(raw_sample)[0]  
+    predicted_class = model.predict(raw_sample)[0]
+    predicted_probs = model.predict_proba(raw_sample)[0]
+    # Creating a zero array of size equal to number of classes
+    pred = np.zeros(len(np.unique(y_train)))
+    # Assigning the predicted probability to the respective class index
+    pred[predicted_class] = predicted_probs[predicted_class]
+    return pred
 #</Predict>
