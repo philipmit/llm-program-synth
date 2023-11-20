ee7984a0e4cfa25e924b1376db2df3694fb533af
philipmit <pschro@mit.edu>
2023-11-20 12:13:29 -0500

    Output 2 (phase 2): Response for </PrepData>
    Response from GPT:
    #<PrepData>
    ######## Parse and prepare the dataset for training
    # It appears this file contains tab-separated data. Try again with appropriate options.
    col_names = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'Class'] # Adding column names
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, names=col_names)
    # Preview dataset and datatypes after parsing
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    # Define features, X, and labels, y
    X = df.iloc[:, 1:-1]  # All rows, all columns except the sequence name and the last one
    y = df.iloc[:, -1]   # All rows, only the last column
    y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
    X=X.to_numpy()
    y=y.to_numpy()
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set18/test04.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set18/test04.py
@@ -13,3 +13,39 @@ for col in df.applymap(type).columns:
     print(col, df.applymap(type)[col].unique())
 print(df.isnull().sum())
 #</PrevData>
+#<PrepData>
+######## Parse and prepare the dataset for training
+# It appears this file contains tab-separated data. Try again with appropriate options.
+col_names = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'Class'] # Adding column names
+df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, names=col_names)
+# Preview dataset and datatypes after parsing
+print(df.shape)
+print(df.head())
+print(df.info())
+print(df.dtypes)
+for col in df.applymap(type).columns:
+    print(col, df.applymap(type)[col].unique())
+print(df.isnull().sum())
+
+######## Prepare the dataset for training
+# Import necessary packages
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+# Define features, X, and labels, y
+X = df.iloc[:, 1:-1]  # All rows, all columns except the sequence name and the last one
+y = df.iloc[:, -1]   # All rows, only the last column
+y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
+X=X.to_numpy()
+y=y.to_numpy()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+# Scale the features 
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+print(X_train.shape)
+print(y_train.shape)
+print(X_train[0:5])
+print(y_train[0:5])
+#</PrepData>
