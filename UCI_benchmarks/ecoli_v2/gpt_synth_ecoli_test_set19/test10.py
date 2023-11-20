9d4936ea6225ff9d19e1eed15e5c478249e6f061
philipmit <pschro@mit.edu>
2023-11-20 13:28:33 -0500

    Output 2 (phase 2): Response for </PrepData>
    Response from GPT:
    #<PrepData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    
    # Split data into separate rows
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, sep='\s+', names=["name","mcg","gvh","lip","chg","aac","alm1","alm2","class"])
    
    # Remove name column
    df.drop(['name'], axis=1, inplace=True)
    
    # Define features, X, and labels, y
    X = df.iloc[:, :-1].values  # All rows, all columns except the last one
    y = df.iloc[:, -1].values   # All rows, only the last column
    
    # Encoding - convert class labels to numbers
    from sklearn.preprocessing import LabelEncoder
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test10.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test10.py
@@ -13,3 +13,38 @@ for col in df.applymap(type).columns:
     print(col, df.applymap(type)[col].unique())
 print(df.isnull().sum())
 #</PrevData>
+#<PrepData>
+######## Load and preview the dataset and datatypes
+# Import necessary libraries
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Split data into separate rows
+df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, sep='\s+', names=["name","mcg","gvh","lip","chg","aac","alm1","alm2","class"])
+
+# Remove name column
+df.drop(['name'], axis=1, inplace=True)
+
+# Define features, X, and labels, y
+X = df.iloc[:, :-1].values  # All rows, all columns except the last one
+y = df.iloc[:, -1].values   # All rows, only the last column
+
+# Encoding - convert class labels to numbers
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+
+# Scale the features 
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_train[0:5])
+print(y_train[0:5])
+#</PrepData>
