de8cb7a87423a219cc8e5210ba0e057600817e33
philipmit <pschro@mit.edu>
2023-11-20 14:01:20 -0500

    Validation output 9 (phase 4): Response to ValueError
    Error from previous code:
      File "<stdin>", line 14, in <module>
      File "<string>", line 28, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 737, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Number of classes in y_true not equal to the number of columns in 'y_score'
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, delim_whitespace=True)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    from sklearn import preprocessing
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    from keras.utils import to_categorical
    
    # Define features, X, and labels, y
    X = df.iloc[:, 1:-1]  # All rows, all columns except the first and last one
    y = df.iloc[:, -1]   # All rows, only the last column
    # To make sure that the labels y are integers, map the unique label values to integers
    le = preprocessing.LabelEncoder()
    y = le.fit_transform(y)
    mapping = dict(zip(le.classes_, le.transform(le.classes_)))
    
    X = X.to_numpy()
    y = to_categorical(y) #one-hot encoding of y labels for multi-class classification
    
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    model = LogisticRegression()
    for i in range(y_train.shape[1]): #loop over each class and train a model for each class
        model.fit(X_train, y_train[:,i])
    #</Train>
    
    #<Predict>
    ######## Define a function that can be used to make new predictions given one raw sample of data
    def predict_label(raw_sample):
        # Standardize the raw_sample to match the data model was trained on
        raw_sample = sc.transform(raw_sample.reshape(1, -1))
        # Return the class probabilities as a 1D array
        predictions = []
        for i in range(y_train.shape[1]): #loop over each class and get prediction for each class
            predictions.append(model.predict_proba(raw_sample)[0][1])
        return np.array(predictions)
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set20/test02.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set20/test02.py
@@ -16,23 +16,24 @@ print(df.isnull().sum())
 
 #<PrepData>
 ######## Prepare the dataset for training
-# Define features, X, and labels, y
-X = df.iloc[:, 1:-1]  # All rows, all columns except the first and last one
-y = df.iloc[:, -1]   # All rows, only the last column
-# To make sure that the labels y are integers, map the unique label values to integers
-mapping = {label: idx for idx, label in enumerate(y.unique())}
-y = y.replace(mapping)
-
-X = X.astype(float)
-
 # Import necessary packages
+from sklearn import preprocessing
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
-X=X.to_numpy()
-y=y.to_numpy()
+# Define features, X, and labels, y
+X = df.iloc[:, 1:-1]  # All rows, all columns except the first and last one
+y = df.iloc[:, -1]   # All rows, only the last column
+# To make sure that the labels y are integers, map the unique label values to integers
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(y)
+mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+
+X = X.to_numpy()
+y = to_categorical(y) #one-hot encoding of y labels for multi-class classification
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
@@ -49,19 +50,9 @@ print(y_train[0:5])
 #<Train>
 ######## Train the model using the training data, X_train and y_train
 model = LogisticRegression()
-model.fit(X_train, y_train)
+for i in range(y_train.shape[1]): #loop over each class and train a model for each class
+    model.fit(X_train, y_train[:,i])
 #</Train>
-
-#<Eval>
-######## Evaluate the model using the test data, X_test and y_test
-X_test = sc.transform(X_test)  # Scale the test dataset
-y_pred = model.predict(X_test)  # Make predictions using the model
-# Import the necessary library
-from sklearn.metrics import accuracy_score
-# Calculate and print the accuracy score
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Model accuracy: {accuracy}')
-#</Eval>
 
 #<Predict>
 ######## Define a function that can be used to make new predictions given one raw sample of data
@@ -69,5 +60,8 @@ def predict_label(raw_sample):
     # Standardize the raw_sample to match the data model was trained on
     raw_sample = sc.transform(raw_sample.reshape(1, -1))
     # Return the class probabilities as a 1D array
-    return model.predict_proba(raw_sample)[0]  
+    predictions = []
+    for i in range(y_train.shape[1]): #loop over each class and get prediction for each class
+        predictions.append(model.predict_proba(raw_sample)[0][1])
+    return np.array(predictions)  
 #</Predict>
