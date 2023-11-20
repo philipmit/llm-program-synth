6775c696c027ce2ea923f1b4f3cb04444bfeb2ac
philipmit <pschro@mit.edu>
2023-11-20 12:43:21 -0500

    Initial output: #<PrevData> # Load and preview the dataset and datatypes # Import necessary libraries import pandas as pd # Read file df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None) df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class'] # Preview dataset and datatypes print(df.shape) print(df.head()) print(df.info()) print(df.dtypes) for col in df.applymap(type).columns:     print(col, df.applymap(type)[col].unique()) print(df.isnull().sum()) #</PrevData>

---
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test02.py
@@ -0,0 +1,16 @@
+#<PrevData>
+# Load and preview the dataset and datatypes
+# Import necessary libraries
+import pandas as pd
+# Read file
+df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
+df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+# Preview dataset and datatypes
+print(df.shape)
+print(df.head())
+print(df.info())
+print(df.dtypes)
+for col in df.applymap(type).columns:
+    print(col, df.applymap(type)[col].unique())
+print(df.isnull().sum())
+#</PrevData>
