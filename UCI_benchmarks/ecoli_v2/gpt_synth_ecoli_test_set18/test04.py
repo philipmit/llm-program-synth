3910a28a4f23876edcefb1d10aac16ad60265cba
philipmit <pschro@mit.edu>
2023-11-20 12:23:43 -0500

    Validation output 14 (phase 4): Response to KeyError
    Error from previous code:
      File "<stdin>", line 7, in execute_val_code
      File "<string>", line 13, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/pandas/core/frame.py", line 3761, in __getitem__
        indexer = self.columns.get_loc(key)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/pandas/core/indexes/base.py", line 3655, in get_loc
        raise KeyError(key) from err
    <class 'KeyError'>
    '                                                     0\n0    AAT_ECOLI   0.49  0.29  0.48  0.50  0.56  0.24...\n1    ACEA_ECOLI  0.07  0.40  0.48  0.50  0.54  0.35...\n2    ACEK_ECOLI  0.56  0.40  0.48  0.50  0.49  0.37...\n3    ACKA_ECOLI  0.59  0.49  0.48  0.50  0.52  0.45...\n4    ADI_ECOLI   0.23  0.32  0.48  0.50  0.55  0.25...\n..                                                 ...\n331  TREA_ECOLI  0.74  0.56  0.48  0.50  0.47  0.68...\n332  UGPB_ECOLI  0.71  0.57  0.48  0.50  0.48  0.35...\n333  USHA_ECOLI  0.61  0.60  0.48  0.50  0.44  0.39...\n334  XYLF_ECOLI  0.59  0.61  0.48  0.50  0.42  0.42...\n335  YTFQ_ECOLI  0.74  0.74  0.48  0.50  0.31  0.53...\n\n[336 rows x 1 columns]'
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file and adding column names
    col_names = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'Class']
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, names=col_names)
    # Print dataset shape for debugging
    print("Shape of the dataset: ", df.shape)
    # Preview dataset and datatypes
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.columns:
        print(col, type(df[col].loc[0]))
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    # Define features, X, and labels, y
    X = df.iloc[:, 1:-1]  # All rows, all columns except the Sequence Name and the last one
    y = df.iloc[:, -1]   # All rows, only the last column
    y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
    X = X.to_numpy()
    y = y.to_numpy()
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    model = LogisticRegression(max_iter=1000, multi_class='ovr') # Setting multi_class to ovr
    model.fit(X_train, y_train)
    #</Train>
    
    #<Predict>
    ######## When using this model in a separate script, make sure to define the `StandardScaler` object `sc`
    def predict_label(raw_sample, model=model, scaler=sc, np=np):
      # Standardize the raw_sample with scaler to match the data the model was trained on
      raw_sample = scaler.transform(np.array(raw_sample).reshape(1, -1))
      # Extend the predictions to match the number of classes in the data
      class_probs = model.predict_proba(raw_sample).flatten()
      extended_probs = list(class_probs) + [0] * (8 - len(class_probs))
      return extended_probs
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set18/test04.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set18/test04.py
@@ -2,22 +2,22 @@
 ######## Load and preview the dataset and datatypes
 # Import necessary libraries
 import pandas as pd
-# Read file
-df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None)
+# Read file and adding column names
+col_names = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'Class']
+df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, names=col_names)
+# Print dataset shape for debugging
+print("Shape of the dataset: ", df.shape)
 # Preview dataset and datatypes
-print(df.shape)
 print(df.head())
 print(df.info())
 print(df.dtypes)
-for col in df.applymap(type).apply(pd.value_counts).idxmax(axis=0):
+for col in df.columns:
     print(col, type(df[col].loc[0]))
 print(df.isnull().sum())
 #</PrevData>
 
 #<PrepData>
 ######## Prepare the dataset for training
-col_names = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'Class'] # Adding column names
-df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, names=col_names)
 # Import necessary packages
 import numpy as np
 from sklearn.linear_model import LogisticRegression
@@ -27,8 +27,8 @@ from sklearn.model_selection import train_test_split
 X = df.iloc[:, 1:-1]  # All rows, all columns except the Sequence Name and the last one
 y = df.iloc[:, -1]   # All rows, only the last column
 y = y.replace(list(np.unique(y)), list(range(len(np.unique(y)))))
-X=X.to_numpy()
-y=y.to_numpy()
+X = X.to_numpy()
+y = y.to_numpy()
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 # Scale the features 
@@ -53,6 +53,6 @@ def predict_label(raw_sample, model=model, scaler=sc, np=np):
   raw_sample = scaler.transform(np.array(raw_sample).reshape(1, -1))
   # Extend the predictions to match the number of classes in the data
   class_probs = model.predict_proba(raw_sample).flatten()
-  extended_probs = list(class_probs) + [0] * (8 - len(class_probs))
-  return extended_probs
+  extended_probs = list(class_probs) + [0] * (8 - len(class_probs)) 
+  return extended_probs  
 #</Predict>
