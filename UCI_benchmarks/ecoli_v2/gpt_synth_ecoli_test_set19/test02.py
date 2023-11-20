bb5be8e5d87b19356531bf8301c394ac231b9ab5
philipmit <pschro@mit.edu>
2023-11-20 12:45:25 -0500

    Validation output 6 (phase 4): Response to NameError
    Error from previous code:
      File "<stdin>", line 8, in execute_val_code
      File "<string>", line 17, in <module>
      File "<string>", line 50, in predict_label
    <class 'NameError'>
    name 'sc' is not defined
    
    Response from GPT:
    #<PrevData>
    # Load and preview the dataset and datatypes
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    # Prepare the dataset for training
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler
    from sklearn.model_selection import train_test_split
    # Define features, X, and labels, y
    X = df.iloc[:, 1:-1]  # All rows, all columns except the last one and the first one
    y = df.iloc[:, -1]   # All rows, only the last column
    # replacing string labels with ints
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    # Split the dataset into training and testing sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    # Train the model using the training data, X_train and y_train
    model = LogisticRegression()
    model.fit(X_train, y_train)
    #</Train>
    
    #<Predict>
    # Define a function that can be used to make new predictions given one raw sample of data
    class Predictor:
        def __init__(self, model, scaler):
            self.model = model
            self.scaler = scaler
    
        def predict_label(self, raw_sample):
            # Standardize the raw_sample to match the data model was trained on
            raw_sample = self.scaler.transform(raw_sample.reshape(1, -1))
            # Return the class probabilities as a 1D array
            return self.model.predict_proba(raw_sample)[0]
    pred = Predictor(model, sc)
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test02.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set19/test02.py
@@ -1,6 +1,5 @@
 #<PrevData>
 # Load and preview the dataset and datatypes
-# Import necessary libraries
 import pandas as pd
 # Read file
 df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
@@ -14,9 +13,9 @@ for col in df.applymap(type).columns:
     print(col, df.applymap(type)[col].unique())
 print(df.isnull().sum())
 #</PrevData>
+
 #<PrepData>
 # Prepare the dataset for training
-# Import necessary packages
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
@@ -38,16 +37,24 @@ print(y_train.shape)
 print(X_train[0:5])
 print(y_train[0:5])
 #</PrepData>
+
 #<Train>
 # Train the model using the training data, X_train and y_train
 model = LogisticRegression()
 model.fit(X_train, y_train)
 #</Train>
+
 #<Predict>
 # Define a function that can be used to make new predictions given one raw sample of data
-def predict_label(raw_sample):
-    # Standardize the raw_sample to match the data model was trained on
-    raw_sample = sc.transform(raw_sample.reshape(1, -1))
-    # Return the class probabilities as a 1D array
-    return model.predict_proba(raw_sample)[0]  
+class Predictor:
+    def __init__(self, model, scaler):
+        self.model = model
+        self.scaler = scaler
+
+    def predict_label(self, raw_sample):
+        # Standardize the raw_sample to match the data model was trained on
+        raw_sample = self.scaler.transform(raw_sample.reshape(1, -1))
+        # Return the class probabilities as a 1D array
+        return self.model.predict_proba(raw_sample)[0]
+pred = Predictor(model, sc)
 #</Predict>
