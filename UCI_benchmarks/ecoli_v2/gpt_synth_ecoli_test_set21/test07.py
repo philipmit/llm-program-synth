6461b5244fbb6733a9ca5d392cff2af80a587d2d
philipmit <pschro@mit.edu>
2023-11-20 14:48:04 -0500

    Validation output 4 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 14, in <module>
      File "<string>", line 28, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 605, in roc_auc_score
        y_score = check_array(y_score, ensure_2d=False)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 953, in check_array
        raise ValueError(
    <class 'ValueError'>
    Found array with dim 3. None expected <= 2.
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.metrics import roc_auc_score
    from sklearn.preprocessing import StandardScaler
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    # replace strings with numbers in y
    np.unique( y)
    len(list(np.unique( y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    X_test = sc.transform(X_test)  # apply same transformation to test data
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    from sklearn.linear_model import LogisticRegression
    
    # Create an instance of LogisticRegression
    logreg = LogisticRegression(multi_class='ovr', solver='liblinear')
    
    # Fit the model with data
    logreg.fit(X_train, y_train)
    #</Train>
    #<Predict>
    ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
    def predict_label(sample):
        proba = logreg.predict_proba([sample])  # Get the probability of each class
        return proba[0]  # Return the inner list only
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test07.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test07.py
@@ -35,24 +35,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 # Scale the features 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)  # apply same transformation to test data
 print(X_train.shape)
 print(y_train.shape)
 print(X_train[0:5])
 print(y_train[0:5])
 #</PrepData>
-
-#<Train>
-######## Train the model using the training data, X_train and y_train
-### Start your code
-### End your code
-#</Train>
-
-#<Predict>
-######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
-### Start your code
-### End your code
-#</Predict>
-
 
 #<Train>
 from sklearn.linear_model import LogisticRegression
@@ -66,8 +54,6 @@ logreg.fit(X_train, y_train)
 #<Predict>
 ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
 def predict_label(sample):
-    sample = sc.transform([sample])  # Scale the sample
-    proba = logreg.predict_proba(sample)  # Get the probability of each class
-    return proba
-### End your code
+    proba = logreg.predict_proba([sample])  # Get the probability of each class
+    return proba[0]  # Return the inner list only
 #</Predict>
