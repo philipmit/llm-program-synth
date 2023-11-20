4b043e9537b373405d0a0600ed2d95feeaed33d7
philipmit <pschro@mit.edu>
2023-11-20 14:06:01 -0500

    Validation output 16 (phase 4): Response to AttributeError
    Error from previous code:
      File "<stdin>", line 13, in <module>
      File "<string>", line 25, in <module>
    <class 'AttributeError'>
    'numpy.ndarray' object has no attribute 'to_numpy'
    
    Response from GPT:
    #<PrevData>
    # Load and preview the dataset and datatypes
    import pandas as pd
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None, delim_whitespace=True)
    df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    # Prepare the dataset for training
    import numpy as np
    from sklearn.linear_model import LogisticRegression
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    from sklearn.model_selection import train_test_split
    
    X = df.iloc[:, 1:-1].values  # All rows, all columns except the first and last one
    y = df.iloc[:, -1].values   # All rows, only the last column
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    # Splitting the dataset into the Training set and Test set
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Feature Scaling
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    #</PrepData>
    
    #<Train>
    # Train the model using the training data
    from sklearn.multiclass import OneVsRestClassifier
    
    # Create an instance of Logistic Regression Classifier and fit the data.
    lr = OneVsRestClassifier(LogisticRegression(solver='lbfgs'))
    lr.fit(X_train, y_train)
    #</Train>
    
    #<Predict>
    # Define a function that can be used to make new predictions given one raw sample of data
    def predict_label(raw_sample):
        if len(raw_sample.shape) == 1:
            raw_sample = raw_sample.reshape(1, -1)
        raw_sample = sc.transform(raw_sample)
        prediction = lr.predict_proba(raw_sample)[0]
        return prediction
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set20/test02.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set20/test02.py
@@ -19,17 +19,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 
-X = df.iloc[:, 1:-1]  # All rows, all columns except the first and last one
-y = df.iloc[:, -1]   # All rows, only the last column
+X = df.iloc[:, 1:-1].values  # All rows, all columns except the first and last one
+y = df.iloc[:, -1].values   # All rows, only the last column
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-X=X.to_numpy()
-y=y.to_numpy()
-
+# Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
-# Scale the features
+# Feature Scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 #</PrepData>
@@ -46,9 +44,9 @@ lr.fit(X_train, y_train)
 #<Predict>
 # Define a function that can be used to make new predictions given one raw sample of data
 def predict_label(raw_sample):
-    if raw_sample.ndim == 1:
+    if len(raw_sample.shape) == 1:
         raw_sample = raw_sample.reshape(1, -1)
     raw_sample = sc.transform(raw_sample)
-    prediction = lr.predict_proba(raw_sample)[0] # Get the first item from the predictions list
+    prediction = lr.predict_proba(raw_sample)[0] 
     return prediction
 #</Predict>
