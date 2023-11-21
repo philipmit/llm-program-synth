c2cb6116a0ce3abd3a61b383dfe5e2b0fc3391c2
Philip Herman Schroeder <pschro@sls-510-0.csail.mit.edu>
2023-11-21 16:25:15 -0500

    Output 2 (phase 1): Response to ImportError
    Error from previous code:
      File "<stdin>", line 12, in execute_code
      File "<string>", line 3, in <module>
    <class 'ImportError'>
    cannot import name 'fetch_uci_ml' from 'sklearn.datasets' (/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/datasets/__init__.py)
    
    Response from GPT:
    #<PrevData>
    # Import necessary libraries
    import pandas as pd
    from sklearn import preprocessing
    from sklearn.model_selection import train_test_split
    
    # Load ecoli dataset
    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/ecoli/ecoli.data', delim_whitespace=True,
                     header=None, names=['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class'])
    
    # Drop 'Sequence Name' as it is not a feature for model training
    df = df.drop(columns = ['Sequence Name'])
    
    # Encode the categorical labels in 'classification' to numerical values
    le = preprocessing.LabelEncoder()
    df['class'] = le.fit_transform(df['class'])
    
    # Prepare X (features) and y (target)
    X = df.drop(columns = ['class'])
    y = df['class']
    
    # Split the dataset into training and test sets
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
    
    # Preview dataset and datatypes
    print(X.head())
    print(y.head())
    #</PrevData>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set22/test01.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set22/test01.py
@@ -1,15 +1,28 @@
 #<PrevData>
 # Import necessary libraries
-from sklearn.datasets import fetch_uci_ml
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
-# Fetch ecoli dataset
-dataset = fetch_uci_ml('ecoli')
-data = dataset.data
-target = dataset.target
+# Load ecoli dataset
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/ecoli/ecoli.data', delim_whitespace=True, 
+                 header=None, names=['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class'])
 
-# Check the dataset
-print(f'Data shape: {data.shape}')
-print(f'Target shape: {target.shape}')
-print(f'First 5 data instances: {data[:5]}')
-print(f'First 5 targets: {target[:5]}')
+# Drop 'Sequence Name' as it is not a feature for model training
+df = df.drop(columns = ['Sequence Name'])
+
+# Encode the categorical labels in 'classification' to numerical values
+le = preprocessing.LabelEncoder()
+df['class'] = le.fit_transform(df['class'])
+
+# Prepare X (features) and y (target)
+X = df.drop(columns = ['class'])
+y = df['class']
+
+# Split the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+# Preview dataset and datatypes
+print(X.head())
+print(y.head())
 #</PrevData>
