46a5dab715a9a6292b8e7a3e2d5d1999127a1943
Philip Herman Schroeder <pschro@sls-510-0.csail.mit.edu>
2023-11-21 16:26:08 -0500

    Output 4 (phase 2): Response to NameError
    Error from previous code:
      File "<stdin>", line 12, in execute_code
      File "<string>", line 6, in <module>
    <class 'NameError'>
    name 'X_train' is not defined
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('ecoli.data', header=None, delim_whitespace=True)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.metrics import roc_auc_score
    from sklearn.preprocessing import StandardScaler
    ecoli = pd.read_csv('ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    # replace strings with numbers in y
    np.unique(y)
    len(list(np.unique(y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    X=X.to_numpy()
    y=y.to_numpy()
    # 50% training and 50% testing
    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.5, random_state=42)
    X_train=X_train.tolist()
    X_test=X_test.tolist()
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set22/test01.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set22/test01.py
@@ -1,33 +1,45 @@
-#<Train>
-######## Train the model on the training dataset using sklearn's logistic regression model
-from sklearn.linear_model import LogisticRegression
-from sklearn.multiclass import OneVsRestClassifier
+#<PrevData>
+######## Load and preview the dataset and datatypes
+# Import necessary libraries
+import pandas as pd
+# Read file
+df = pd.read_csv('ecoli.data', header=None, delim_whitespace=True)
+# Preview dataset and datatypes
+print(df.shape)
+print(df.head())
+print(df.info())
+print(df.dtypes)
+for col in df.applymap(type).columns:
+    print(col, df.applymap(type)[col].unique())
+print(df.isnull().sum())
+#</PrevData>
 
-model = OneVsRestClassifier(LogisticRegression(max_iter=1000)) # creating Logistic Regression model instance
-
-model.fit(X_train, y_train) # training the model
-#</Train>
-
-#<CheckModel>
-# Check the accuracy of the model
-from sklearn.metrics import accuracy_score
-
-y_pred_train = model.predict(X_train)
-accuracy_train = accuracy_score(y_train, y_pred_train)
-print(f'Training accuracy: {accuracy_train:.2f}')
-
-y_pred_test = model.predict(X_test)
-accuracy_test = accuracy_score(y_test, y_pred_test)
-print(f'Test accuracy: {accuracy_test:.2f}')
-#</CheckModel>
-
-#<Predict>
-# Define the predict_label function
-def predict_label(sample):
-    probabilities = model.predict_proba([sample])
-    return probabilities[0]
-
-# Make a prediction for the first sample in the test set
-first_sample_prob = predict_label(X_test.values[0])
-print(f'First sample probabilities: {first_sample_prob}')
-#</Predict></PrepData>
+#<PrepData>
+######## Prepare the dataset for training
+# Import necessary packages
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+ecoli = pd.read_csv('ecoli.data', delim_whitespace=True, header=None)
+ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+X = ecoli.iloc[:, 1:-1]
+y = ecoli.iloc[:, -1]
+# replace strings with numbers in y
+np.unique(y)
+len(list(np.unique(y)))
+y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+X=X.to_numpy()
+y=y.to_numpy()
+# 50% training and 50% testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.5, random_state=42)
+X_train=X_train.tolist()
+X_test=X_test.tolist()
+# Scale the features
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+print(X_train.shape)
+print(y_train.shape)
+print(X_train[0:5])
+print(y_train[0:5])
+#</PrepData>
