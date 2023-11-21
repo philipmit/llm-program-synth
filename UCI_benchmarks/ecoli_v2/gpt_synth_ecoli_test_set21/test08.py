d29dbebc4f5eb32180b0fa609df4fc681cf6c27c
Philip Herman Schroeder <pschro@sls-510-0.csail.mit.edu>
2023-11-21 11:52:08 -0500

    Validation output 9 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 14, in <module>
      File "<string>", line 28, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_param_validation.py", line 211, in wrapper
        return func(*args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 620, in roc_auc_score
        return _multiclass_roc_auc_score(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/metrics/_ranking.py", line 693, in _multiclass_roc_auc_score
        raise ValueError(
    <class 'ValueError'>
    Target scores need to be probabilities for multiclass roc_auc, i.e. they should sum up to 1.0 over classes
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.metrics import roc_auc_score
    from sklearn.preprocessing import StandardScaler
    from sklearn.preprocessing import LabelEncoder
    from sklearn.compose import make_column_transformer
    
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    y = LabelEncoder().fit_transform(y)
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    X_test = sc.transform(X_test) # save scaled features for later use in the predict_label function
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    # Import necessary modules
    from sklearn.linear_model import LogisticRegression
    
    ### Start your code
    # Create a logistic regression model
    clf = LogisticRegression(random_state=0, max_iter=200, multi_class='multinomial')
    
    # Train the logistic regression model
    clf.fit(X_train, y_train)
    ### End your code
    #</Train>
    
    #<Predict>
    ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
    
    ### Start your code
    def predict_label(sample):
        probabilities = clf.predict_proba(sample.reshape(1, -1)) # Predict the probabilistic outputs
        return probabilities[0]  # Return the first (and only) prediction
    ### End your code
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test08.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test08.py
@@ -21,18 +21,15 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import make_column_transformer
+
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]  
 y = ecoli.iloc[:, -1]  
-# replace strings with numbers in y
-le = LabelEncoder()
-y = le.fit_transform(y)
-encoder = OneHotEncoder()
-y = encoder.fit_transform(y.reshape(-1,1)).toarray()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Scale the features 
+y = LabelEncoder().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, stratify=y)
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test) # save scaled features for later use in the predict_label function
@@ -45,11 +42,11 @@ print(y_train[0:5])
 #<Train>
 ######## Train the model using the training data, X_train and y_train
 # Import necessary modules
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
 
 ### Start your code
 # Create a logistic regression model
-clf = OneVsRestClassifier(LogisticRegression(random_state=0, max_iter=200))
+clf = LogisticRegression(random_state=0, max_iter=200, multi_class='multinomial')
 
 # Train the logistic regression model
 clf.fit(X_train, y_train)
@@ -61,8 +58,7 @@ clf.fit(X_train, y_train)
 
 ### Start your code
 def predict_label(sample):
-    sample = sample.reshape(1, -1)  # Reshape the sample to 2D array
-    probabilities = clf.predict_proba(sample)  # Predict the probabilistic outputs
+    probabilities = clf.predict_proba(sample.reshape(1, -1)) # Predict the probabilistic outputs
     return probabilities[0]  # Return the first (and only) prediction
 ### End your code
 #</Predict>
