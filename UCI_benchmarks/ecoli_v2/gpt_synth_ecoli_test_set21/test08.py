8ad0bf4574e6af60af716a17112436efc5549a16
Philip Herman Schroeder <pschro@sls-510-0.csail.mit.edu>
2023-11-21 11:47:29 -0500

    Validation output 4 (phase 2): Response to sklearn.exceptions.NotFittedError
    Error from previous code:
      File "<stdin>", line 14, in <module>
      File "<string>", line 17, in <module>
      File "<string>", line 69, in predict_label
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/_set_output.py", line 140, in wrapped
        data_to_wrap = f(self, X, *args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/preprocessing/_data.py", line 1001, in transform
        check_is_fitted(self)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 1462, in check_is_fitted
        raise NotFittedError(msg % {"name": type(estimator).__name__})
    <class 'sklearn.exceptions.NotFittedError'>
    This StandardScaler instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    # Read file
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.metrics import roc_auc_score
    from sklearn.preprocessing import StandardScaler
    
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    # replace strings with numbers in y
    np.unique( y)
    len(list(np.unique( y)))
    y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
    
    X=X.to_numpy()
    y=y.to_numpy()
    
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Create StandardScaler model and fit it to Train data
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    X_test = sc.transform(X_test) # save scaled features for later use in the predict_label function
    
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    # Import necessary modules
    from sklearn.linear_model import LogisticRegression
    
    ### Start your code
    # Create a logistic regression model
    clf = LogisticRegression(random_state=0, max_iter=200)
    
    # Train the logistic regression model
    clf.fit(X_train, y_train)
    ### End your code
    #</Train>
    
    #<Predict>
    ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
    
    # Define the function
    def predict_label(x):
        x = sc.transform([x])  # Scale the features
        y_pred = clf.predict_proba(x)  # Predict the labels
        return y_pred
    
    ### End your code
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test08.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test08.py
@@ -21,6 +21,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
+
 ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]  
@@ -29,30 +30,22 @@ y = ecoli.iloc[:, -1]
 np.unique( y)
 len(list(np.unique( y)))
 y = y.replace(list(np.unique(y)), [0,1,2,3,4,5,6,7])
+
 X=X.to_numpy()
 y=y.to_numpy()
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
-# Scale the features 
+
+# Create StandardScaler model and fit it to Train data
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test) # save scaled features for later use in the predict_label function
+
 print(X_train.shape)
 print(y_train.shape)
 print(X_train[0:5])
 print(y_train[0:5])
 #</PrepData>
-
-#<Train>
-######## Train the model using the training data, X_train and y_train
-### Start your code
-### End your code
-#</Train>
-
-#<Predict>
-######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
-### Start your code
-### End your code
-#</Predict>
-
 
 #<Train>
 ######## Train the model using the training data, X_train and y_train
@@ -67,10 +60,10 @@ clf = LogisticRegression(random_state=0, max_iter=200)
 clf.fit(X_train, y_train)
 ### End your code
 #</Train>
+
 #<Predict>
 ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
+
 
 # Define the function
 def predict_label(x):
