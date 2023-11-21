4b370a09f94ea04e90a7cfaef485e763169b2653
Philip Herman Schroeder <pschro@sls-510-0.csail.mit.edu>
2023-11-21 15:45:57 -0500

    Validation output 9 (phase 2): Response to ValueError
    Error from previous code:
      File "<stdin>", line 14, in <module>
      File "<string>", line 49, in <module>
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 1151, in wrapper
        return fit_method(estimator, *args, **kwargs)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/linear_model/_logistic.py", line 1207, in fit
        X, y = self._validate_data(
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/base.py", line 621, in _validate_data
        X, y = check_X_y(X, y, **check_params)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 1163, in check_X_y
        y = _check_y(y, multi_output=multi_output, y_numeric=y_numeric, estimator=estimator)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 1184, in _check_y
        y = column_or_1d(y, warn=True)
      File "/data/sls/scratch/pschro/anaconda3/envs/env310/lib/python3.10/site-packages/sklearn/utils/validation.py", line 1245, in column_or_1d
        raise ValueError(
    <class 'ValueError'>
    y should be a 1d array, got an array of shape (168, 8) instead.
    
    Response from GPT:
    #<PrevData>
    ######## Load and preview the dataset and datatypes
    # Import necessary libraries
    import pandas as pd
    df = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', header=None)
    # Preview dataset and datatypes
    print(df.shape)
    print(df.head())
    print(df.info())
    print(df.dtypes)
    for col in df.applymap(type).columns:
        print(col, df.applymap(type)[col].unique())
    print(df.isnull().sum())
    #</PrevData>
    
    #<PrepData>
    ######## Prepare the dataset for training
    # Import necessary packages
    import numpy as np
    from sklearn.model_selection import train_test_split
    from sklearn.preprocessing import StandardScaler, LabelEncoder
    
    ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli.data', delim_whitespace=True, header=None)
    ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
    X = ecoli.iloc[:, 1:-1]
    y = ecoli.iloc[:, -1]
    
    # replace strings with numbers in y
    le = LabelEncoder()
    y = le.fit_transform(y)
    
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
    
    # Scale the features
    sc = StandardScaler()
    X_train = sc.fit_transform(X_train)
    X_test = sc.transform(X_test)
    
    print(X_train.shape)
    print(y_train.shape)
    print(X_train[0:5])
    print(y_train[0:5])
    #</PrepData>
    
    #<Train>
    ######## Train the model using the training data, X_train and y_train
    from sklearn.linear_model import LogisticRegression
    
    # Initialize the model
    LR = LogisticRegression(random_state=0, multi_class='ovr', solver='lbfgs')
    
    # Fit the model to the data
    LR.fit(X_train, y_train)
    
    ### End your code
    #</Train>
    
    #<Predict>
    ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
    
    def predict_label(sample):
        # reshape sample to be 2D as a model expects the input to be in 2D
        sample = np.array(sample).reshape(1, -1)
        # predict the class label for the sample
        pred = LR.predict_proba(sample)
    
        return pred[0]
    #</Predict>

--- UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test13.py
+++ UCI_benchmarks/ecoli_v2/gpt_synth_ecoli_test_set21/test13.py
@@ -24,18 +24,18 @@ ecoli = pd.read_csv('/data/sls/scratch/pschro/p2/data/UCI_benchmarks/ecoli/ecoli
 ecoli.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 X = ecoli.iloc[:, 1:-1]  
 y = ecoli.iloc[:, -1]  
+
 # replace strings with numbers in y
 le = LabelEncoder()
 y = le.fit_transform(y)
-# ensure that target variable becomes binary
-y_bin = np.zeros((y.shape[0], len(np.unique(y))))
-for i, y_val in enumerate(y):
-    y_bin[i, y_val] = 1
-X_train, X_test, y_train, y_test = train_test_split(X, y_bin, test_size=0.5, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+
 # Scale the features 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
 print(X_train.shape)
 print(y_train.shape)
 print(X_train[0:5])
@@ -44,7 +44,6 @@ print(y_train[0:5])
 
 #<Train>
 ######## Train the model using the training data, X_train and y_train
-### Start your code
 from sklearn.linear_model import LogisticRegression
 
 # Initialize the model
@@ -52,17 +51,18 @@ LR = LogisticRegression(random_state=0, multi_class='ovr', solver='lbfgs')
 
 # Fit the model to the data
 LR.fit(X_train, y_train)
+
 ### End your code
 #</Train>
 
 #<Predict>
 ######## Define the predict_labels function that can be used to make new predictions using the trained model above given one sample from X_test
-### Start your code
+
 def predict_label(sample):
-    # reshape sample to be 2D as model expects input to be in 2D
+    # reshape sample to be 2D as a model expects the input to be in 2D
     sample = np.array(sample).reshape(1, -1)
     # predict the class label for the sample
-    pred = LR.pred_proba(sample)
+    pred = LR.predict_proba(sample)
+    
     return pred[0]
-### End your code
 #</Predict>
